8b1f9c285160cef01f162f08eade4e71a3003536
Sean Morgan <seanmorgan91@gmail.com>
2019-12-16 21:24:48 -0500

    Add baseline models

---
+++ armory/baseline_models/tf1/simple_keras.py
@@ -0,0 +1,44 @@
+"""
+
+"""
+import tensorflow.keras as keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+from art.classifiers import KerasClassifier
+
+
+def make_model():
+    model = Sequential()
+    model.add(
+        Conv2D(
+            filters=4,
+            kernel_size=(5, 5),
+            strides=1,
+            activation="relu",
+            input_shape=(28, 28, 1),
+        )
+    )
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(
+        Conv2D(
+            filters=10,
+            kernel_size=(5, 5),
+            strides=1,
+            activation="relu",
+            input_shape=(23, 23, 4),
+        )
+    )
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(100, activation="relu"))
+    model.add(Dense(10, activation="softmax"))
+
+    model.compile(
+        loss=keras.losses.categorical_crossentropy,
+        optimizer=keras.optimizers.SGD(lr=0.01),
+        metrics=["accuracy"],
+    )
+    return model
+
+
+SIMPLE_MODEL = KerasClassifier(make_model())
