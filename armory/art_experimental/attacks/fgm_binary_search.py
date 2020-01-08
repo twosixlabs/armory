af4e2e6a6574eed0237bae6333f546d66307d766
David Slater <david.slater@twosixlabs.com>
2020-01-07 16:12:07 -0800

    Enable binary search for optimal epsilon

--- armory/art_experimental/attacks/fgm_binary_search.py
+++ armory/art_experimental/attacks/fgm_binary_search.py
@@ -23,84 +23,85 @@ class FGMBinarySearch(attacks.FastGradientMethod):
         super().__init__(*args, **kwargs)
         self.set_params(minimal=True)
 
-    def _minimal_perturbation_binary(self, x, y):
-        adv = x.copy()
-        y_class = np.argmax(y)
-
-        # Get perturbation
-        perturbation = self._compute_perturbation(batch, batch_labels)
-
-        # Get current predictions
-        active_indices = np.arange(len(batch))
-        current_eps = self.eps_step
-        while active_indices.size > 0 and current_eps <= self.eps:
-            # Adversarial crafting
-            adv_batch[active_indices] = self._apply_perturbation(
-                batch[active_indices],
-                perturbation[active_indices],
-                current_eps,
-            )
-
-            # Check for success
-            adv_preds = self.classifier.predict(adv_batch)  # can we pare this down?
-            adv_classes = np.argmax(adv_preds, axis=1)
-            # If targeted active check to see whether we have hit the target
-            if self.targeted:
-                active_indices = np.where(batch_classes != adv_classes)[0]
-            else:
-                active_indices = np.where(batch_classes == adv_classes)[0]
-            current_eps += self.eps_step
-
-        return adv
-
-    def _minimal_perturbation(self, x, y) -> np.ndarray:
+    def _minimal_perturbation_binary_batch(self, batch, batch_labels, adv_batch=None):
         """
         Iteratively compute the minimal perturbation necessary to make the 
         class prediction change, using binary search.
         """
-        adv_x = x.copy()
-
-        # for now, ignore batching and do individually
-        for i in range(adv_x.shape[0]):
-            adv_x[i] = self._minimal_perturbation_binary(x[i], y[i])
-
-        return adv_x
-
-
-### Below: rewrite of art.attacks.FastGradientMethod._minimal_perturbation function
-
-    def _minimal_perturbation_linear_batch(self, batch, batch_labels, adv_batch=None):
         if adv_batch is None:
             adv_batch = batch.copy()
         batch_classes = np.argmax(batch_labels, axis=1)
+        adv_classes = batch_classes.copy()
+
+        # Get perturbation
+        perturbation = self._compute_perturbation(batch, batch_labels)
+
+        def check_epsilon(i, epsilon):
+            adv_batch = self._apply_perturbation(batch[[i]], perturbation[[i]], epsilon)
+            adv_pred = self.classifier.predict(adv_batch)
+            adv_class = np.argmax(adv_pred, axis=1)
+            if self.targeted:
+                success = batch_classes[[i]] == adv_class
+            else:
+                success = batch_classes[[i]] != adv_class
+            return success, adv_batch
+            
+
+        tolerance = self.eps_step
+        for i in range(len(batch)):
+            # Assume endpoints are correct
+            min_eps = 0
+            max_eps = self.eps
+            while max_eps - min_eps > tolerance:
+                mid_eps = (max_eps + min_eps)/2
+                #print(min_eps, mid_eps, max_eps, tolerance)
+
+                success, adv_i = check_epsilon(i, mid_eps)
+                if success:
+                    adv_batch[[i]] = adv_i
+                    max_eps = mid_eps
+                else:
+                    min_eps = mid_eps
+            
+        return adv_batch
+            
+    def _minimal_perturbation_linear_batch(self, batch, batch_labels, adv_batch=None):
+        """
+        Rewrite of inner loop for linear search
+        """
+        if adv_batch is None:
+            adv_batch = batch.copy()
+        batch_classes = np.argmax(batch_labels, axis=1)
+        adv_classes = batch_classes.copy()
 
         # Get perturbation
         perturbation = self._compute_perturbation(batch, batch_labels)
 
         # Get current predictions
-        active_indices = np.arange(len(batch))
+        active = np.arange(len(batch))
         current_eps = self.eps_step
-        while active_indices.size > 0 and current_eps <= self.eps:
+        while active.size > 0 and current_eps <= self.eps:
             # Adversarial crafting
-            adv_batch[active_indices] = self._apply_perturbation(
-                batch[active_indices],
-                perturbation[active_indices],
+            adv_batch[active] = self._apply_perturbation(
+                batch[active],
+                perturbation[active],
                 current_eps,
             )
 
             # Check for success
-            adv_preds = self.classifier.predict(adv_batch)  # can we pare this down?
-            adv_classes = np.argmax(adv_preds, axis=1)
+            adv_preds = self.classifier.predict(adv_batch[active])
+            #adv_preds = self.classifier.predict(adv_batch)  # can we pare this down?
+            adv_classes[active] = np.argmax(adv_preds, axis=1)
             # If targeted active check to see whether we have hit the target
             if self.targeted:
-                active_indices = np.where(batch_classes != adv_classes)[0]
+                active = np.where(batch_classes != adv_classes)[0]
             else:
-                active_indices = np.where(batch_classes == adv_classes)[0]
+                active = np.where(batch_classes == adv_classes)[0]
             current_eps += self.eps_step
 
         return adv_batch
 
-    def _minimal_perturbation_linear(self, x, y) -> np.ndarray:
+    def _minimal_perturbation(self, x, y) -> np.ndarray:
         """
         Iteratively compute the minimal perturbation necessary to make the 
         class prediction change, using binary search.
@@ -110,7 +111,7 @@ class FGMBinarySearch(attacks.FastGradientMethod):
         # Compute perturbation with implicit batching
         for start in range(0, adv_x.shape[0], self.batch_size):
             end = start + self.batch_size 
-            adv_batch = self._minimal_perturbation_linear_batch(
+            adv_batch = self._minimal_perturbation_binary_batch(
                 x[start:end],
                 y[start:end],
                 adv_x[start:end],
