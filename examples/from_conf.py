fbe636b24a9ba8aa78912211f085d7af36c600ec
David Slater <david.slater@twosixlabs.com>
2020-01-10 14:09:57 -0800

    run from config

---
+++ examples/from_conf.py
@@ -0,0 +1,32 @@
+"""
+python -m examples.from_conf <json_config>
+
+Try: 
+python -m examples.from_conf examples/mnist_fgm_all_epsilon.json
+
+This runs an arbitrary config file. Results are output to the `outputs/` directory.
+"""
+
+import argparse
+import json
+import logging
+
+import coloredlogs
+
+from armory.eval import Evaluator
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run Armory from config file.')
+    parser.add_argument('filepath', metavar='<json_config>', type=str, help='json config file')
+    parser.add_argument('-d', '--debug', dest='log_level',
+                        action='store_const', const=logging.DEBUG,
+                        default=logging.INFO,
+                        help='Debug output (logging=DEBUG)')
+    args = parser.parse_args()
+
+    coloredlogs.install(level=args.log_level)
+    with open(args.filepath) as f:
+        config = json.load(f)
+    rig = Evaluator(config)
+    rig.run_config()
