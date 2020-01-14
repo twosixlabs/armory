6d4f6fd0d06c0f01e88ef2d09c0640f361714040
Sean Morgan <seanmorgan91@gmail.com>
2020-01-14 12:58:12 -0500

    Format with black

--- tests/test_external_repo.py
+++ tests/test_external_repo.py
@@ -7,10 +7,10 @@ from armory.utils.external_repo import download_and_extract
 
 class ExternalRepoTest(unittest.TestCase):
     def test_download(self):
-        with open('tests/test_data/external_repo.json') as f:
+        with open("tests/test_data/external_repo.json") as f:
             config = json.load(f)
 
-        repo_name = config["external_github_repo"].split('/')[-1]
+        repo_name = config["external_github_repo"].split("/")[-1]
 
         download_and_extract(config)
         self.assertTrue(os.path.exists(f"external_repos/{repo_name}"))
