d41019015f1bb7a113500f5fed496cd451c9bf42
David Slater <david.slater@twosixlabs.com>
2020-01-23 09:26:06 -0500

    added usage doc

--- scripts/format_json.py
+++ scripts/format_json.py
@@ -4,6 +4,8 @@ Script to format all JSON in git repo.
 Output is meant to look similar to `black`
 Format is the same as Python's built in json.tool
     However, it overcomes some command line errors in rewriting the same file
+
+Usage: python -m scripts.format_json
 """
 
 import json
