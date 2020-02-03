e4b566e639fe708007f6a5dd5ee01bad86c8fac8
Sean Morgan <seanmorgan@outlook.com>
2020-02-03 10:40:57 -0500

    Add pre-commit hook information (#89)
    * Add pre-commit hook information
    * Update permissions and supress extra output

---
+++ tools/pre-commit.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+python -m black --check ./ > /dev/null 2>&1
+need_format=$?
+
+set -e
+if [ $need_format -ne 0 ]
+then
+    python -m black ./
+    echo Some Python files were formatted
+    echo You need to do git add and git commit again
+    exit $need_format
+fi
+
+python -m flake8
