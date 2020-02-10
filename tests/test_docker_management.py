1b647ab9b5762d36843eb47e6f1fb0ff5690038c
Sean Morgan <seanmorgan@outlook.com>
2020-02-10 13:08:08 -0500

    Update versions on master branch (#148)
    * Update versions on master branch
    
    * Correction

--- tests/test_docker_management.py
+++ tests/test_docker_management.py
@@ -9,13 +9,13 @@ from docker.models.containers import Container
 
 class ContainerTest(unittest.TestCase):
     def test_creation(self):
-        manager = ManagementInstance(image_name="twosixarmory/tf1:0.3.0")
+        manager = ManagementInstance(image_name="twosixarmory/tf1:0.3.3")
         instance = manager.start_armory_instance()
         self.assertIsInstance(instance.docker_container, Container)
         self.assertIn(instance.docker_container.short_id, manager.instances)
 
     def test_deletion(self):
-        manager = ManagementInstance(image_name="twosixarmory/tf1:0.3.0")
+        manager = ManagementInstance(image_name="twosixarmory/tf1:0.3.3")
         instance = manager.start_armory_instance()
         manager.stop_armory_instance(instance)
         self.assertEqual(manager.instances, {})
