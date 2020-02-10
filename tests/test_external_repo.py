236c7c2abfc0eaa38e8767d7759f2909aae9796c
armory-twosix <60190012+armory-twosix@users.noreply.github.com>
2020-02-10 10:58:38 -0500

    Switch to github actions (#141)
    * Switch to GitHub actions

--- tests/test_external_repo.py
+++ tests/test_external_repo.py
@@ -8,11 +8,7 @@ from armory.utils.external_repo import download_and_extract_repo
 
 class ExternalRepoTest(unittest.TestCase):
     def test_download(self):
-        # Skip test if token is unavailable
-        if os.environ.get("GITHUB_TOKEN") is None:
-            return
-
-        repo = "twosixlabs/armory-performer"
+        repo = "twosixlabs/armory-example"
         repo_name = repo.split("/")[-1]
 
         download_and_extract_repo(repo)
