c9f96294e81b66fe79ea250550c0ab5b6549fd39
David Slater <david.slater@twosixlabs.com>
2020-02-19 15:45:59 -0800

    working clean

--- armory/docker/images.py
+++ armory/docker/images.py
@@ -14,3 +14,19 @@ ALL = (
     TF2,
     PYTORCH,
 )
+REPOSITORIES = tuple(x.split(":")[0] for x in ALL)
+
+def is_old(tag: str):
+    """
+    Return True if tag is an old armory container, False otherwise
+    """
+    if not isinstance(tag, str):
+        raise ValueError(f"tag must be of type str, not type {type(tag)}")
+    if tag in ALL:
+        return False
+    tokens = tag.split(":")
+    if len(tokens) != 2:
+        return False
+    if tokens[0] in REPOSITORIES:
+        return True
+    return False
