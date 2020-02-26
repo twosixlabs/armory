078c219a2d0d4a13e40aed15534fac0a2184b405
Neal Gupta <neal.gupta@twosixlabs.com>
2020-02-26 14:15:36 -0500

    Update transfer attack to resize and add test

--- armory/baseline_models/keras/keras_inception_resnet_v2.py
+++ armory/baseline_models/keras/keras_inception_resnet_v2.py
@@ -1,10 +1,21 @@
 from art.classifiers import KerasClassifier
+import numpy as np
+from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.inception_resnet_v2 import (
     InceptionResNetV2,
-    preprocess_input,
+    preprocess_input as preprocess_input_inception_resnet_v2,
 )
 
-preprocessing_fn = preprocess_input
+
+def preprocessing_fn(x: np.ndarray) -> np.ndarray:
+    shape = (299, 299)  # Expected input shape of model
+    output = []
+    for i in range(x.shape[0]):
+        im_raw = image.array_to_img(x[i])
+        im = image.img_to_array(im_raw.resize(shape))
+        output.append(im)
+    output = preprocess_input_inception_resnet_v2(np.array(output))
+    return output
 
 
 def get_art_model(model_kwargs, wrapper_kwargs):
