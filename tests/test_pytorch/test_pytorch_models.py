a4d0bc71a75027ff357cb54ea9638fc8e0ab5a54
Sean Morgan <seanmorgan@outlook.com>
2020-03-17 11:55:55 -0400

    Add pytorch cifar baseline model (#273)

--- tests/test_pytorch/test_pytorch_models.py
+++ tests/test_pytorch/test_pytorch_models.py
@@ -43,3 +43,37 @@ class PyTorchModelsTest(unittest.TestCase):
             predictions = classifier.predict(x)
             accuracy += np.sum(np.argmax(predictions, axis=1) == y) / len(y)
         self.assertGreater(accuracy / test_dataset.total_iterations, 0.9)
+
+    def test_keras_cifar(self):
+        classifier_module = import_module(
+            "armory.baseline_models.pytorch.pytorch_cifar"
+        )
+        classifier_fn = getattr(classifier_module, "get_art_model")
+        classifier = classifier_fn(model_kwargs={}, wrapper_kwargs={})
+        preprocessing_fn = getattr(classifier_module, "preprocessing_fn")
+
+        train_dataset = datasets.cifar10(
+            split_type="train",
+            epochs=1,
+            batch_size=500,
+            dataset_dir=DATASET_DIR,
+            preprocessing_fn=preprocessing_fn,
+        )
+        test_dataset = datasets.cifar10(
+            split_type="test",
+            epochs=1,
+            batch_size=100,
+            dataset_dir=DATASET_DIR,
+            preprocessing_fn=preprocessing_fn,
+        )
+
+        classifier.fit_generator(
+            train_dataset, nb_epochs=train_dataset.total_iterations,
+        )
+
+        accuracy = 0
+        for _ in range(test_dataset.total_iterations):
+            x, y = test_dataset.get_batch()
+            predictions = classifier.predict(x)
+            accuracy += np.sum(np.argmax(predictions, axis=1) == y) / len(y)
+        self.assertGreater(accuracy / test_dataset.total_iterations, 0.3)
