3bca344050b93a48f1e5a9c04353bb450dfd2f91
Sean Morgan <seanmorgan@outlook.com>
2020-03-17 09:50:42 -0400

    Add pytorch mnist model (#272)
    * Add pytorch mnist model

--- tests/test_pytorch/test_pytorch_models.py
+++ tests/test_pytorch/test_pytorch_models.py
@@ -1,6 +1,45 @@
 import unittest
+from importlib import import_module
+
+import numpy as np
+
+from armory.data import datasets
+from armory import paths
+
+DATASET_DIR = paths.docker().dataset_dir
 
 
 class PyTorchModelsTest(unittest.TestCase):
     def test_pytorch_mnist(self):
-        pass
+        classifier_module = import_module(
+            "armory.baseline_models.pytorch.pytorch_mnist"
+        )
+        classifier_fn = getattr(classifier_module, "get_art_model")
+        classifier = classifier_fn(model_kwargs={}, wrapper_kwargs={})
+        preprocessing_fn = getattr(classifier_module, "preprocessing_fn")
+
+        train_dataset = datasets.mnist(
+            split_type="train",
+            epochs=1,
+            batch_size=600,
+            dataset_dir=DATASET_DIR,
+            preprocessing_fn=preprocessing_fn,
+        )
+        test_dataset = datasets.mnist(
+            split_type="test",
+            epochs=1,
+            batch_size=100,
+            dataset_dir=DATASET_DIR,
+            preprocessing_fn=preprocessing_fn,
+        )
+
+        classifier.fit_generator(
+            train_dataset, nb_epochs=train_dataset.total_iterations,
+        )
+
+        accuracy = 0
+        for _ in range(test_dataset.total_iterations):
+            x, y = test_dataset.get_batch()
+            predictions = classifier.predict(x)
+            accuracy += np.sum(np.argmax(predictions, axis=1) == y) / len(y)
+        self.assertGreater(accuracy / test_dataset.total_iterations, 0.9)
