71eed710eaa43c9d2f522f2f0d1ab4f54443dc7f
Sean Morgan <seanmorgan91@gmail.com>
2020-03-20 12:15:30 -0400

    Support external branches

--- tests/test_host/test_external_repo.py
+++ tests/test_host/test_external_repo.py
@@ -23,3 +23,20 @@ class ExternalRepoTest(unittest.TestCase):
         os.remove(external_repo_dir / (repo_name + ".tar.gz"))
         os.rmdir(external_repo_dir)
         os.rmdir(tmp_subdir)
+
+    def test_download_branch(self):
+        tmp_subdir = pathlib.Path(paths.host().tmp_dir, "test-external-repo-subdir")
+        external_repo_dir = pathlib.Path(paths.get_external(tmp_subdir))
+        repo = "twosixlabs/armory-example@armory-0.6"
+        org_repo_name = repo.split("@")[0]
+        repo_name = org_repo_name.split("/")[-1]
+
+        download_and_extract_repo(repo, external_repo_dir=external_repo_dir)
+        basedir = external_repo_dir / repo_name
+
+        self.assertTrue(os.path.exists(basedir))
+        self.assertTrue(os.path.isfile(basedir / "README.md"))
+        shutil.rmtree(basedir)
+        os.remove(external_repo_dir / (repo_name + ".tar.gz"))
+        os.rmdir(external_repo_dir)
+        os.rmdir(tmp_subdir)
