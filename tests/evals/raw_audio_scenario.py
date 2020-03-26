0be2280738afaf8a3b332744470f0ca4b3bc0181
Hiro <hkakitani@mitre.org>
2020-03-26 17:45:36 +0000

    using label map constant instead of function

--- tests/evals/raw_audio_scenario.py
+++ tests/evals/raw_audio_scenario.py
@@ -20,17 +20,7 @@ logger.setLevel(logging.INFO)
 
 DEMO = True
 
-def get_label_map():
-    metadata_dir = os.path.join(paths.docker().dataset_dir, "librispeech_dev_clean_split/plain_text/1.1.0/metadata.json")
-    metadata = json.load(open(metadata_dir))    
-    relevant_speakers = {}
-    for id, info in metadata["speakers"].items():
-        if(info["subset"] == "dev-clean"):
-            relevant_speakers[id] = info
-    original_id_list = list(map(int,relevant_speakers.keys()))
-    original_id_list.sort()
-    return original_id_list
-
+LABEL_MAP = [84, 174, 251, 422, 652, 777, 1272, 1462, 1673, 1919, 1988, 1993, 2035, 2078, 2086, 2277, 2412, 2428, 2803, 2902, 3000, 3081, 3170, 3536, 3576, 3752, 3853, 5338, 5536, 5694, 5895, 6241, 6295, 6313, 6319, 6345, 7850, 7976, 8297, 8842]
 
 def evaluate_classifier(config_path: str) -> None:
     with open(config_path) as f:
@@ -48,18 +38,32 @@ def evaluate_classifier(config_path: str) -> None:
         split_type="test",
         preprocessing_fn=preprocessing_fn
     )    
-
+    
     if(not model_config["model_kwargs"]["pretrained"]):
         logger.info(
             f"Fitting clean unpoisoned model of {model_config['module']}.{model_config['name']}..."
         )
-        # TODO train here
+        train_data_generator = load_dataset(
+            config["dataset"],
+            epochs=config["adhoc"]["epochs"],
+            split_type="train",
+            preprocessing_fn=preprocessing_fn,
+        )
+
+        if DEMO:
+            nb_epochs = 10
+        else:
+            nb_epochs = train_data_generator.total_iterations
+        # TODO [immediate] index out of bounds for fit. using label as an index for an array that has nb_classes elements 
+        #(determined by get_art_model, in PyTorchClassifier declaration in sincnet.py)
+        # generator needs to output label in range (0-39)
+        classifier.fit_generator(train_data_generator, nb_epochs=nb_epochs)
 
     # Evaluate the ART classifier on benign test examples
     logger.info("Running inference on benign examples...")
     benign_accuracy = 0
     cnt = 0
-    label_map = get_label_map()
+
     if DEMO:
         iterations = 640
     else:
@@ -67,7 +71,7 @@ def evaluate_classifier(config_path: str) -> None:
     for _ in range(iterations):
         x, y = test_data_generator.get_batch()
         predictions = classifier.predict(x) 
-        benign_accuracy += np.sum([label_map[i] for i in np.argmax(predictions, axis=1)] == y) / len(y)
+        benign_accuracy += np.sum([LABEL_MAP[i] for i in np.argmax(predictions, axis=1)] == y) / len(y)
         cnt += 1
     logger.info(
         "Accuracy on benign test examples: {}%".format(benign_accuracy * 100 / cnt)
