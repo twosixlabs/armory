5e573e841b6f6d9ad8b26252ad4d492fc7787060
Sean Morgan <seanmorgan91@gmail.com>
2020-03-30 10:15:44 -0400

    Lint

--- armory/baseline_models/pytorch/pytorch_ucf101_mars.py
+++ armory/baseline_models/pytorch/pytorch_ucf101_mars.py
@@ -1,4 +1,3 @@
-import logging
 import os
 import sys
 
@@ -6,10 +5,7 @@ from art.classifiers import PyTorchClassifier
 import numpy as np
 from PIL import Image
 import torch
-from torch import nn
 from torch import optim
-from torchvision import models
-from torch.optim import lr_scheduler
 from armory import paths
 
 # MARS specific imports
@@ -17,30 +13,35 @@ from MARS.opts import parse_opts
 from MARS.models.model import generate_model
 from MARS.dataset import preprocess_data
 
+
 def make_model(**kwargs):
-    sys.argv=[''];
+    sys.argv = [""]
     opt = parse_opts()
 
     # Default opts for UCF101 dataset
-    opt.dataset = 'UCF101'
-    opt.modality = 'RGB'
+    opt.dataset = "UCF101"
+    opt.modality = "RGB"
     opt.split = 1
     opt.only_RGB = True
-    opt.model = 'resnext'
+    opt.model = "resnext"
     opt.model_depth = 101
     opt.sample_duration = 16
     opt.log = 0
     opt.batch_size = 1
     opt.input_channels = 3
-    opt.arch = '{}-{}'.format(opt.model, opt.model_depth)
+    opt.arch = "{}-{}".format(opt.model, opt.model_depth)
 
-    model_status = kwargs.get('model_status', 'kinetics_pretrained')
-    if model_status == 'ucf101_trained':
+    model_status = kwargs.get("model_status", "kinetics_pretrained")
+    if model_status == "ucf101_trained":
         opt.n_classes = 101
-        opt.resume_path1 = os.path.join(paths.docker().saved_model_dir, "mars", "MARS_UCF101_16f.pth")
+        opt.resume_path1 = os.path.join(
+            paths.docker().saved_model_dir, "mars", "MARS_UCF101_16f.pth"
+        )
     else:
         opt.n_classes = 400
-        opt.pretrain_path = os.path.join(paths.docker().saved_model_dir, "mars", "RGB_Kinetics_16f.pth")
+        opt.pretrain_path = os.path.join(
+            paths.docker().saved_model_dir, "mars", "RGB_Kinetics_16f.pth"
+        )
         opt.n_finetune_classes = 101
         opt.batch_size = 32
         opt.ft_begin_index = 4
@@ -49,23 +50,25 @@ def make_model(**kwargs):
     model, parameters = generate_model(opt)
 
     # Loading trained model weights
-    if opt.resume_path1:
-        print('loading checkpoint for UCF101 trained model {}'.format(opt.resume_path1))
-        checkpoint = torch.load(opt.resume_path1)
-        assert opt.arch == checkpoint['arch']
-        opt.begin_epoch = checkpoint['epoch']
-        model.load_state_dict(checkpoint['state_dict'])
+    # if opt.resume_path1:
+    #     print("loading checkpoint for UCF101 trained model {}".format(opt.resume_path1))
+    #     checkpoint = torch.load(opt.resume_path1)
+    #     assert opt.arch == checkpoint["arch"]
+    #     opt.begin_epoch = checkpoint["epoch"]
+    #     model.load_state_dict(checkpoint["state_dict"])
 
     # Initializing the optimizer
     if opt.pretrain_path:
         opt.weight_decay = 1e-5
         opt.learning_rate = 0.001
-    if opt.nesterov: dampening = 0
-    else: dampening = opt.dampening
+    if opt.nesterov:
+        dampening = 0
+    else:
+        dampening = opt.dampening
 
-    #print("lr = {} \t momentum = {} \t dampening = {} \t weight_decay = {}, \t nesterov = {}"
+    # print("lr = {} \t momentum = {} \t dampening = {} \t weight_decay = {}, \t nesterov = {}"
     #            .format(opt.learning_rate, opt.momentum, dampening, opt. weight_decay, opt.nesterov))
-    #print("LR patience = ", opt.lr_patience)
+    # print("LR patience = ", opt.lr_patience)
 
     optimizer = optim.SGD(
         parameters,
@@ -73,9 +76,11 @@ def make_model(**kwargs):
         momentum=opt.momentum,
         dampening=dampening,
         weight_decay=opt.weight_decay,
-        nesterov=opt.nesterov)
+        nesterov=opt.nesterov,
+    )
 
     return model, optimizer
+
 
 def preprocessing_fn(inputs):
     """
@@ -86,37 +91,45 @@ def preprocessing_fn(inputs):
 
     Outputs is a list of videos, each of shape (n_stack, 3, 16, 112, 112)
     """
-    sample_duration = 16 # expected number of consecutive frames as input to the model
+    sample_duration = 16  # expected number of consecutive frames as input to the model
     outputs = []
-    if inputs.dtype == np.uint8: # inputs is a single video, i.e., batch size == 1
+    if inputs.dtype == np.uint8:  # inputs is a single video, i.e., batch size == 1
         inputs = [inputs]
     # else, inputs is an ndarray (of type object) of ndarrays
-    for input in inputs: # each input is (1, time, height, width, 3) from the same video
+    for (
+        input
+    ) in inputs:  # each input is (1, time, height, width, 3) from the same video
         input = np.squeeze(input)
 
         # select a fixed number of consecutive frames
         total_frames = input.shape[0]
-        if total_frames <= sample_duration: # cyclic pad if not enough frames
-            input_fixed = np.vstack((input, input[:sample_duration-total_frames,...]))
-            assert(input_fixed.shape[0] == sample_duration)
-        else: input_fixed = input
+        if total_frames <= sample_duration:  # cyclic pad if not enough frames
+            input_fixed = np.vstack(
+                (input, input[: sample_duration - total_frames, ...])
+            )
+            assert input_fixed.shape[0] == sample_duration
+        else:
+            input_fixed = input
 
         # apply MARS preprocessing: scaling, cropping, normalizing
         opt = parse_opts()
-        opt.modality = 'RGB'
+        opt.modality = "RGB"
         opt.sample_size = 112
-        input_Image = [] # convert each frame to PIL Image
+        input_Image = []  # convert each frame to PIL Image
         for f in input_fixed:
             input_Image.append(Image.fromarray(f))
         input_mars_preprocessed = preprocess_data.scale_crop(input_Image, 0, opt)
 
         # reshape
         input_reshaped = []
-        for ns in range(int(total_frames/sample_duration)):
-            np_frames = input_mars_preprocessed[:,ns*sample_duration:(ns+1)*sample_duration,:,:].numpy()
+        for ns in range(int(total_frames / sample_duration)):
+            np_frames = input_mars_preprocessed[
+                :, ns * sample_duration : (ns + 1) * sample_duration, :, :
+            ].numpy()
             input_reshaped.append(np_frames)
         outputs.append(np.array(input_reshaped, dtype=np.float32))
     return outputs
+
 
 # NOTE: PyTorchClassifier expects numpy input, not torch.Tensor input
 def get_art_model(model_kwargs, wrapper_kwargs):
@@ -128,11 +141,13 @@ def get_art_model(model_kwargs, wrapper_kwargs):
         loss=torch.nn.CrossEntropyLoss().cuda(),
         optimizer=optimizer,
         input_shape=(3, 16, 112, 112),
-        nb_classes = 101,
+        nb_classes=101,
         **wrapper_kwargs,
         clip_values=(
-            np.transpose(np.zeros((16,112,112,3))-activity_means, (3,0,1,2)),
-            np.transpose(255.*np.ones((16,112,112,3))-activity_means, (3,0,1,2))
+            np.transpose(np.zeros((16, 112, 112, 3)) - activity_means, (3, 0, 1, 2)),
+            np.transpose(
+                255.0 * np.ones((16, 112, 112, 3)) - activity_means, (3, 0, 1, 2)
+            ),
         )
     )
     return wrapped_model
