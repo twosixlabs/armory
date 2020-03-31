10af6c794fe90e79f5ad77a3c6653ce274188b64
Sean Morgan <seanmorgan91@gmail.com>
2020-03-30 20:53:05 -0400

    Move test to docker

--- tests/test_docker/test_weight_download.py
+++ tests/test_docker/test_weight_download.py
@@ -7,8 +7,13 @@ from armory.data.utils import download_file_from_s3
 
 class ModelWeightsTest(unittest.TestCase):
     def test_download(self):
-        saved_model_dir = paths.host().saved_model_dir
+        saved_model_dir = paths.docker().saved_model_dir
         weights_file = "resnet50_weights_tf_dim_ordering_tf_kernels.h5"
+
+        filepath = os.path.join(saved_model_dir, weights_file)
+
+        if os.path.isfile(filepath):
+            os.remove(filepath)
 
         download_file_from_s3(
             "armory-public-data",
