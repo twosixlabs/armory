935d9f0437ed524c9e572c2b1274c5dde8e20940
davidslater <david.slater@twosixlabs.com>
2020-04-09 08:24:25 -0700

    Cifar10 scenario (#354)
    * basic cifar structure
    
    * basic cifar structure
    
    * updated log
    
    * wip cifar10
    
    * no print
    
    * responses
    
    * not
    
    * added defense
    
    * lint
    
    * remove defense from scenario

--- armory/utils/config_loading.py
+++ armory/utils/config_loading.py
@@ -1,7 +1,25 @@
 """
 Helper utilies to load things from armory configuration files.
 """
+
 from importlib import import_module
+
+from art.attacks import Attack
+from art.defences import Preprocessor, Postprocessor, Trainer, Transformer
+from art.classifiers import Classifier
+
+from armory.data.datasets import ArmoryDataGenerator
+
+
+DEFENSES = (Preprocessor, Postprocessor, Trainer, Transformer)
+
+
+def load(sub_config):
+    module = import_module(sub_config["module"])
+    fn = getattr(module, sub_config["name"])
+    args = sub_config.get("args", [])
+    kwargs = sub_config.get("kwargs", {})
+    return fn(*args, **kwargs)
 
 
 def load_dataset(dataset_config, *args, **kwargs):
@@ -11,7 +29,10 @@ def load_dataset(dataset_config, *args, **kwargs):
     dataset_module = import_module(dataset_config["module"])
     dataset_fn = getattr(dataset_module, dataset_config["name"])
     batch_size = dataset_config["batch_size"]
-    return dataset_fn(batch_size=batch_size, *args, **kwargs)
+    dataset = dataset_fn(batch_size=batch_size, *args, **kwargs)
+    if not isinstance(dataset, ArmoryDataGenerator):
+        raise ValueError(f"{dataset} is not an instance of {ArmoryDataGenerator}")
+    return dataset
 
 
 def load_model(model_config):
@@ -24,15 +45,28 @@ def load_model(model_config):
     model = model_fn(
         model_config["model_kwargs"], model_config["wrapper_kwargs"], weights_file
     )
+    if not isinstance(model, Classifier):
+        raise TypeError(f"{model} is not an instance of {Classifier}")
 
-    # If no preprocessing function with model return `None`
     preprocessing_fn = getattr(model_module, "preprocessing_fn", None)
+    if preprocessing_fn is not None and not callable(preprocessing_fn):
+        raise TypeError(f"preprocessing_fn {preprocessing_fn} must be None or callable")
     return model, preprocessing_fn
 
 
-def load_attack(attack_config):
-    raise NotImplementedError
+def load_attack(attack_config, classifier):
+    attack_module = import_module(attack_config["module"])
+    attack_fn = getattr(attack_module, attack_config["name"])
+    attack = attack_fn(classifier=classifier, **attack_config["kwargs"])
+    if not isinstance(attack, Attack):
+        raise TypeError(f"attack {attack} is not an instance of {Attack}")
+    return attack
 
 
-def load_defense(defense_config):
-    raise NotImplementedError
+def load_defense(defense_config, classifier):
+    defense_module = import_module(defense_config["module"])
+    defense_fn = getattr(defense_module, defense_config["name"])
+    defense = defense_fn(classifier=classifier, **defense_config["kwargs"])
+    if not any(isinstance(defense, x) for x in DEFENSES):
+        raise TypeError(f"defense {defense} is not a defense instance: {DEFENSES}")
+    return defense
--- armory/utils/metrics.py
+++ armory/utils/metrics.py
@@ -1,3 +1,35 @@
+"""
+Metrics for scenarios
+
+Outputs are lists of python variables amenable to JSON serialization:
+    e.g., bool, int, float
+    numpy data types and tensors generally fail to serialize
+"""
+
+import numpy as np
+
+
+def categorical_accuracy(y, y_pred):
+    """
+    Return the categorical accuracy of the predictions
+    """
+    if y.shape == y_pred.shape:
+        return [bool(x) for x in list(y == y_pred)]
+    elif y.ndim + 1 == y_pred.ndim:
+        return [bool(x) for x in list(y == np.argmax(y_pred, axis=1))]
+    else:
+        raise ValueError(f"{y} and {y_pred} have mismatched dimensions")
+
+
+def linf(x, x_adv):
+    """
+    Return the L infinity norm over a batch of inputs as a float
+    """
+    # uint8 inputs will return incorrect results, so cast to float
+    diff = np.abs(x.astype(float) - x_adv.astype(float))
+    return list(np.max(diff, tuple(range(1, diff.ndim))))
+
+
 class AverageMeter:
     """
     Computes and stores the average and current value
