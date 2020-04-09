8f57f9f82a8aaaf71a9657b280f46a37f5e750c5
davidslater <david.slater@twosixlabs.com>
2020-04-09 09:28:37 -0700

    Resisc45 class (#362)
    * class format
    * updated scenario

--- armory/scenarios/resisc45.py
+++ armory/scenarios/resisc45.py
@@ -2,99 +2,81 @@
 Classifier evaluation within ARMORY
 """
 
-import json
-import os
-import sys
 import logging
-from importlib import import_module
 
-import numpy as np
+from tqdm import tqdm
 
-from armory.utils.config_loading import load_dataset, load_model
-from armory import paths
+from armory.scenarios.base import Scenario
+from armory.utils import metrics
+from armory.utils.config_loading import load_dataset, load_model, load_attack
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
 
-def evaluate_classifier(config_path: str) -> None:
-    """
-    Evaluate a config file for classiifcation robustness against attack.
-    """
-    with open(config_path) as f:
-        config = json.load(f)
+class Resisc45(Scenario):
+    def _evaluate(self, config: dict) -> dict:
+        """
+        Evaluate the config and return a results dict
+        """
 
-    model_config = config["model"]
-    classifier, preprocessing_fn = load_model(model_config)
+        model_config = config["model"]
+        classifier, preprocessing_fn = load_model(model_config)
 
-    logger.info(f"Loading dataset {config['dataset']['name']}...")
-    test_data_generator = load_dataset(
-        config["dataset"],
-        epochs=1,
-        split_type="test",
-        preprocessing_fn=preprocessing_fn,
-    )
+        if not model_config["weights_file"]:
+            classifier.set_learning_phase(True)
+            logger.info(
+                f"Fitting model {model_config['module']}.{model_config['name']}..."
+            )
+            fit_kwargs = model_config["fit_kwargs"]
+            train_data = load_dataset(
+                config["dataset"],
+                epochs=fit_kwargs["nb_epochs"],
+                split_type="train",
+                preprocessing_fn=preprocessing_fn,
+            )
+            classifier.fit_generator(train_data, **fit_kwargs)
 
-    if not model_config["weights_file"]:
-        logger.info(
-            f"Fitting clean unpoisoned model of {model_config['module']}.{model_config['name']}..."
+        classifier.set_learning_phase(False)
+        logger.info(f"Loading dataset {config['dataset']['name']}...")
+        test_data_generator = load_dataset(
+            config["dataset"],
+            epochs=1,
+            split_type="test",
+            preprocessing_fn=preprocessing_fn,
         )
-        # TODO train here
 
-    # Evaluate the ART classifier on benign test examples
-    logger.info("Running inference on benign examples...")
-    benign_accuracy = 0
-    cnt = 0
-    for _ in range(test_data_generator.batches_per_epoch):
-        x, y = test_data_generator.get_batch()
-        predictions = classifier.predict(x)
-        benign_accuracy += np.sum(np.argmax(predictions, axis=1) == y) / len(y)
-        cnt += 1
-    benign_accuracy = benign_accuracy / cnt
-    logger.info("Accuracy on benign test examples: {}%".format(benign_accuracy * 100))
+        logger.info("Running inference on benign examples...")
+        task_metric = metrics.categorical_accuracy
+        benign_accuracies = []
+        for x, y in tqdm(test_data_generator, desc="Benign"):
+            y_pred = classifier.predict(x)
+            benign_accuracies.extend(task_metric(y, y_pred))
+        benign_accuracy = sum(benign_accuracies) / test_data_generator.size
+        logger.info(f"Accuracy on benign test examples: {benign_accuracy:.2%}")
 
-    # Generate adversarial test examples
-    attack_config = config["attack"]
-    attack_module = import_module(attack_config["module"])
-    attack_fn = getattr(attack_module, attack_config["name"])
+        # Generate adversarial test examples
+        logger.info("Generating / testing adversarial examples...")
+        attack = load_attack(config["attack"], classifier)
+        test_data_generator = load_dataset(
+            config["dataset"],
+            epochs=1,
+            split_type="test",
+            preprocessing_fn=preprocessing_fn,
+        )
+        # Evaluate the ART classifier on adversarial test examples
 
-    test_data_generator = load_dataset(
-        config["dataset"],
-        epochs=1,
-        split_type="test",
-        preprocessing_fn=preprocessing_fn,
-    )
-    # Evaluate the ART classifier on adversarial test examples
-    logger.info("Generating / testing adversarial examples...")
+        adversarial_accuracies = []
+        for x, y in tqdm(test_data_generator, desc="Attack"):
+            x_adv = attack.generate(x=x)
+            y_pred_adv = classifier.predict(x_adv)
+            adversarial_accuracies.extend(task_metric(y, y_pred_adv))
+        adversarial_accuracy = sum(adversarial_accuracies) / test_data_generator.size
+        logger.info(
+            f"Accuracy on adversarial test examples: {adversarial_accuracy:.2%}"
+        )
 
-    attack = attack_fn(classifier=classifier, **attack_config["kwargs"])
-    adversarial_accuracy = 0
-    cnt = 0
-    for _ in range(test_data_generator.batches_per_epoch):
-        x, y = test_data_generator.get_batch()
-        test_x_adv = attack.generate(x=x)
-        predictions = classifier.predict(test_x_adv)
-        adversarial_accuracy += np.sum(np.argmax(predictions, axis=1) == y) / len(y)
-        cnt += 1
-    adversarial_accuracy = adversarial_accuracy / cnt
-    logger.info(
-        "Accuracy on adversarial test examples: {}%".format(adversarial_accuracy * 100)
-    )
-
-    logger.info("Saving json output...")
-    filepath = os.path.join(paths.docker().output_dir, "evaluation-results.json")
-    with open(filepath, "w") as f:
-        output_dict = {
-            "config": config,
-            "results": {
-                "baseline_accuracy": str(benign_accuracy),
-                "adversarial_accuracy": str(adversarial_accuracy),
-            },
+        results = {
+            "baseline_accuracy": str(benign_accuracy),
+            "adversarial_accuracy": str(adversarial_accuracy),
         }
-        json.dump(output_dict, f, sort_keys=True, indent=4)
-    logger.info(f"Evaluation Results written <output_dir>/evaluation-results.json")
-
-
-if __name__ == "__main__":
-    config_path = sys.argv[-1]
-    evaluate_classifier(config_path)
+        return results
