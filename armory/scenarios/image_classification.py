2c1fdb0d7dade2258443234fcc75236d00e82031
David Slater <david.slater@twosixlabs.com>
2020-04-10 10:57:23 -0700

    unification

--- armory/scenarios/image_classification.py
+++ armory/scenarios/image_classification.py
@@ -1,5 +1,5 @@
 """
-CIFAR10 scenario evaluation
+General image classifiation scenario
 """
 
 import logging
@@ -18,7 +18,7 @@ from armory.scenarios.base import Scenario
 logger = logging.getLogger(__name__)
 
 
-class Cifar10(Scenario):
+class ImageClassification(Scenario):
     def _evaluate(self, config: dict) -> dict:
         """
         Evaluate the config and return a results dict
@@ -27,7 +27,7 @@ class Cifar10(Scenario):
         model_config = config["model"]
         classifier, preprocessing_fn = load_model(model_config)
 
-        if not model_config["weights_file"]:
+        if model_config["fit"]:
             classifier.set_learning_phase(True)
             logger.info(
                 f"Fitting model {model_config['module']}.{model_config['name']}..."
@@ -60,11 +60,12 @@ class Cifar10(Scenario):
         )
 
         logger.info("Running inference on benign examples...")
+
         task_metric = metrics.categorical_accuracy
         perturbation_metric = metrics.linf
 
         benign_accuracies = []
-        for cnt, (x, y) in tqdm(enumerate(test_data_generator), desc="Benign"):
+        for x, y in tqdm(test_data_generator, desc="Benign"):
             y_pred = classifier.predict(x)
             benign_accuracies.extend(task_metric(y, y_pred))
         benign_accuracy = sum(benign_accuracies) / test_data_generator.size
@@ -81,7 +82,7 @@ class Cifar10(Scenario):
             preprocessing_fn=preprocessing_fn,
         )
         adversarial_accuracies, perturbations = [], []
-        for cnt, (x, y) in tqdm(enumerate(test_data_generator), desc="Attack"):
+        for x, y in tqdm(test_data_generator, desc="Attack"):
             x_adv = attack.generate(x=x)
             y_pred_adv = classifier.predict(x_adv)
             adversarial_accuracies.extend(task_metric(y, y_pred_adv))
