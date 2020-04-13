6eddf496ca30ac34562e2d6347d65162049886df
Neal Gupta <neal.gupta@twosixlabs.com>
2020-04-13 12:04:42 -0400

    Remove curl convenience wrapper (unused)

--- armory/data/utils.py
+++ armory/data/utils.py
@@ -6,7 +6,6 @@ import logging
 import hashlib
 import tarfile
 import os
-import subprocess
 import shutil
 import random
 import string
@@ -66,21 +65,6 @@ def download_file_from_s3(bucket_name: str, key: str, local_path: str) -> None:
 
     else:
         logger.info(f"Reusing cached file {local_path}...")
-
-
-def curl(url: str, dirpath: str, filename: str) -> None:
-    """
-    Downloads a file with a specified output filename and directory
-    :param url: URL to file
-    :param dirpath: Output directory
-    :param filename: Output filename
-    """
-    try:
-        subprocess.check_call(["curl", "-L", url, "--output", filename], cwd=dirpath)
-    except FileNotFoundError as e:
-        raise FileNotFoundError(f"curl command not found. Is curl installed? {e}")
-    except subprocess.CalledProcessError:
-        raise subprocess.CalledProcessError
 
 
 def download_requests(url: str, dirpath: str, filename: str):
