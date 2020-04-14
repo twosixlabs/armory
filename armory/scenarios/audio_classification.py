68f46c27e8d5901904bb1a5f781421be9deb2b53
Sean Morgan <seanmorgan@outlook.com>
2020-04-13 21:14:58 -0400

    Consolidate audio classification (#399)
    * Consolidate audio classification
    
    * lint
    
    * Use metrics

--- armory/scenarios/audio_classification.py
+++ armory/scenarios/audio_classification.py
@@ -1,5 +1,5 @@
 """
-Librispeech Dev Clean scenario evaluation
+General audio classification scenario
 """
 
 import logging
@@ -10,6 +10,7 @@ from armory.utils.config_loading import (
     load_dataset,
     load_model,
     load_attack,
+    load_defense,
 )
 from armory.utils import metrics
 from armory.scenarios.base import Scenario
@@ -17,7 +18,7 @@ from armory.scenarios.base import Scenario
 logger = logging.getLogger(__name__)
 
 
-class LibrispeechDevClean(Scenario):
+class AudioClassificationTask(Scenario):
     def _evaluate(self, config: dict) -> dict:
         """
         Evaluate the config and return a results dict
@@ -26,21 +27,29 @@ class LibrispeechDevClean(Scenario):
         model_config = config["model"]
         classifier, preprocessing_fn = load_model(model_config)
 
-        if not model_config["weights_file"]:
+        if model_config["fit"]:
             classifier.set_learning_phase(True)
             logger.info(
                 f"Fitting model {model_config['module']}.{model_config['name']}..."
             )
             fit_kwargs = model_config["fit_kwargs"]
+
+            logger.info(f"Loading train dataset {config['dataset']['name']}...")
             train_data = load_dataset(
                 config["dataset"],
                 epochs=fit_kwargs["nb_epochs"],
                 split_type="train",
                 preprocessing_fn=preprocessing_fn,
             )
-            classifier.fit_generator(train_data, **fit_kwargs)
+            if config["defense"] is not None:
+                logger.info("loading defense")
+                defense = load_defense(config["defense"], classifier)
+                defense.fit_generator(train_data, **fit_kwargs)
+            else:
+                classifier.fit_generator(train_data, **fit_kwargs)
 
         classifier.set_learning_phase(False)
+
         # Evaluate the ART classifier on benign test examples
         logger.info(f"Loading dataset {config['dataset']['name']}...")
         test_data_generator = load_dataset(
@@ -50,15 +59,12 @@ class LibrispeechDevClean(Scenario):
             preprocessing_fn=preprocessing_fn,
         )
         logger.info("Running inference on benign examples...")
+        metrics_logger = metrics.MetricsLogger.from_config(config["metric"])
 
-        task_metric = metrics.categorical_accuracy
-
-        benign_accuracies = []
         for x, y in tqdm(test_data_generator, desc="Benign"):
             y_pred = classifier.predict(x)
-            benign_accuracies.extend(task_metric(y, y_pred))
-        benign_accuracy = sum(benign_accuracies) / test_data_generator.size
-        logger.info(f"Accuracy on benign test examples: {benign_accuracy:.2%}")
+            metrics_logger.update_task(y, y_pred)
+        metrics_logger.log_task()
 
         # Evaluate the ART classifier on adversarial test examples
         logger.info("Generating / testing adversarial examples...")
@@ -70,17 +76,10 @@ class LibrispeechDevClean(Scenario):
             split_type="test",
             preprocessing_fn=preprocessing_fn,
         )
-        adversarial_accuracies = []
         for x, y in tqdm(test_data_generator, desc="Attack"):
             x_adv = attack.generate(x=x)
             y_pred_adv = classifier.predict(x_adv)
-            adversarial_accuracies.extend(task_metric(y, y_pred_adv))
-        adversarial_accuracy = sum(adversarial_accuracies) / test_data_generator.size
-        logger.info(
-            f"Accuracy on adversarial test examples: {adversarial_accuracy:.2%}"
-        )
-        results = {
-            "mean_benign_accuracy": benign_accuracy,
-            "mean_adversarial_accuracy": adversarial_accuracy,
-        }
-        return results
+            metrics_logger.update_task(y, y_pred_adv, adversarial=True)
+            metrics_logger.update_perturbation(x, x_adv)
+        metrics_logger.log_task(adversarial=True)
+        return metrics_logger.results()
