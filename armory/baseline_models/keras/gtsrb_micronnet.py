375379aba133965f8ec432338945b2f678a47810
Yusong Tan <ytan@mitre.org>
2020-05-06 23:55:23 +0000

    Ran black, flake8

--- armory/baseline_models/keras/gtsrb_micronnet.py
+++ armory/baseline_models/keras/gtsrb_micronnet.py
@@ -4,7 +4,7 @@ MicronNet CNN model for 48x48x3 image classification
 Model contributed by: MITRE Corporation
 """
 import numpy as np
-from PIL import Image, ImageOps
+from PIL import ImageOps
 import tensorflow as tf
 from tensorflow.keras import Model, Input
 from tensorflow.keras.layers import Dense, Conv2D, Activation
@@ -15,23 +15,24 @@ from art.classifiers import KerasClassifier
 def preprocessing_fn(img):
     img_size = 48
     img_out = []
-    from im in img:
+    for im in img:
         img_eq = ImageOps.equalize(im)
         width, height = img_eq.size
         min_side = min(img_eq.size)
-        center = width//2, height//2
+        center = width // 2, height // 2
 
-        left = center[0] - min_side//2
-        top = center[1] - min_side//2
-        right = center[0] + min_side//2
-        bottom = center[1] + min_side//2
+        left = center[0] - min_side // 2
+        top = center[1] - min_side // 2
+        right = center[0] + min_side // 2
+        bottom = center[1] + min_side // 2
 
         img_eq = img_eq.crop((left, top, right, bottom))
-        img_eq = img_eq.resize([img_size,img_size])
+        img_eq = img_eq.resize([img_size, img_size])
 
         img_out.append(img_eq)
 
     return np.array(img_out, dtype=np.float32)
+
 
 def make_model(**kwargs) -> tf.keras.Model:
     """
