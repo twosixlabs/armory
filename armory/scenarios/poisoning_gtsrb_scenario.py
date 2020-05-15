99e3c3e24c2150ff8eec364a80eb79194516efc5
Neal Gupta <neal.gupta@twosixlabs.com>
2020-05-15 16:07:22 +0000

    Add metric logging, poison entire test set

--- armory/scenarios/poisoning_gtsrb_scenario.py
+++ armory/scenarios/poisoning_gtsrb_scenario.py
@@ -153,11 +153,23 @@ class GTSRB(Scenario):
             preprocessing_fn=preprocessing_fn,
         )
         validation_metric = metrics.MetricList("categorical_accuracy")
+        target_class_benign_metric = metrics.MetricList("categorical_accuracy")
         for x, y in tqdm(test_data, desc="Testing"):
             y_pred = classifier.predict(x)
             validation_metric.append(y, y_pred)
+            y_pred_tgt_class = y_pred[y == src_class]
+            if len(y_pred_tgt_class):
+                target_class_benign_metric.append(
+                    [src_class] * len(y_pred_tgt_class), y_pred_tgt_class
+                )
         logger.info(f"Unpoisoned validation accuracy: {validation_metric.mean():.2%}")
-        results = {"validation_accuracy": validation_metric.mean()}
+        logger.info(
+            f"Unpoisoned validation accuracy on targeted class: {target_class_benign_metric.mean():.2%}"
+        )
+        results = {
+            "validation_accuracy": validation_metric.mean(),
+            "validation_accuracy_targeted_class": target_class_benign_metric.mean(),
+        }
 
         if poison_dataset_flag:
             logger.info("Testing on poisoned test data")
@@ -169,12 +181,10 @@ class GTSRB(Scenario):
             )
             test_metric = metrics.MetricList("categorical_accuracy")
             targeted_test_metric = metrics.MetricList("categorical_accuracy")
-            fraction_poisoned_test = config["adhoc"].get("fraction_poisoned_test", 1)
             for x_test, y_test in tqdm(test_data, desc="Testing"):
-                if np.random.rand() < fraction_poisoned_test:
-                    x_test, _ = poison_batch(
-                        x_test, y_test, src_class, tgt_class, len(y_test), attack
-                    )
+                x_test, _ = poison_batch(
+                    x_test, y_test, src_class, tgt_class, len(y_test), attack
+                )
                 y_pred = classifier.predict(x_test)
                 test_metric.append(y_test, y_pred)
 
