82c52fdde70f9710f864d9df4520f03c629ddebf
David Willmes <dwillmes@mitre.org>
2020-06-08 21:29:52 +0000

    Poisoning scenario including variable fractions of train and test poisoned samples

--- armory/scenarios/poisoning_gtsrb_scenario.py
+++ armory/scenarios/poisoning_gtsrb_scenario.py
@@ -23,32 +23,6 @@ from armory.scenarios.base import Scenario
 logger = logging.getLogger(__name__)
 
 
-def poison_dataset(src_imgs, src_lbls, src, tgt, ds_size, attack, poisoned_indices):
-    # In this example, all images of "src" class have a trigger
-    # added and re-labeled as "tgt" class
-    # NOTE: currently art.attacks.PoisonAttackBackdoor only supports
-    #   black-white images.  One way to generate poisoned examples
-    #   is to convert each batch of multi-channel images of shape
-    #   (N,W,H,C) to N separate (C,W,H)-tuple, where C would be
-    #   interpreted by PoisonAttackBackdoor as the batch size,
-    #   and each channel would have a backdoor trigger added
-    poison_x = []
-    poison_y = []
-    for idx in range(ds_size):
-        if src_lbls[idx] == src and idx in poisoned_indices:
-            src_img = np.transpose(src_imgs[idx], (2, 0, 1))
-            p_img, p_label = attack.poison(src_img, [tgt])
-            poison_x.append(np.transpose(p_img, (1, 2, 0)))
-            poison_y.append(p_label)
-        else:
-            poison_x.append(src_imgs[idx])
-            poison_y.append(src_lbls[idx])
-
-    poison_x, poison_y = np.array(poison_x), np.array(poison_y)
-
-    return poison_x, poison_y
-
-
 class GTSRB(Scenario):
     def _evaluate(self, config: dict) -> dict:
         """
@@ -56,7 +30,6 @@ class GTSRB(Scenario):
         """
 
         model_config = config["model"]
-        # Scenario assumes preprocessing_fn makes images all same size
         classifier, preprocessing_fn = load_model(model_config)
 
         config_adhoc = config.get("adhoc") or {}
@@ -70,53 +43,41 @@ class GTSRB(Scenario):
         use_poison_filtering_defense = config_adhoc.get(
             "use_poison_filtering_defense", True
         )
-        if self.check_run:
-            # filtering defense requires more than a single batch to run properly
-            use_poison_filtering_defense = False
 
         logger.info(f"Loading dataset {config['dataset']['name']}...")
         batch_size = config["dataset"]["batch_size"]
-        train_data = load_dataset(
+        clean_data = load_dataset(
             config["dataset"],
             epochs=1,
             split_type="train",
             preprocessing_fn=preprocessing_fn,
         )
 
+        logger.info(f"Loading poison dataset {config['poison_samples']['name']}...")
+        batch_size = config["poison_samples"]["batch_size"]
+        poison_data = load_dataset(
+            config["poison_samples"],
+            epochs=1,
+            split_type="poison",
+            preprocessing_fn=None,
+        )
+
         logger.info("Building in-memory dataset for poisoning detection and training")
         attack_config = config["attack"]
         attack = load(attack_config)
-        fraction_poisoned = config["adhoc"]["fraction_poisoned"]
         poison_dataset_flag = config["adhoc"]["poison_dataset"]
-        # detect_poison does not currently support data generators
-        #     therefore, make in memory dataset
+	# Concatenate the clean and poisoned samples
         x_train_all, y_train_all = [], []
-        for x_train, y_train in train_data:
-            x_train_all.append(x_train)
-            y_train_all.append(y_train)
+        for x_clean, y_clean in clean_data:
+            x_poison, y_poison = poison_data.get_batch()
+            x_poison = np.array([xp for xp in x_poison], dtype = np.float)
+            x_train_all.append(x_clean)
+            y_train_all.append(y_clean)
+            x_train_all.append(x_poison)
+            y_train_all.append(y_poison)
+        
         x_train_all = np.concatenate(x_train_all, axis=0)
         y_train_all = np.concatenate(y_train_all, axis=0)
-
-        if poison_dataset_flag:
-            total_count = np.bincount(y_train_all)[src_class]
-            poison_count = int(fraction_poisoned * total_count)
-            if poison_count == 0:
-                logger.warning(
-                    f"No poisons generated with fraction_poisoned {fraction_poisoned} for class {src_class}."
-                )
-            src_indices = np.where(y_train_all == src_class)[0]
-            poisoned_indices = np.random.choice(
-                src_indices, size=poison_count, replace=False
-            )
-            x_train_all, y_train_all = poison_dataset(
-                x_train_all,
-                y_train_all,
-                src_class,
-                tgt_class,
-                y_train_all.shape[0],
-                attack,
-                poisoned_indices,
-            )
 
         y_train_all_categorical = to_categorical(y_train_all)
 
@@ -130,7 +91,7 @@ class GTSRB(Scenario):
             classifier_for_defense.fit(
                 x_train_all,
                 y_train_all_categorical,
-                batch_size=batch_size,
+                batch_size=512,
                 nb_epochs=train_epochs,
                 verbose=False,
             )
@@ -152,7 +113,7 @@ class GTSRB(Scenario):
                 "Defense does not require filtering. Model fitting will use all data."
             )
             x_train_final = x_train_all
-            y_train_final = y_train_all
+            y_train_final = y_train_all_categorical
         if len(x_train_final):
             logger.info(
                 f"Fitting model of {model_config['module']}.{model_config['name']}..."
@@ -160,74 +121,40 @@ class GTSRB(Scenario):
             classifier.fit(
                 x_train_final,
                 y_train_final,
-                batch_size=batch_size,
+                batch_size=512,
                 nb_epochs=train_epochs,
                 verbose=False,
             )
         else:
             logger.warning("All data points filtered by defense. Skipping training")
 
-        logger.info("Validating on clean test data")
+        logger.info("Validating on poisoned test data")
         test_data = load_dataset(
-            config["dataset"],
+            config["poison_samples"],
             epochs=1,
-            split_type="test",
-            preprocessing_fn=preprocessing_fn,
+            split_type="poison_test",
+            preprocessing_fn=None,
         )
         validation_metric = metrics.MetricList("categorical_accuracy")
-        target_class_benign_metric = metrics.MetricList("categorical_accuracy")
-        for x, y in tqdm(test_data, desc="Testing"):
-            y_pred = classifier.predict(x)
-            validation_metric.append(y, y_pred)
-            y_pred_tgt_class = y_pred[y == src_class]
-            if len(y_pred_tgt_class):
-                target_class_benign_metric.append(
-                    [src_class] * len(y_pred_tgt_class), y_pred_tgt_class
-                )
-        logger.info(f"Unpoisoned validation accuracy: {validation_metric.mean():.2%}")
-        logger.info(
-            f"Unpoisoned validation accuracy on targeted class: {target_class_benign_metric.mean():.2%}"
-        )
-        results = {
-            "validation_accuracy": validation_metric.mean(),
-            "validation_accuracy_targeted_class": target_class_benign_metric.mean(),
-        }
+        x_test_all, y_test_all = [], []
+        for x_test, y_test in test_data:
+            x_poison_test, y_poison_test = test_data.get_batch()
+            x_poison_test = np.array([xp for xp in x_poison_test], dtype = np.float)
+            x_test_all.append(x_poison_test)
+            y_test_all.append(y_poison_test)
 
-        if poison_dataset_flag:
-            logger.info("Testing on poisoned test data")
-            test_data = load_dataset(
-                config["dataset"],
-                epochs=1,
-                split_type="test",
-                preprocessing_fn=preprocessing_fn,
-            )
-            test_metric = metrics.MetricList("categorical_accuracy")
-            targeted_test_metric = metrics.MetricList("categorical_accuracy")
-            for x_test, y_test in tqdm(test_data, desc="Testing"):
-                src_indices = np.where(y_test == src_class)[0]
-                poisoned_indices = src_indices  # Poison entire class
-                x_test, _ = poison_dataset(
-                    x_test,
-                    y_test,
-                    src_class,
-                    tgt_class,
-                    len(y_test),
-                    attack,
-                    poisoned_indices,
-                )
-                y_pred = classifier.predict(x_test)
-                test_metric.append(y_test, y_pred)
+        x_test_all = np.concatenate(x_test_all, axis=0)
+        y_test_all = np.concatenate(y_test_all, axis=0)
 
-                y_pred_targeted = y_pred[y_test == src_class]
-                if not len(y_pred_targeted):
-                    continue
-                targeted_test_metric.append(
-                    [tgt_class] * len(y_pred_targeted), y_pred_targeted
-                )
-            results["test_accuracy"] = test_metric.mean()
-            results["targeted_misclassification_accuracy"] = targeted_test_metric.mean()
-            logger.info(f"Test accuracy: {test_metric.mean():.2%}")
-            logger.info(
-                f"Test targeted misclassification accuracy: {targeted_test_metric.mean():.2%}"
-            )
+        fraction_poisoned = config["adhoc"]["fraction_poisoned"]
+        num_test_all = x_test_all.shape[0]
+        num_test = round(fraction_poisoned * num_test_all)
+        x_test = x_test_all[0:num_test,:,:,:]
+        y_test = y_test_all[0:num_test]
+        y_test_all_categorical = to_categorical(y_test)
+        y_pred = np.argmax(classifier.predict(x_test), axis=1)
+        validation_metric.append(y_test, y_pred)
+        logger.info(f"poisoned validation accuracy: {validation_metric.mean():.2%}")
+        results = {"validation_accuracy": validation_metric.mean()}
+
         return results
