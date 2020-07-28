df39d2c57422eee8232d7099ffb07c86b5f9b6d6
Kevin Merchant <kevin.merchant@twosixlabs.com>
2020-07-28 17:26:43 -0400

    Adding comment

--- armory/data/pytorch_loader.py
+++ armory/data/pytorch_loader.py
@@ -9,9 +9,11 @@ class TFToTorchGenerator(torch.utils.data.IterableDataset):
     def __iter__(self):
         for ex in self.tf_dataset.take(-1):
             x, y = ex
+            # manually handle adverarial dataset
             if isinstance(x, tuple):
                 x = (torch.from_numpy(x[0].numpy()), torch.from_numpy(x[1].numpy()))
                 y = torch.from_numpy(y.numpy())
+            # non-adversarial dataset
             else:
                 x = torch.from_numpy(x.numpy())
                 y = torch.from_numpy(y.numpy())
