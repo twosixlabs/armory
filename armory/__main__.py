656f11ff61a4bd84c52b6cdd11d44a1b83b6dfa4
Kevin Merchant <kevin.merchant@twosixlabs.com>
2020-09-16 15:23:26 +0000

    Modify existing config file during configuration

--- armory/__main__.py
+++ armory/__main__.py
@@ -438,11 +438,13 @@ def configure(command_args, prog, description):
     if os.path.exists(default_host_paths.armory_config):
         prompt = f"Existing configuration found: {default_host_paths.armory_config}"
         print(prompt)
-    
+
         response = input("Load existing configuration? [Y/n]")
         if response in ("Y", "y", ""):
             print("Loading configuration...")
-            config = load_global_config(default_host_paths.armory_config, validate=False)
+            config = load_global_config(
+                default_host_paths.armory_config, validate=False
+            )
             print("Load successful")
         elif response in ("N", "n"):
             print("Configuration not loaded")
@@ -463,12 +465,32 @@ def configure(command_args, prog, description):
     )
     print(instructions)
 
-    default_dataset_dir = config["dataset_dir"] if config is not None and "dataset_dir" in config.keys() else default_host_paths.dataset_dir
-    default_local_dir = config["local_git_dir"] if config is not None and "local_git_dir" in config.keys() else default_host_paths.local_git_dir
-    default_saved_model_dir = config["saved_model_dir"] if config is not None and "saved_model_dir" in config.keys() else default_host_paths.saved_model_dir
-    default_tmp_dir = config["tmp_dir"] if config is not None and "tmp_dir" in config.keys() else default_host_paths.tmp_dir
-    default_output_dir = config["output_dir"] if config is not None and "output_dir" in config.keys() else default_host_paths.output_dir
-    
+    default_dataset_dir = (
+        config["dataset_dir"]
+        if config is not None and "dataset_dir" in config.keys()
+        else default_host_paths.dataset_dir
+    )
+    default_local_dir = (
+        config["local_git_dir"]
+        if config is not None and "local_git_dir" in config.keys()
+        else default_host_paths.local_git_dir
+    )
+    default_saved_model_dir = (
+        config["saved_model_dir"]
+        if config is not None and "saved_model_dir" in config.keys()
+        else default_host_paths.saved_model_dir
+    )
+    default_tmp_dir = (
+        config["tmp_dir"]
+        if config is not None and "tmp_dir" in config.keys()
+        else default_host_paths.tmp_dir
+    )
+    default_output_dir = (
+        config["output_dir"]
+        if config is not None and "output_dir" in config.keys()
+        else default_host_paths.output_dir
+    )
+
     config = {
         "dataset_dir": _get_path("dataset_dir", default_dataset_dir),
         "local_git_dir": _get_path("local_git_dir", default_local_dir),
