4b109e59d3a7877db5980b442b157c78f9c59f2f
lucas.cadalzo <lucas.cadalzo@twosixlabs.com>
2020-10-13 10:45:44 -0400

    convert xview to floats in [0,1]

--- armory/data/datasets.py
+++ armory/data/datasets.py
@@ -640,7 +640,8 @@ def xview(
 
 class XViewContext:
     def __init__(self):
-        self.default_type = np.uint8
+        self.default_float = np.float32
+        self.quantization = 255
         self.x_dimensions = (
             None,
             None,
@@ -657,11 +658,10 @@ def xview_canonical_preprocessing(batch):
         raise ValueError(
             f"input batch dim {batch.ndim} != {len(xview_context.x_dimensions)}"
         )
-    for dim, (source, target) in enumerate(
-        zip(batch.shape, xview_context.x_dimensions)
-    ):
-        pass
-    assert batch.dtype == xview_context.default_type
+
+    batch = batch.astype(xview_context.default_float) / xview_context.quantization
+
+    assert batch.dtype == xview_context.default_float
     assert batch.shape[1] == batch.shape[2]  # Ensure square shape
     assert batch.shape[3] == xview_context.x_dimensions[3]
 
