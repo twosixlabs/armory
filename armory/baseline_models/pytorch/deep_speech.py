daac9741ac38f653e39f6e2f49bef4a9fc7bb1f8
David Slater <david.slater@twosixlabs.com>
2020-10-15 22:58:14 -0700

    remove unneeded things

--- armory/baseline_models/pytorch/deep_speech.py
+++ armory/baseline_models/pytorch/deep_speech.py
@@ -5,9 +5,6 @@ Model contributed by: MITRE Corporation
 """
 
 from art.estimators.speech_recognition import PyTorchDeepSpeech
-import torch
-
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def get_art_model(model_kwargs, wrapper_kwargs, weights_file=None):
