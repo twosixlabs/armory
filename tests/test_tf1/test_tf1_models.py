455b8c4cde539d854515d75bd243783f64d509c8
lucas.cadalzo <lucas.cadalzo@twosixlabs.com>
2020-10-19 21:53:23 -0400

    added mAP test for apricot

--- tests/test_tf1/test_tf1_models.py
+++ tests/test_tf1/test_tf1_models.py
@@ -3,8 +3,9 @@ from importlib import import_module
 import numpy as np
 import pytest
 
-from armory.data import datasets
+from armory.data import datasets, adversarial_datasets
 from armory import paths
+from armory.utils.metrics import object_detection_AP_per_class
 
 DATASET_DIR = paths.runtime_paths().dataset_dir
 
@@ -32,3 +33,33 @@ def test_tf1_mnist():
         predictions = classifier.predict(x)
         accuracy += np.sum(np.argmax(predictions, axis=1) == y) / len(y)
     assert (accuracy / test_dataset.batches_per_epoch) > 0.9
+
+
+@pytest.mark.usefixtures("ensure_armory_dirs")
+def test_tf1_apricot():
+    detector_module = import_module("armory.baseline_models.tf_graph.mscoco_frcnn")
+    detector_fn = getattr(detector_module, "get_art_model")
+    detector = detector_fn(model_kwargs={}, wrapper_kwargs={})
+
+    test_dataset = adversarial_datasets.apricot_dev_adversarial(
+        split_type="adversarial",
+        epochs=1,
+        batch_size=1,
+        dataset_dir=DATASET_DIR,
+        shuffle_files=False,
+    )
+
+    list_of_ys = []
+    list_of_ypreds = []
+    for x, y in test_dataset:
+        y_pred = detector.predict(x)[0]
+        list_of_ys.append(y)
+        list_of_ypreds.append(y_pred)
+
+    average_precision_by_class = object_detection_AP_per_class(
+        list_of_ys, list_of_ypreds
+    )
+    mAP = np.fromiter(average_precision_by_class.values(), dtype=float).mean()
+    for class_id in [13, 15, 64]:
+        assert average_precision_by_class[class_id] > 0.79
+    assert mAP > 0.08
