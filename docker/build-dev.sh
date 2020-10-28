3518fc5139f4af84412ca6e8032b6bd182ad6a6b
David Slater <david.slater@twosixlabs.com>
2020-10-28 14:45:02 -0700

    updated docker builds

--- docker/build-dev.sh
+++ docker/build-dev.sh
@@ -4,13 +4,13 @@
 
 if [ "$#" -ne 1 ]; then
     echo "Usage: bash docker/build-dev.sh <framework>"
-    echo "    <framework> be \`armory\`, \`tf1\`, \`tf2\`, \`pytorch\`, or \`all\`"
+    echo "    <framework> be \`armory\`, \`tf1\`, \`tf2\`, \`pytorch\`, \`pytorch-deepspeech\`, or \`all\`"
     exit 1
 fi
 
 # Parse framework argument
-if [[ "$1" != "armory" && "$1" != "pytorch" && "$1" != "tf1" && "$1" != "tf2" && "$1" != "all" ]]; then
-    echo "ERROR: <framework> argument must be \`armory\`, \`tf1\`, \`tf2\`, \`pytorch\`, or \`all\`, not \`$1\`"
+if [[ "$1" != "armory" && "$1" != "pytorch" && "$1" != "tf1" && "$1" != "tf2" && "$1" != "pytorch-deepspeech" && "$1" != "all" ]]; then
+    echo "ERROR: <framework> argument must be \`armory\`, \`tf1\`, \`tf2\`, \`pytorch\`, \`pytorch-deepspeech\`, or \`all\`, not \`$1\`"
     exit 1
 fi
 
@@ -20,16 +20,10 @@ version=$(python -m armory --version)
 # Build images
 echo "Building base docker image: armory"
 docker build --force-rm --file docker/Dockerfile --target armory -t twosixarmory/armory:${version} .
-for framework in "tf1" "tf2" "pytorch"; do
+for framework in "tf1" "tf2" "pytorch" "pytorch-deepspeech"; do
     if [[ "$1" == "$framework" || "$1" == "all" ]]; then
         echo "Building docker images for framework: $framework"
         docker build --force-rm --file docker/${framework}/Dockerfile --build-arg armory_version=${version} --target armory-${framework}-base -t twosixarmory/${framework}-base:${version} .
         docker build --force-rm --file docker/${framework}-dev/Dockerfile --build-arg armory_version=${version} --target armory-${framework}-dev -t twosixarmory/${framework}:${version} .
     fi
 done
-
-if [[ "$1" == "pytorch" || "$1" == "all" ]]; then
-    echo "Building docker images for deep speech model"
-    docker build --force-rm --file docker/pytorch-deepspeech/Dockerfile --build-arg armory_version=${version} --target armory-pytorch-deepspeech-base -t twosixarmory/pytorch-deepspeech-base:${version} .
-    docker build --force-rm --file docker/pytorch-deepspeech-dev/Dockerfile --build-arg armory_version=${version} --target armory-pytorch-deepspeech-dev -t twosixarmory/pytorch-deepspeech:${version} .
-fi
