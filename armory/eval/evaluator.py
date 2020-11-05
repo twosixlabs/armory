8322f02441e7d4e80f089dd4d7ef0f798d67a5cd
matt wartell <matt.wartell@twosixlabs.com>
2020-11-05 16:54:30 -0500

    used pinned black version (19.10b0)

--- armory/eval/evaluator.py
+++ armory/eval/evaluator.py
@@ -42,7 +42,9 @@ class Evaluator(object):
         else:
             self.armory_global_config = {"verify_ssl": True}
 
-        date_time = datetime.datetime.utcnow().isoformat(timespec='seconds').replace(":", "")
+        date_time = (
+            datetime.datetime.utcnow().isoformat(timespec="seconds").replace(":", "")
+        )
         output_dir = self.config["sysconfig"].get("output_dir", None)
         eval_id = f"{output_dir}_{date_time}" if output_dir else date_time
 
