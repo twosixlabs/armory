c663c3882a7d83f6e7039b69f5c9ecee7c70cb58
Kevin Merchant <kevin.merchant@twosixlabs.com>
2020-11-17 15:05:48 +0000

    fix clip values

--- armory/baseline_models/pytorch/resnet50.py
+++ armory/baseline_models/pytorch/resnet50.py
@@ -5,7 +5,6 @@ import logging
 from typing import Optional
 
 from art.classifiers import PyTorchClassifier
-import numpy as np
 import torch
 from torchvision import models
 
@@ -13,24 +12,6 @@ from torchvision import models
 logger = logging.getLogger(__name__)
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-IMAGENET_MEANS = [0.485, 0.456, 0.406]
-IMAGENET_STDEV = [0.229, 0.224, 0.225]
-
-
-def preprocessing_fn(img: np.ndarray):
-    """
-    Standardize, then normalize imagenet images
-    """
-    # Standardize images to [0, 1]
-    img /= 255.0
-
-    # Normalize images ImageNet means
-    for i, (mean, std) in enumerate(zip(IMAGENET_MEANS, IMAGENET_STDEV)):
-        img[i] -= mean
-        img[i] /= std
-
-    return img
 
 
 # NOTE: PyTorchClassifier expects numpy input, not torch.Tensor input
@@ -50,21 +31,6 @@ def get_art_model(
         optimizer=torch.optim.Adam(model.parameters(), lr=0.003),
         input_shape=(224, 224, 3),
         **wrapper_kwargs,
-        clip_values=(
-            np.array(
-                [
-                    0.0 - IMAGENET_MEANS[0] / IMAGENET_STDEV[0],
-                    0.0 - IMAGENET_MEANS[1] / IMAGENET_STDEV[1],
-                    0.0 - IMAGENET_MEANS[2] / IMAGENET_STDEV[2],
-                ]
-            ),
-            np.array(
-                [
-                    1.0 - IMAGENET_MEANS[0] / IMAGENET_STDEV[0],
-                    1.0 - IMAGENET_MEANS[1] / IMAGENET_STDEV[1],
-                    1.0 - IMAGENET_MEANS[2] / IMAGENET_STDEV[2],
-                ]
-            ),
-        ),
+        clip_values=(0.0, 1.0)
     )
     return wrapped_model
