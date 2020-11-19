f82c95b79b7f421177d42399d649a7c6d1e6122b
lcadalzo <39925313+lcadalzo@users.noreply.github.com>
2020-11-19 11:47:15 -0500

    cascading attack (#953)
    * added object detection metrics
    
    * added fixed label targeted for object detection
    
    * udpate OD metrics to work with arbitrary batch size, as batch_size=1 is no longer enforced for xView
    
    * update test to handle arbitrary batch size
    
    * also need to update apricot test to no longer assume batch_size=1
    
    * object detection labels are now list of dicts
    
    * update label generation to recieve a list of dicts
    
    * add score
    
    * this works for numpy and torch arrays; ART overrides former to the latter during targeted attack
    
    * added cascading attack
    
    * refactored to be more flexible
    
    * use AutoAttack kwargs
    
    * json formatting

---
+++ armory/art_experimental/attacks/cascading_attack.py
@@ -0,0 +1,25 @@
+from art.attacks.evasion import AutoAttack
+from armory.utils.config_loading import load_attack
+
+
+class CascadingAttack(AutoAttack):
+    def __init__(self, estimator, **kwargs):
+        self._check_kwargs(kwargs)
+        self.targeted = kwargs.get("targeted", False)
+        self.attacks = []
+        for inner_config in kwargs["inner_configs"]:
+            inner_config["kwargs"]["targeted"] = self.targeted
+            self.attacks.append(load_attack(inner_config, estimator))
+        kwargs.pop("inner_configs")
+        super().__init__(estimator=estimator, attacks=self.attacks, **kwargs)
+
+    def _check_kwargs(self, kwargs):
+        if "inner_configs" not in kwargs:
+            raise ValueError("Missing 'inner_configs' key in attack kwargs")
+        if not isinstance(kwargs["inner_configs"], (list, tuple)):
+            raise ValueError("`inner_configs` key must map to a tuple or list")
+        for i, config in enumerate(kwargs["inner_configs"]):
+            if "module" not in config:
+                raise ValueError(f"Missing 'module' key in inner_configs[{i}]")
+            if "name" not in config:
+                raise ValueError(f"Missing 'name' key in inner_configs[{i}]")
