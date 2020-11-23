d891724d157fdb37cd35fcaa737d9c5d6f1f3d55
Yusong Tan <ytan@mitre.org>
2020-11-23 19:01:32 +0000

    performed ucf101 baseline attack using frame saliency and updated docs

--- armory/art_experimental/defences/video_compression_normalized.py
+++ armory/art_experimental/defences/video_compression_normalized.py
@@ -4,23 +4,18 @@ import numpy as np
 
 class VideoCompressionNormalized(VideoCompression):
     """
-    Unnormalize inputs that were normalized during preprocessing,
-    process use ART VideoCompression, and renormalize
+    Convert x from [0,1] to [0, 255] and back, if necessary
     """
 
     def __init__(
         self,
         video_format,
-        clip_values=None,
         constant_rate_factor=28,
         channels_first=False,
         apply_fit=False,
         apply_predict=True,
-        transpose=None,
-        means=None,
-        stds=None,
+        verbose=False,
         dtype=np.float32,
-        same_video=True,
     ):
         super().__init__(
             video_format=video_format,
@@ -28,48 +23,18 @@ class VideoCompressionNormalized(VideoCompression):
             channels_first=channels_first,
             apply_fit=apply_fit,
             apply_predict=apply_predict,
+            verbose=verbose,
         )
-        if clip_values is None:
-            clip_values = [0.0, 255.0]
-        self.clip_values = clip_values
-
-        if transpose is None:
-            transpose = (0, 1, 2, 3, 4)  # identify operation
-        if len(transpose) != 5:
-            raise ValueError("transpose must have 5 dimensions specified")
-        self.transpose = transpose
-
-        if means is None:
-            means = (0.0, 0.0, 0.0)  # identity operation
-        if len(means) != 3:
-            raise ValueError("means must have 3 values, one per channel")
-        self.means = means
-
-        if stds is None:
-            stds = (1.0, 1.0, 1.0)  # identity operation
-        if len(stds) != 3:
-            raise ValueError("stds must have 3 values, one per channel")
-        self.stds = stds
-
         self.dtype = dtype
 
-        self.same_video = same_video
-
     def __call__(self, x, y=None):
-        # transpose to shape NFHWC
-        x = x.transpose(self.transpose)
-        x = x * self.stds + self.means  # use broadcasting
-        np.clip(x, self.clip_values[0], self.clip_values[1], x)
-        # if batches are from same video, then stack them to reduce
-        # number of video compression calls
-        if self.same_video:
-            x_shape = x.shape
-            x = x.reshape((x_shape[0] * x_shape[1], *x_shape[2:]), order="C")
-            x = np.expand_dims(x, axis=0)
-        x, _ = super().__call__(x)
-        if self.same_video:
-            x = x.reshape(x_shape, order="C")
-        x = (x - self.means) / self.stds
+        scale = 1
+        if x.min() >= 0 and x.max() <= 1.0:
+            scale = 255
+
+        x2 = x.copy()
+        x2 *= scale
+        x, _ = super().__call__(x2)
+        x /= scale
         x = x.astype(self.dtype)
-        x = x.transpose(np.argsort(self.transpose))
         return x, y
