bef496e957b06d93ec1221481e03f7b96953e948
Yusong Tan <ytan@mitre.org>
2020-12-03 05:00:00 +0000

    made modifications to support masked PGD attack on ucf101

--- armory/art_experimental/attacks/pgd_patch.py
+++ armory/art_experimental/attacks/pgd_patch.py
@@ -3,6 +3,12 @@ import numpy as np
 
 
 class PGDPatch(ProjectedGradientDescent):
+    """
+    Apply Masked PGD to image and video inputs,
+    where images are assumed to have shape (NHWC)
+    and video are assumed to have shape (NFHWC)
+    """
+
     def __init__(self, estimator, **kwargs):
         super().__init__(estimator=estimator, **kwargs)
         assert (
@@ -10,6 +16,8 @@ class PGDPatch(ProjectedGradientDescent):
         ), "This attack currently only supports channels last format"
 
     def generate(self, x, y=None, **generate_kwargs):
+        video_input = generate_kwargs.get("video_input", False)
+
         if "ymin" in generate_kwargs:
             ymin = generate_kwargs["ymin"]
         else:
@@ -20,19 +28,33 @@ class PGDPatch(ProjectedGradientDescent):
         else:
             raise ValueError("generate_kwargs did not define 'xmin'")
 
-        assert x.ndim == 4, "This attack is designed for images (i.e. 4 dimensions)"
-        channels = generate_kwargs.get("channels", range(x.shape[3]))
+        assert x.ndim in [
+            4,
+            5,
+        ], "This attack is designed for images (4-dim) and videos (5-dim)"
+
+        channels = generate_kwargs.get("channels", range(x.shape[-1]))
 
         mask = np.zeros(shape=x.shape[1:])
         if "patch_ratio" in generate_kwargs:
             patch_ratio = generate_kwargs["patch_ratio"]
-            ymax = ymin + int(x.shape[1] * patch_ratio ** 0.5)
-            xmax = xmin + int(x.shape[2] * patch_ratio ** 0.5)
-            mask[ymin:ymax, xmin:xmax, channels] = 1
+            ymax = ymin + int(x.shape[-3] * patch_ratio ** 0.5)
+            xmax = xmin + int(x.shape[-2] * patch_ratio ** 0.5)
+            if video_input:
+                mask[:, ymin:ymax, xmin:xmax, channels] = 1
+            else:
+                mask[ymin:ymax, xmin:xmax, channels] = 1
         elif "patch_height" in generate_kwargs and "patch_width" in generate_kwargs:
             patch_height = generate_kwargs["patch_height"]
             patch_width = generate_kwargs["patch_width"]
-            mask[ymin : ymin + patch_height, xmin : xmin + patch_width, channels] = 1
+            if video_input:
+                mask[
+                    :, ymin : ymin + patch_height, xmin : xmin + patch_width, channels
+                ] = 1
+            else:
+                mask[
+                    ymin : ymin + patch_height, xmin : xmin + patch_width, channels
+                ] = 1
         else:
             raise ValueError(
                 "generate_kwargs did not define 'patch_ratio', or it did not define 'patch_height' and 'patch_width'"
