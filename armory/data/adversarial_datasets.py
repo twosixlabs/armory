dac44e467104ead0cf1c8bdb96271c101d29821b
lucas.cadalzo <lucas.cadalzo@twosixlabs.com>
2020-12-15 17:57:32 -0500

    return all data if using adversarial split

--- armory/data/adversarial_datasets.py
+++ armory/data/adversarial_datasets.py
@@ -336,6 +336,9 @@ def apricot_dev_adversarial(
         )
         return data
 
+    if split == "adversarial":
+        split = "frcnn+ssd+retinanet"
+
     return datasets._generator_from_tfds(
         "apricot_dev:1.0.2",
         split=split,
