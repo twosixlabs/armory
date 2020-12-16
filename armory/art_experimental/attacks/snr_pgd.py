aef92606c72139fbe4753858f1b1d0b0f59904f1
David Slater <david.slater@twosixlabs.com>
2020-12-15 18:49:17 -0800

    config and attack

--- armory/art_experimental/attacks/snr_pgd.py
+++ armory/art_experimental/attacks/snr_pgd.py
@@ -6,14 +6,14 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class SNR_PGD(ProjectedGradientDescent):
+class SNR_PGD:
     """
     Applies L2 PGD to signal based on an SNR bound defined by norm 'snr' or 'snr_db'.
         This is a *lower* bound on allowable SNR (as opposed to L2 upper bound)
 
     norm - snr or snr_db
     eps - the lower bound on allowable SNR and SNR_DB
-    eps_step - float value in [0, 1] that is the ratio of max L2 distance per step
+    eps_step - float value in (0, 1] that is the ratio of max L2 distance per step
         NOTE: this is different from original PGD because SNR is not additive
 
     SNR measures the original signal power to adversarial perturbation power
@@ -22,11 +22,17 @@ class SNR_PGD(ProjectedGradientDescent):
     SNR_DB = 10 * log10(SNR)
 
     If SNR is set to 0 or SNR_DB to -inf, no limit is provided to PGD
-        If SNR or SNR_DB is set to Inf, this only returns the input
+        If SNR and SNR_DB cannot be set to inf
     """
 
-    def __init__(self, estimator, norm="snr", eps=10, eps_step=0.5, **kwargs):
-        super().__init__(estimator=estimator, norm=2, **kwargs)
+    def __init__(
+        self, estimator, norm="snr", eps=10, eps_step=0.5, batch_size=1, **kwargs
+    ):
+        if batch_size != 1:
+            raise NotImplementedError("only batch_size 1 supported")
+        self._attack = ProjectedGradientDescent(
+            estimator=estimator, norm=2, batch_size=1, **kwargs
+        )
 
         # Map to SNR domain
         eps = float(eps)
@@ -39,24 +45,23 @@ class SNR_PGD(ProjectedGradientDescent):
 
         if snr < 0:
             raise ValueError(f"snr must be nonnegative, not {snr}")
+        elif snr == np.inf:
+            raise ValueError(f"snr must be finite, not {snr}")
         elif snr == 0:
             self.snr_sqrt_reciprocal = np.inf
         else:
             self.snr_sqrt_reciprocal = 1 / np.sqrt(snr)
 
         eps_step = float(eps_step)
-        if not (0 <= eps_step <= 1):
-            raise ValueError(f"eps_step must be in [0, 1], not {eps_step}")
+
+        if not (0 < eps_step <= 1):
+            raise ValueError(f"eps_step must be in (0, 1], not {eps_step}")
         self.step_fraction = eps_step
 
-    def generate(self, x: np.ndarray, y=None, **kwargs) -> np.ndarray:
-        x_l2 = np.linalg.norm(x, ord=2, axis=1)
-
+    def generate(self, x, y=None, **kwargs):
+        x_l2 = np.linalg.norm(x, ord=2)
         eps = x_l2 * self.snr_sqrt_reciprocal
-        if self.snr_sqrt_reciprocal == np.inf and self.step_fraction == 0:
-            eps_step = np.zeros(eps.shape)  # avoid nan if eps
-        else:
-            eps_step = eps * self.step_fraction
-
-        self.set_params(eps=eps, eps_step=eps_step)
-        return super().generate(x, y=y, **kwargs)
+        eps_step = eps * self.step_fraction
+        self._attack.set_params(eps=eps, eps_step=eps_step)
+        print(eps, eps_step, x, y, kwargs)
+        return self._attack.generate(x, y=y, **kwargs)
