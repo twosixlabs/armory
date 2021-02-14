da8a0e0b94e4432d413f73f7e34e00cd0bf6f108
David Slater <david.slater@twosixlabs.com>
2021-02-13 18:18:55 -0800

    update

--- armory/eval/plot.py
+++ armory/eval/plot.py
@@ -195,3 +195,86 @@ class SpeakerID:
             plt.tight_layout()
             plt.savefig(save_filepath, format="pdf")
         plt.close()
+
+
+class SpeakerIDLinf:
+    def __init__(self, json_filepaths=None, names=None):
+        self.xs = []
+        self.ys = []
+        self.names = []
+        if json_filepaths:
+            self.update(json_filepaths, names=names)
+
+    def get_results(self, json_filepath):
+        with open(json_filepath) as f:
+            blob = json.load(f)
+
+        results = blob["results"]
+        adversarial = results["adversarial_categorical_accuracy"]
+        benign = results["benign_categorical_accuracy"]
+        epsilon = results["perturbation_linf"]
+        return benign, adversarial, epsilon
+
+    def add(self, json_filepath, name=None):
+        if name is None:
+            name = os.path.basename(json_filepath)
+        benign, adversarial, epsilon = self.get_results(json_filepath)
+
+        print(min(epsilon), max(epsilon))
+        x, y = get_xy(
+            benign,
+            adversarial,
+            epsilon,
+            ascending_attack=True,
+            min_value=0,
+            max_value=0.06,
+            round_epsilon=False,
+        )
+        print(x, y)
+        self.xs.append(x)
+        self.ys.append(y)
+        self.names.append(name)
+
+    def update(self, json_filepaths, names=None):
+        if names is None:
+            names = [None] * len(json_filepaths)
+
+        for json_filepath, name in zip(json_filepaths, names):
+            self.add(json_filepath, name=name)
+
+    def plot(
+        self,
+        save_filepath=None,
+        show=True,
+        title=r"Speaker ID Classification, L$\infty$ Attack",
+        xlabel=r"Epsilon (L$\infty$)",
+        ylabel="Task Accuracy",
+    ):
+        if save_filepath is not None and show is True:
+            logger.warning("setting show to False")
+            show = False
+
+        for x, y, name in zip(self.xs, self.ys, self.names):
+            x = np.array(list(x))
+            y = np.array(list(y))
+            plt.plot(x, y, "x-", label=name)
+
+        plt.xlabel(xlabel)
+        plt.xticks(ticks=np.arange(0, 0.0201, 0.002))
+        #    ticks=[-3] + list(np.arange(0, 64, 10)) + [64, 67],
+        #    labels=[-3]
+        #    + list(str(x) for x in np.arange(0, 64, 10))
+        #    + [64, r"$\infty$"],
+        #
+        plt.ylabel(ylabel)
+        # plt.yticks(ticks=np.arange(0, 1.01, 0.2))
+        plt.title(title)
+        plt.legend(loc="upper right")
+        plt.axis([0 - 0.001, 0.02 + 0.001, 0 - 0.05, 1 + 0.05])
+
+        if show:
+            plt.show()
+        else:
+            plt.tight_layout()
+            plt.savefig(save_filepath, format="pdf")
+        plt.close()
