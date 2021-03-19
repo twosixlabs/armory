ebb58acc99c80207832a41f6097352780359672c
lucas.cadalzo <lucas.cadalzo@twosixlabs.com>
2021-03-19 17:29:10 -0400

    add masked pgd attack for dapricot

--- armory/art_experimental/attacks/dapricot_patch.py
+++ armory/art_experimental/attacks/dapricot_patch.py
@@ -1,13 +1,14 @@
-from art.attacks.evasion import RobustDPatch
-from armory.art_experimental.utils.dapricot_patch_utils import insert_patch
+
 import numpy as np
+import cv2
+
+from art.attacks.evasion import RobustDPatch, ProjectedGradientDescent
+from armory.art_experimental.utils.dapricot_patch_utils import insert_patch, shape_coords, create_mask
+
 
 
 class DApricotPatch(RobustDPatch):
     """
-    Apply Masked PGD to image and video inputs,
-    where images are assumed to have shape (NHWC)
-    and video are assumed to have shape (NFHWC)
     """
 
     def __init__(self, estimator, **kwargs):
@@ -42,6 +43,7 @@ class DApricotPatch(RobustDPatch):
                 shape = "diamond"  # temporary, having trouble parsing "shape" key
                 cc_gt = y_patch_metadata[i]["cc_ground_truth"]
                 cc_scene = y_patch_metadata[i]["cc_scene"]
+                breakpoint()
 
                 patch = super().generate(np.expand_dims(x[i], axis=0))
 
@@ -63,3 +65,58 @@ class DApricotPatch(RobustDPatch):
             pass  # TODO: threat_model is physical. Generate patch universal across the 3 cameras
 
         return np.array(attacked_images)
+
+
+class DApricotMaskedPGD(ProjectedGradientDescent):
+    """
+    """
+
+    def __init__(self, estimator, **kwargs):
+        super().__init__(estimator=estimator, **kwargs)
+
+    def generate(self, x, y_object=None, y_patch_metadata=None, **generate_kwargs):
+
+        if "threat_model" not in generate_kwargs:
+            raise ValueError(
+                "'threat_model' kwarg must be defined in attack config's"
+                "'generate_kwargs' as one of ('physical', 'digital')"
+            )
+        elif generate_kwargs["threat_model"].lower() not in ("physical", "digital"):
+            raise ValueError(
+                f"'threat_model must be set to one of ('physical', 'digital'), not {generate_kwargs['threat_model']}."
+            )
+
+        else:
+            threat_model = generate_kwargs["threat_model"].lower()
+
+        num_imgs = x.shape[0]
+        attacked_images = []
+
+        from PIL import (
+            Image,
+        )  # temporary so we can view outputs, see bottom of loop below
+
+        if threat_model == "digital":
+            for i in range(num_imgs):
+                gs_coords = y_patch_metadata[i]["gs_coords"]
+                # shape = y_patch_metadata[0]["shape"]
+                shape = "diamond"  # temporary, having trouble parsing "shape" key
+                img_mask = self._compute_image_mask(x[i], y_object[i]["area"], gs_coords, shape)
+                img_with_patch = super().generate(np.expand_dims(x[i], axis=0), mask=img_mask)
+
+                img_with_patch_pil = Image.fromarray(np.uint8(img_with_patch[0] * 255.0))
+                img_with_patch_pil.save(f"image_with_patch_{i}.jpg")
+
+        else:
+            pass  # TODO: threat_model is physical. Generate patch universal across the 3 cameras
+
+        return np.array(attacked_images)
+
+
+    def _compute_image_mask(self, x, gs_area, gs_coords, shape):
+        gs_size = int(np.sqrt(gs_area))
+        patch_coords = shape_coords(gs_size, gs_size, shape)
+        h, status = cv2.findHomography(patch_coords, gs_coords)
+        inscribed_patch_mask = create_mask(shape, gs_size, gs_size)
+        img_mask = cv2.warpPerspective(inscribed_patch_mask, h, (x.shape[1], x.shape[0]), cv2.INTER_CUBIC)
+        return img_mask
