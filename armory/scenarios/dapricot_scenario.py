49d6b05a4257b780cf7b4d912091f1757d3603a4
lucas.cadalzo <lucas.cadalzo@twosixlabs.com>
2021-03-22 19:59:17 -0400

    make scenario code more specific to dapricot threat model

--- armory/scenarios/dapricot_scenario.py
+++ armory/scenarios/dapricot_scenario.py
@@ -34,12 +34,38 @@ class ObjectDetectionTask(Scenario):
         """
         Evaluate the config and return a results dict
         """
+        if skip_attack:
+            raise ValueError("--skip-attack should not be set for D-APRICOT scenario.")
+        if skip_benign:
+            logger.warning(
+                "--skip-benign is being ignored since the D-APRICOT"
+                " scenario doesn't include benign evaluation."
+            )
 
         model_config = config["model"]
         estimator, _ = load_model(model_config)
 
         defense_config = config.get("defense") or {}
         defense_type = defense_config.get("type")
+
+        attack_config = config["attack"]
+        attack_type = attack_config.get("type")
+        if attack_type == "preloaded":
+            raise ValueError(
+                "D-APRICOT scenario should not have preloaded set to True in attack config"
+            )
+        elif "targeted_labels" not in attack_config:
+            raise ValueError(
+                "Attack config must have 'targeted_labels' key, as the "
+                "D-APRICOT threat model is targeted."
+            )
+        elif attack_config.get("use_label"):
+            raise ValueError(
+                "The D-APRICOT scenario threat model is targeted, and"
+                " thus 'use_label' should be set to false."
+            )
+
+        label_targeter = load_label_targeter(attack_config["targeted_labels"])
 
         if defense_type in ["Preprocessor", "Postprocessor"]:
             logger.info(f"Applying internal {defense_type} defense to estimator")
@@ -77,82 +103,23 @@ class ObjectDetectionTask(Scenario):
             defense = load_defense_wrapper(config["defense"], estimator)
             estimator = defense()
 
-        attack_config = config["attack"]
-        attack_type = attack_config.get("type")
-
-        targeted = bool(attack_config.get("kwargs", {}).get("targeted"))
         metrics_logger = metrics.MetricsLogger.from_config(
-            config["metric"],
-            skip_benign=skip_benign,
-            skip_attack=skip_attack,
-            targeted=targeted,
+            config["metric"], skip_benign=True, skip_attack=False, targeted=True,
         )
 
         eval_split = config["dataset"].get("eval_split", "test")
-        if skip_benign:
-            logger.info("Skipping benign classification...")
-        else:
-            # Evaluate the ART estimator on benign test examples
-            logger.info(f"Loading test dataset {config['dataset']['name']}...")
-            test_data = load_dataset(
-                config["dataset"],
-                epochs=1,
-                split=eval_split,
-                num_batches=num_eval_batches,
-                shuffle_files=False,
-            )
-
-            logger.info("Running inference on benign examples...")
-            for x, y in tqdm(test_data, desc="Benign"):
-                # Ensure that input sample isn't overwritten by estimator
-                x.flags.writeable = False
-                with metrics.resource_context(
-                    name="Inference",
-                    profiler=config["metric"].get("profiler_type"),
-                    computational_resource_dict=metrics_logger.computational_resource_dict,
-                ):
-                    if x.shape[0] != 1:
-                        raise ValueError("D-APRICOT batch size must be set to 1")
-                    # (nb=1, num_cameras, h, w, c) --> (num_cameras, h, w, c)
-                    images = x[0]
-                    y_object, y_patch_metadata = y
-                    y_pred = estimator.predict(images)
-                    for img_idx in range(len(y_object)):
-                        y_i_object = y_object[img_idx]
-                        y_i_pred = y_pred[img_idx]
-                        metrics_logger.update_task([y_i_object], [y_i_pred])
-
-            metrics_logger.log_task()
-
-        if skip_attack:
-            logger.info("Skipping attack generation...")
-            return metrics_logger.results()
 
         # Evaluate the ART estimator on adversarial test examples
         logger.info("Generating or loading / testing adversarial examples...")
 
-        if targeted and attack_config.get("use_label"):
-            raise ValueError("Targeted attacks cannot have 'use_label'")
-
-        if attack_type == "preloaded":
-            raise ValueError(
-                "D-APRICOT scenario should not have preloaded set to True in attack config"
-            )
-        else:
-            attack = load_attack(attack_config, estimator)
-            if targeted != getattr(attack, "targeted", False):
-                logger.warning(
-                    f"targeted config {targeted} != attack field {getattr(attack, 'targeted', False)}"
-                )
-            test_data = load_dataset(
-                config["dataset"],
-                epochs=1,
-                split=eval_split,
-                num_batches=num_eval_batches,
-                shuffle_files=False,
-            )
-            if targeted:
-                label_targeter = load_label_targeter(attack_config["targeted_labels"])
+        attack = load_attack(attack_config, estimator)
+        test_data = load_dataset(
+            config["dataset"],
+            epochs=1,
+            split=eval_split,
+            num_batches=num_eval_batches,
+            shuffle_files=False,
+        )
 
         export_samples = config["scenario"].get("export_samples")
         if export_samples is not None and export_samples > 0:
@@ -176,27 +143,25 @@ class ObjectDetectionTask(Scenario):
                 y_object, y_patch_metadata = y
 
                 generate_kwargs = deepcopy(attack_config.get("generate_kwargs", {}))
-                generate_kwargs["y_object"] = y_object
                 generate_kwargs["y_patch_metadata"] = y_patch_metadata
-                if targeted:
-                    # TODO: test targeted attack
-                    y_target = label_targeter.generate(y_object)
-                    generate_kwargs["y_object"] = y_target
+                y_target = label_targeter.generate(y_object)
+                generate_kwargs["y_object"] = y_target
 
                 x_adv = attack.generate(x=images, **generate_kwargs)
 
             # Ensure that input sample isn't overwritten by estimator
             x_adv.flags.writeable = False
             y_pred_adv = estimator.predict(x_adv)
-            metrics_logger.update_task(y_object, y_pred_adv, adversarial=True)
-            if targeted:
+            for img_idx in range(len(y_object)):
+                y_i_target = y_target[img_idx]
+                y_i_pred = y_pred_adv[img_idx]
                 metrics_logger.update_task(
-                    y_object, y_pred_adv, adversarial=True, targeted=True
+                    [y_i_target], [y_i_pred], adversarial=True, targeted=True
                 )
+
             metrics_logger.update_perturbation(x, x_adv)
             if sample_exporter is not None:
                 sample_exporter.export(x, x_adv, y, y_pred_adv)
-        metrics_logger.log_task(adversarial=True)
-        if targeted:
-            metrics_logger.log_task(adversarial=True, targeted=True)
+
+        metrics_logger.log_task(adversarial=True, targeted=True)
         return metrics_logger.results()
