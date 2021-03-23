caaa5f34edf3d73b3d764209b3c0c362928167ec
matt wartell <matt.wartell@twosixlabs.com>
2021-03-23 15:47:24 -0400

    unit test for config merge
    arguments.py created because __main__  is not pytest importable
    (or at least not easily)
    
    additional args like filepath get added to sysconfig, I don't know
    if that's a bad thing

---
+++ armory/arguments.py
@@ -0,0 +1,62 @@
+"""
+Handling of command line arguments and other configuration.
+"""
+
+import argparse
+
+
+def merge_config_and_args(config, args):
+    """
+    Override members of config if specified as args. The config dict is mutated.
+    Members in config are percolated into args to act as if they were specified.
+    Members of args that are not in config are put there so that the output
+    accurately records what was run. Returns a modified config and a newly
+    created args.
+    The precedence becomes defaults < config block < command args.
+    """
+
+    sysconfig = config["sysconfig"]
+
+    # TODO: this list will get out of sync as options are added; find a way
+    # to read them out of the argparse parser
+    names = (
+        "interactive jupyter port no_docker root output_dir "
+        "output_filename check num_eval_batches skip_benign skip_attack "
+        "skip_misclassified validate_config"
+    ).split()
+
+    # the truth table is complicated because they are actually tri-states:
+    # undef, defined but falsy, defined and truthy
+    #
+    # config    args    action
+    # u         u       nothing
+    # f         u       nothing
+    # d         u       args <- config
+    # u         f       nothing
+    # f         f       nothing
+    # d         f       args <- config
+    # u         d       config <- args
+    # f         d       config <- args
+    # d         d       config <- args
+
+    # find truthy config specifications
+    new_spec = {}
+    for name in sysconfig:
+        if sysconfig[name]:
+            new_spec[name] = sysconfig[name]
+
+    # find truthy args specifications, overwriting config if present
+    specified = vars(args)
+    for name in specified:
+        if specified[name]:
+            new_spec[name] = specified[name]
+
+    # sysconfig gets updated with prioritized union
+    sysconfig.update(new_spec)
+
+    # new_args now gets the original namespace and all truthy members of the prioritized
+    # union
+    specified.update(new_spec)
+    new_args = argparse.Namespace(**specified)
+
+    return config, new_args
