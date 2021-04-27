6ab5fcad0c795ac4a00e69e40bad2ee6549dc1f8
lucas.cadalzo <lucas.cadalzo@twosixlabs.com>
2021-04-27 16:04:55 -0400

    formatting

--- armory/utils/metrics.py
+++ armory/utils/metrics.py
@@ -1037,12 +1037,15 @@ def dapricot_patch_target_success(y, y_pred, iou_threshold=0.1, conf_threshold=0
 
 
 def _dapricot_patch_target_success(y, y_pred, iou_threshold=0.1, conf_threshold=0.5):
-    target_label = int(y['labels'])
-    target_box = y['boxes']
-    pred_indices = np.where(y_pred['scores'] > conf_threshold)[0]
+    target_label = int(y["labels"])
+    target_box = y["boxes"]
+    pred_indices = np.where(y_pred["scores"] > conf_threshold)[0]
     for pred_idx in pred_indices:
-        if y_pred['labels'][pred_idx] == target_label:
-            if _intersection_over_union(y_pred['boxes'][pred_idx], target_box) > iou_threshold:
+        if y_pred["labels"][pred_idx] == target_label:
+            if (
+                _intersection_over_union(y_pred["boxes"][pred_idx], target_box)
+                > iou_threshold
+            ):
                 return 1
     return 0
 
