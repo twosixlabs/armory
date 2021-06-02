25fed277cfce94a7afa63a013ce98c9a17bfdb98
Neal Gupta <neal.gupta@twosixlabs.com>
2021-06-02 20:47:29 +0000

    Update template to include all parameters (except indexing params)

--- armory/data/template_boilerplate.py
+++ armory/data/template_boilerplate.py
@@ -5,10 +5,18 @@ def {name}(
     batch_size: int = 1,
     dataset_dir: str = None,
     preprocessing_fn: Callable = {name}_canonical_preprocessing,
-    fit_preprocessing_fn: Callable = None,
+    label_preprocessing_fn: Callable = None,
+    as_supervised: bool = True,
+    supervised_xy_keys=None,  # May need to update value
+    download_and_prepare_kwargs=None,  # May need to update value
+    variable_y=False,  # May need to update value
+    lambda_map: Callable = None,  # May need to update value
+    fit_preprocessing_fn: Callable = None,  # May need to update value
     cache_dataset: bool = True,
     framework: str = "numpy",
     shuffle_files: bool = True,
+    class_ids=None,  # May need to update value
+    index=None,  # May need to update value
     **kwargs,
 ) -> ArmoryDataGenerator:
     preprocessing_fn = preprocessing_chain(preprocessing_fn, fit_preprocessing_fn)
@@ -20,6 +28,13 @@ def {name}(
         epochs=epochs,
         dataset_dir=dataset_dir,
         preprocessing_fn=preprocessing_fn,
+        label_preprocessing_fn=label_preprocessing_fn,
+        as_supervised=as_supervised,
+        supervised_xy_keys=supervised_xy_keys,
+        download_and_prepare_kwargs=download_and_prepare_kwargs,
+        variable_length=bool(batch_size > 1),
+        variable_y=variable_y,
+        lambda_map=lambda_map,
         cache_dataset=cache_dataset,
         framework=framework,
         shuffle_files=shuffle_files,
