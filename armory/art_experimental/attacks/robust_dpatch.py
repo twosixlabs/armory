92700aaef2f523ae9e5c7a555e1a230c810fcf7f
lcadalzo <39925313+lcadalzo@users.noreply.github.com>
2021-07-12 18:44:33 -0400

    updating scenario configs for eval 3 (#1123)
    * handling merge conflict
    
    * bump version 0.13.3 --> 0.13.4
    
    * log that random patch location is being used
    
    Co-authored-by: Yusong Tan <ytan@mitre.org>

---
+++ armory/art_experimental/attacks/robust_dpatch.py
@@ -0,0 +1,31 @@
+import logging
+
+from art.attacks.evasion import RobustDPatch
+import numpy as np
+
+
+logger = logging.getLogger(__name__)
+
+
+class RobustDPatch(RobustDPatch):
+    """
+    Generate and apply patch
+    """
+
+    def __init__(self, estimator, **kwargs):
+        # allows for random patch location
+        if "patch_location" not in kwargs:
+            self.random_location = True
+        else:
+            self.random_location = False
+        super().__init__(estimator=estimator, **kwargs)
+
+    def generate(self, x, y=None, **generate_kwargs):
+        if self.random_location:
+            logger.info("Selecting random coordinates for patch_location.")
+            self.patch_location = (
+                np.random.randint(int(x.shape[-3] - self.patch_shape[0])),
+                np.random.randint(int(x.shape[-2] - self.patch_shape[1])),
+            )
+        super().generate(x, y=y, **generate_kwargs)
+        return super().apply_patch(x)
