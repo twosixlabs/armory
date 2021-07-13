15622be3fc1015902bf09a7b6625df569926b259
David Slater <david.slater@twosixlabs.com>
2021-07-13 14:29:38 -0700

    updated kwargs and moved assert -> valueerror

--- armory/art_experimental/attacks/video_frame_border.py
+++ armory/art_experimental/attacks/video_frame_border.py
@@ -12,34 +12,31 @@ class FrameBorderPatch(ProjectedGradientDescent):
     def __init__(self, estimator, **kwargs):
         super().__init__(estimator=estimator, **kwargs)
 
-    def generate(self, x, y=None, **generate_kwargs):
-        assert x.ndim == 5, "This attack is designed for videos (5-dim)"
-
+    def generate(self, x, y=None, patch_ratio=None, **kwargs):
+        if patch_ratio is None:
+            raise ValueError("generate_kwargs did not define 'patch_ratio'")
+        if x.ndim != 5:
+            raise ValueError("This attack is designed for videos (5-dim)")
         width = x.shape[3]
         height = x.shape[2]
 
-        if "patch_ratio" in generate_kwargs:
-            patch_ratio = generate_kwargs["patch_ratio"]
-            t1 = (
-                2 * (width + height)
-                + (4 * (width + height) ** 2 - 16 * (patch_ratio * width * height))
-                ** 0.5
-            ) / 8
-            t2 = (
-                2 * (width + height)
-                - (4 * (width + height) ** 2 - 16 * (patch_ratio * width * height))
-                ** 0.5
-            ) / 8
-            thickness = int(min(t1, t2))
+        t1 = (
+            2 * (width + height)
+            + (4 * (width + height) ** 2 - 16 * (patch_ratio * width * height)) ** 0.5
+        ) / 8
+        t2 = (
+            2 * (width + height)
+            - (4 * (width + height) ** 2 - 16 * (patch_ratio * width * height)) ** 0.5
+        ) / 8
+        thickness = int(min(t1, t2))
 
-            assert (width - 2 * thickness) * (height - 2 * thickness) >= (
-                1 - patch_ratio
-            ) * width * height
-        else:
-            raise ValueError("generate_kwargs did not define 'patch_ratio'")
+        if (width - 2 * thickness) * (height - 2 * thickness) < (
+            1 - patch_ratio
+        ) * width * height:
+            raise ValueError("patch_ratio does not match height and width")
 
         mask = np.ones(shape=x.shape[1:], dtype=np.float32)
 
         mask[:, thickness : height - thickness, thickness : width - thickness, :] = 0.0
 
-        return super().generate(x, y=y, mask=mask)
+        return super().generate(x, y=y, mask=mask, **kwargs)
