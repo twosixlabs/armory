f3071e58395f7192835cec7ac8db5da51a359209
lucas.cadalzo <lucas.cadalzo@twosixlabs.com>
2021-10-15 08:47:02 -0400

    forgot to add these files to previous commit

--- armory/data/adversarial/carla_video_tracking_dev.py
+++ armory/data/adversarial/carla_video_tracking_dev.py
@@ -23,7 +23,7 @@ _CITATION = """
 }
 """
 
-_URLS = "carla_video_tracking_dev.tar.gz"
+_URLS = "https://armory-public-data.s3.us-east-2.amazonaws.com/carla/carla_video_tracking_dev.tar.gz"
 
 
 class CarlaVideoTrackingDev(tfds.core.GeneratorBasedBuilder):
