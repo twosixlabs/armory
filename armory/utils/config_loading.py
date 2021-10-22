88cc45016bab8e0dead9287dc153cecefe19f5c4
lucas.cadalzo <lucas.cadalzo@twosixlabs.com>
2021-10-22 13:21:25 -0400

    reverting things to 7c14ff8

--- armory/utils/config_loading.py
+++ armory/utils/config_loading.py
@@ -51,21 +51,23 @@ def load_fn(sub_config):
 def load_dataset(dataset_config, *args, num_batches=None, **kwargs):
     """
     Loads a dataset from configuration file
-
     If num_batches is None, this function will return a generator that iterates
     over the entire dataset.
     """
-    module = dataset_config.pop("module")
-    dataset_fn_name = dataset_config.pop("name")
-    batch_size = dataset_config.pop("batch_size", 1)
-    framework = dataset_config.pop("framework", "numpy")
-    dataset_module = import_module(module)
-    dataset_fn = getattr(dataset_module, dataset_fn_name)
+    dataset_module = import_module(dataset_config["module"])
+    dataset_fn = getattr(dataset_module, dataset_config["name"])
+    dataset_kwargs = dataset_config.get("kwargs", None)
 
-    for remaining_kwarg in dataset_config:
-        if remaining_kwarg == "eval_split":
-            continue
-        kwargs[remaining_kwarg] = dataset_config[remaining_kwarg]
+    batch_size = dataset_config["batch_size"]
+    framework = dataset_config.get("framework", "numpy")
+
+    if dataset_kwargs is not None:
+        for kwarg in dataset_kwargs:
+            kwargs[kwarg] = dataset_kwargs[kwarg]
+
+    for ds_kwarg in ["index", "class_ids"]:
+        if ds_kwarg not in kwargs and ds_kwarg in dataset_config:
+            kwargs[ds_kwarg] = dataset_config[ds_kwarg]
 
     dataset = dataset_fn(batch_size=batch_size, framework=framework, *args, **kwargs)
     if not isinstance(dataset, ArmoryDataGenerator):
