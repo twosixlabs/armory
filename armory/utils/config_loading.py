e122dd50ff00387ee85ae7e9d8e2f371c3824ddc
swsuggs <jsmitherson2@gmail.com>
2021-10-22 18:03:11 -0600

    Refactor dataset config loading (#1194)
    * refactor dataset config loading
    
    * update carla configs for new dataset config loading
    
    * refactor how check_run is passed through, so it doesnt get passed to the tfds ds function
    
    * formatting
    
    Co-authored-by: Sterling Suggs <sterling.suggs@twosixtech.com>
    Co-authored-by: lucas.cadalzo <lucas.cadalzo@twosixlabs.com>

--- armory/utils/config_loading.py
+++ armory/utils/config_loading.py
@@ -31,6 +31,7 @@ from armory.art_experimental.attacks.sweep import SweepAttack
 from armory.data.datasets import ArmoryDataGenerator, EvalGenerator
 from armory.data.utils import maybe_download_weights_from_s3
 from armory.utils import labels
+import copy
 
 
 def load(sub_config):
@@ -48,31 +49,32 @@ def load_fn(sub_config):
     return getattr(module, sub_config["name"])
 
 
-def load_dataset(dataset_config, *args, num_batches=None, **kwargs):
+def load_dataset(dataset_config, *args, num_batches=None, check_run=False, **kwargs):
     """
     Loads a dataset from configuration file
     If num_batches is None, this function will return a generator that iterates
     over the entire dataset.
     """
-    dataset_module = import_module(dataset_config["module"])
-    dataset_fn = getattr(dataset_module, dataset_config["name"])
-    dataset_kwargs = dataset_config.get("kwargs", None)
+    dataset_config = copy.deepcopy(
+        dataset_config
+    )  # Avoid modifying original dictionary
+    module = dataset_config.pop("module")
+    dataset_fn_name = dataset_config.pop("name")
+    batch_size = dataset_config.pop("batch_size", 1)
+    framework = dataset_config.pop("framework", "numpy")
+    dataset_module = import_module(module)
+    dataset_fn = getattr(dataset_module, dataset_fn_name)
 
-    batch_size = dataset_config["batch_size"]
-    framework = dataset_config.get("framework", "numpy")
-
-    if dataset_kwargs is not None:
-        for kwarg in dataset_kwargs:
-            kwargs[kwarg] = dataset_kwargs[kwarg]
-
-    for ds_kwarg in ["index", "class_ids"]:
-        if ds_kwarg not in kwargs and ds_kwarg in dataset_config:
-            kwargs[ds_kwarg] = dataset_config[ds_kwarg]
+    # Add remaining dataset_config items to kwargs
+    for remaining_kwarg in dataset_config:
+        if remaining_kwarg == "eval_split":
+            continue
+        kwargs[remaining_kwarg] = dataset_config[remaining_kwarg]
 
     dataset = dataset_fn(batch_size=batch_size, framework=framework, *args, **kwargs)
     if not isinstance(dataset, ArmoryDataGenerator):
         raise ValueError(f"{dataset} is not an instance of {ArmoryDataGenerator}")
-    if dataset_config.get("check_run"):
+    if check_run:
         return EvalGenerator(dataset, num_eval_batches=1)
     if num_batches:
         return EvalGenerator(dataset, num_eval_batches=num_batches)
@@ -176,7 +178,7 @@ def load_attack(attack_config, classifier):
     return attack
 
 
-def load_adversarial_dataset(config, num_batches=None, **kwargs):
+def load_adversarial_dataset(config, num_batches=None, check_run=False, **kwargs):
     if config.get("type") != "preloaded":
         raise ValueError(f"attack type must be 'preloaded', not {config.get('type')}")
     dataset_module = import_module(config["module"])
@@ -188,7 +190,7 @@ def load_adversarial_dataset(config, num_batches=None, **kwargs):
     dataset = dataset_fn(**dataset_kwargs)
     if not isinstance(dataset, ArmoryDataGenerator):
         raise ValueError(f"{dataset} is not an instance of {ArmoryDataGenerator}")
-    if config.get("check_run"):
+    if check_run:
         return EvalGenerator(dataset, num_eval_batches=1)
     if num_batches:
         return EvalGenerator(dataset, num_eval_batches=num_batches)
