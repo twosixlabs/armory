5462b5a69fed4c26a90f6929e15b12fb641a76d2
Seth Henshaw <seth.henshaw@twosixtech.com>
2022-02-22 16:40:31 -0500

    Test refactor complete...first attempt at getting CI to work

--- tests/test_models.py
+++ tests/test_models.py
@@ -461,9 +461,305 @@ def test_carla_od(
     assert [ap_per_class[i] > 0.35 for i in range(1, 4)]
 
 
-# TODO:  Still need to add the following
-#  - test_tf1/test_keras_models.py - These complain about tf `eager` execution
-#  - test_tf1/test_tf1_models.py
+# TODO: Just moving tests/test_tf1/test_keras_models.py to below...
+#  Still need to clean up
+@pytest.mark.usefixtures("ensure_armory_dirs")
+@pytest.mark.keras
+@pytest.mark.slow
+def test_keras_mnist(armory_dataset_dir):
+    from armory.data import datasets
+
+    classifier_module = import_module("armory.baseline_models.keras.mnist")
+    classifier_fn = getattr(classifier_module, "get_art_model")
+    classifier = classifier_fn(model_kwargs={}, wrapper_kwargs={})
+
+    train_dataset = datasets.mnist(
+        split="train", epochs=1, batch_size=600, dataset_dir=armory_dataset_dir,
+    )
+    test_dataset = datasets.mnist(
+        split="test", epochs=1, batch_size=100, dataset_dir=armory_dataset_dir,
+    )
+
+    classifier.fit_generator(
+        train_dataset, nb_epochs=1,
+    )
+
+    accuracy = 0
+    for _ in range(test_dataset.batches_per_epoch):
+        x, y = test_dataset.get_batch()
+        predictions = classifier.predict(x)
+        accuracy += np.sum(np.argmax(predictions, axis=1) == y) / len(y)
+    assert (accuracy / test_dataset.batches_per_epoch) > 0.9
+
+
+@pytest.mark.usefixtures("ensure_armory_dirs")
+@pytest.mark.keras
+@pytest.mark.slow
+def test_keras_mnist_pretrained(armory_dataset_dir):
+    from armory.data import datasets
+
+    classifier_module = import_module("armory.baseline_models.keras.mnist")
+    classifier_fn = getattr(classifier_module, "get_art_model")
+    weights_path = maybe_download_weights_from_s3("undefended_mnist_5epochs.h5")
+    classifier = classifier_fn(
+        model_kwargs={}, wrapper_kwargs={}, weights_path=weights_path
+    )
+
+    test_dataset = datasets.mnist(
+        split="test", epochs=1, batch_size=100, dataset_dir=armory_dataset_dir,
+    )
+
+    accuracy = 0
+    for _ in range(test_dataset.batches_per_epoch):
+        x, y = test_dataset.get_batch()
+        predictions = classifier.predict(x)
+        accuracy += np.sum(np.argmax(predictions, axis=1) == y) / len(y)
+    assert (accuracy / test_dataset.batches_per_epoch) > 0.98
+
+
+@pytest.mark.usefixtures("ensure_armory_dirs")
+@pytest.mark.keras
+@pytest.mark.slow
+def test_keras_cifar(armory_dataset_dir):
+    from armory.data import datasets
+
+    classifier_module = import_module("armory.baseline_models.keras.cifar")
+    classifier_fn = getattr(classifier_module, "get_art_model")
+    classifier = classifier_fn(model_kwargs={}, wrapper_kwargs={})
+
+    train_dataset = datasets.cifar10(
+        split="train", epochs=1, batch_size=500, dataset_dir=armory_dataset_dir,
+    )
+    test_dataset = datasets.cifar10(
+        split="test", epochs=1, batch_size=100, dataset_dir=armory_dataset_dir,
+    )
+
+    classifier.fit_generator(
+        train_dataset, nb_epochs=1,
+    )
+
+    accuracy = 0
+    for _ in range(test_dataset.batches_per_epoch):
+        x, y = test_dataset.get_batch()
+        predictions = classifier.predict(x)
+        accuracy += np.sum(np.argmax(predictions, axis=1) == y) / len(y)
+    assert (accuracy / test_dataset.batches_per_epoch) > 0.27
+
+
+@pytest.mark.usefixtures("ensure_armory_dirs")
+@pytest.mark.keras
+@pytest.mark.slow
+def test_keras_imagenet(armory_dataset_dir):
+    from armory.data import adversarial_datasets
+
+    classifier_module = import_module("armory.baseline_models.keras.resnet50")
+    classifier_fn = getattr(classifier_module, "get_art_model")
+    weights_path = maybe_download_weights_from_s3("resnet50_imagenet_v1.h5")
+    classifier = classifier_fn(
+        model_kwargs={}, wrapper_kwargs={}, weights_path=weights_path
+    )
+
+    dataset = adversarial_datasets.imagenet_adversarial(
+        split="adversarial", epochs=1, batch_size=100, dataset_dir=armory_dataset_dir,
+    )
+
+    accuracy_clean = 0
+    accuracy_adv = 0
+    for _ in range(dataset.batches_per_epoch):
+        (x_clean, x_adv), y = dataset.get_batch()
+        predictions_clean = classifier.predict(x_clean)
+        accuracy_clean += np.sum(np.argmax(predictions_clean, axis=1) == y) / len(y)
+        predictions_adv = classifier.predict(x_adv)
+        accuracy_adv += np.sum(np.argmax(predictions_adv, axis=1) == y) / len(y)
+    assert (accuracy_clean / dataset.batches_per_epoch) > 0.65
+    assert (accuracy_adv / dataset.batches_per_epoch) < 0.02
+
+
+@pytest.mark.usefixtures("ensure_armory_dirs")
+@pytest.mark.keras
+@pytest.mark.slow
+def test_keras_imagenet_transfer(armory_dataset_dir):
+    from armory.data import adversarial_datasets
+
+    classifier_module = import_module(
+        "armory.baseline_models.keras.inception_resnet_v2"
+    )
+    classifier_fn = getattr(classifier_module, "get_art_model")
+    weights_path = maybe_download_weights_from_s3("inceptionresnetv2_imagenet_v1.h5")
+    classifier = classifier_fn(
+        model_kwargs={}, wrapper_kwargs={}, weights_path=weights_path
+    )
+
+    dataset = adversarial_datasets.imagenet_adversarial(
+        split="adversarial", epochs=1, batch_size=100, dataset_dir=armory_dataset_dir,
+    )
+    accuracy_clean = 0
+    accuracy_adv = 0
+    for _ in range(dataset.batches_per_epoch):
+        (x_clean, x_adv), y = dataset.get_batch()
+        predictions_clean = classifier.predict(x_clean)
+        accuracy_clean += np.sum(np.argmax(predictions_clean, axis=1) == y) / len(y)
+        predictions_adv = classifier.predict(x_adv)
+        accuracy_adv += np.sum(np.argmax(predictions_adv, axis=1) == y) / len(y)
+
+    assert (accuracy_clean / dataset.batches_per_epoch) > 0.74
+    assert (accuracy_adv / dataset.batches_per_epoch) < 0.73
+
+
+# TODO: Just moving tests/test_tf1/test_tf1_models.py to below...
+#  Still need to clean up
+@pytest.mark.tf1
+def test_tf1_mnist(armory_dataset_dir):
+    msg = "This test is no longer valid as we are no longer supporting TF1. "
+    msg += "ART does not support tf2 compat.v1 mode...ticket being sent to art"
+    pytest.skip(msg)
+    from armory.data import datasets
+
+    classifier_module = import_module("armory.baseline_models.tf_graph.mnist")
+    classifier_fn = getattr(classifier_module, "get_art_model")
+    classifier = classifier_fn(model_kwargs={}, wrapper_kwargs={})
+
+    train_dataset = datasets.mnist(
+        split="train", epochs=1, batch_size=600, dataset_dir=armory_dataset_dir,
+    )
+    test_dataset = datasets.mnist(
+        split="test", epochs=1, batch_size=100, dataset_dir=armory_dataset_dir,
+    )
+
+    classifier.fit_generator(
+        train_dataset, nb_epochs=1,
+    )
+
+    accuracy = 0
+    for _ in range(test_dataset.batches_per_epoch):
+        x, y = test_dataset.get_batch()
+        predictions = classifier.predict(x)
+        accuracy += np.sum(np.argmax(predictions, axis=1) == y) / len(y)
+    assert (accuracy / test_dataset.batches_per_epoch) > 0.9
+
+
+@pytest.mark.tf1
+def test_tf1_coco(armory_dataset_dir):
+    import os
+    from armory.data import datasets
+    from armory.utils.metrics import object_detection_AP_per_class
+
+    if not os.path.exists(os.path.join(armory_dataset_dir, "coco", "2017", "1.1.0")):
+        pytest.skip("coco2017 dataset not downloaded.")
+
+    detector_module = import_module("armory.baseline_models.tf_graph.mscoco_frcnn")
+    detector_fn = getattr(detector_module, "get_art_model")
+    detector = detector_fn(model_kwargs={}, wrapper_kwargs={})
+
+    NUM_TEST_SAMPLES = 10
+    dataset = datasets.coco2017(split="validation", shuffle_files=False)
+
+    list_of_ys = []
+    list_of_ypreds = []
+    for _ in range(NUM_TEST_SAMPLES):
+        x, y = dataset.get_batch()
+        y_pred = detector.predict(x)
+        list_of_ys.extend(y)
+        list_of_ypreds.extend(y_pred)
+
+    average_precision_by_class = object_detection_AP_per_class(
+        list_of_ys, list_of_ypreds
+    )
+    mAP = np.fromiter(average_precision_by_class.values(), dtype=float).mean()
+    for class_id in [0, 2, 5, 9, 10]:
+        assert average_precision_by_class[class_id] > 0.6
+    assert mAP > 0.1
+
+
+@pytest.mark.tf1
+def test_tf1_apricot(armory_dataset_dir):
+    import os
+    from armory.utils.metrics import (
+        object_detection_AP_per_class,
+        apricot_patch_targeted_AP_per_class,
+    )
+
+    if not os.path.isdir(os.path.join(armory_dataset_dir, "apricot_dev", "1.0.1")):
+        pytest.skip("apricot dataset not locally available.")
+    from armory.data import adversarial_datasets
+
+    detector_module = import_module("armory.baseline_models.tf_graph.mscoco_frcnn")
+    detector_fn = getattr(detector_module, "get_art_model")
+    detector = detector_fn(model_kwargs={}, wrapper_kwargs={})
+
+    dev_dataset = adversarial_datasets.apricot_dev_adversarial(
+        split="frcnn+ssd+retinanet",
+        epochs=1,
+        batch_size=1,
+        dataset_dir=armory_dataset_dir,
+        shuffle_files=False,
+    )
+
+    list_of_ys = []
+    list_of_ypreds = []
+    for x, y in dev_dataset:
+        y_pred = detector.predict(x)
+        list_of_ys.extend(y)
+        list_of_ypreds.extend(y_pred)
+
+    average_precision_by_class = object_detection_AP_per_class(
+        list_of_ys, list_of_ypreds
+    )
+    mAP = np.fromiter(average_precision_by_class.values(), dtype=float).mean()
+    for class_id in [13, 15, 64]:
+        assert average_precision_by_class[class_id] > 0.79
+    assert mAP > 0.08
+
+    patch_targeted_AP_by_class = apricot_patch_targeted_AP_per_class(
+        list_of_ys, list_of_ypreds
+    )
+    expected_patch_targeted_AP_by_class = {
+        1: 0.18,
+        17: 0.18,
+        27: 0.27,
+        33: 0.55,
+        44: 0.14,
+    }
+    for class_id, expected_AP in expected_patch_targeted_AP_by_class.items():
+        assert np.abs(patch_targeted_AP_by_class[class_id] - expected_AP) < 0.03
+
+    test_dataset = adversarial_datasets.apricot_test_adversarial(
+        split="frcnn",
+        epochs=1,
+        batch_size=1,
+        dataset_dir=armory_dataset_dir,
+        shuffle_files=False,
+    )
+
+    list_of_ys = []
+    list_of_ypreds = []
+    for x, y in test_dataset:
+        y_pred = detector.predict(x)
+        list_of_ys.extend(y)
+        list_of_ypreds.extend(y_pred)
+
+    average_precision_by_class = object_detection_AP_per_class(
+        list_of_ys, list_of_ypreds
+    )
+    mAP = np.fromiter(average_precision_by_class.values(), dtype=float).mean()
+    for class_id in [2, 3, 4, 6, 15, 72, 76]:
+        assert average_precision_by_class[class_id] > 0.3
+    assert mAP > 0.08
+
+    patch_targeted_AP_by_class = apricot_patch_targeted_AP_per_class(
+        list_of_ys, list_of_ypreds
+    )
+    expected_patch_targeted_AP_by_class = {
+        1: 0.22,
+        17: 0.18,
+        27: 0.4,
+        44: 0.09,
+        53: 0.27,
+        85: 0.43,
+    }
+    for class_id, expected_AP in expected_patch_targeted_AP_by_class.items():
+        assert np.abs(patch_targeted_AP_by_class[class_id] - expected_AP) < 0.03
+
 
 # TODO: Think about how to mark these appropriately so they can be split in CI
 
