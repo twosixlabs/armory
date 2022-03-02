6d44e0c634ee71ad36af3eeabc8100f76ef0ca8d
matt wartell <matt.wartell@twosixlabs.com>
2022-03-02 15:57:55 -0500

    fix a flake8 report                                 and re-add my git commit hook

--- tests/unit/test_cuda.py
+++ tests/unit/test_cuda.py
@@ -1,6 +1,7 @@
 import torch
 import pytest
 
+
 # this test is marked xfail because I don't care if it is present or not, I just want to know
 # in the pytest summary this appears as X when present and lower-case x when not
 @pytest.mark.xfail
