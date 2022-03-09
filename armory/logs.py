770642350bffe9e1d67da70b0cd9aa611dc52d06
matt wartell <matt.wartell@twosixlabs.com>
2022-03-09 16:24:02 -0500

    change base level to warning

--- armory/logs.py
+++ armory/logs.py
@@ -32,7 +32,7 @@ log = loguru.logger
 log.level("PROGRESS", no=15, color="<blue>", icon="\N{downwards quadruple arrow}")
 
 default_message_filters = {
-    "": "TRACE",
+    "": "WARNING",
     "armory": "INFO",
     "art": "INFO",
     "docker": "INFO",
