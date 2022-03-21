4204d84e02e08ac5e063f606765193d5c12a14cb
David Slater <david.slater@twosixlabs.com>
2022-03-21 15:18:01 -0700

    update images

--- armory/docker/images.py
+++ armory/docker/images.py
@@ -67,6 +67,8 @@ def split_name(image_name: str):
     else:
         repo = ""
         name = repo_name
+    if not name:
+        raise ValueError(f"invalid docker image image_name {image_name}. No name")
 
     return repo, name, tag
 
@@ -88,13 +90,53 @@ def join_name(repo: str, name: str, tag: str):
 
 def is_armory(image_name: str):
     """
-    Takes a docker image name, returns False if not a armory image
-        If an armory image, it will return `twosixarmory/<framework>:tag`
+    Return whether image_name refers to an armory docker image
     """
     repo, name, _ = split_name(image_name)
     if repo and repo != DOCKER_REPOSITORY:
         return False
     return name in IMAGE_MAP
+
+
+def get_armory_name(image_name: str):
+    """
+    Takes a docker image name, returns False if not a armory image
+        If an armory image, it will return `twosixarmory/<framework>:tag`
+    """
+    if not is_armory(image_name):
+        raise ValueError(f"Not an armory image name: {image_name}")
+    repo, name, tag = split_name(image_name)
+    repo = "twosixarmory"
+    if tag:  # tag is explicitly defined, use it
+        return join_name(repo, name, tag)
+    return IMAGE_MAP[name]
+
+
+def last_armory_release(image_name: str):
+    """
+    Return the image_name corresponding to the last armory major.minor.patch release
+        If the current image_name is a release, return the current
+    """
+    repo, name, tag = split_name(image_name)
+    if not repo or not tag:
+        raise ValueError("Must be a full repo/name:tag docker image_name")
+    tokens = tag.split(".")
+    if len(tokens) == 3:
+        return image_name
+    elif len(tokens) == 4:
+        # remove hash and decrement patch
+        major, minor, patch, _ = tokens
+        patch = int(patch)
+        if patch == 0:
+            raise ValueError(f"Tag {tag}: patch cannot be 0 for SCM with hash appended")
+        patch -= 1
+        patch = str(patch)
+        release_tag = ".".join([major, minor, patch])
+        return join_name(repo, name, release_tag)
+    else:
+        raise ValueError(
+            f"Tag {tag} must be in major.minor.patch[.hash] SCM version format"
+        )
 
 
 def ensure_image_present(self, image_name: str) -> str:
@@ -126,54 +168,51 @@ def ensure_image_present(self, image_name: str) -> str:
             log.error("Docker connection refused. Is Docker Daemon running?")
             raise
 
-    #
+    canon_image_name = get_armory_name(image_name)
+    log.info(f"Retrieved canonical image name for {image_name} as {canon_image_name}")
 
-    # look first for the versioned and then the unversioned, return if hit
-    # if there is a tag present, use that. otherwise add the current version
-    if ":" in image_name:
-        checks = (image_name,)
-    else:
-        # TODO: This needs to be fixed if 'image_name' does not refer to twosixarmory image
-        #   There should be a more explicit check for specific armory image names.
-        check = f"{image_name}:{armory.__version__}"
-        check_previous = ".".join(check.split(".")[:3])
-        if check_previous != check:
-            checks = (check, check_previous)
-        else:
-            checks = (check,)
-
-    for check in checks:
-        log.trace(f"asking local docker for image {check}")
+    prev_release = last_armory_release(canon_image_name)
+    if canon_image_name != prev_release:  # currently on hashed dev branch
+        log.trace(f"asking local docker for image {canon_image_name}")
         try:
-            docker_client.images.get(check)
-            log.success(f"found docker image {image_name} as {check}")
-            return check
+            docker_client.images.get(canon_image_name)
+            log.success(f"found docker image {image_name} as {canon_image_name}")
+            return canon_image_name
         except docker.errors.ImageNotFound:
-            log.trace(f"image {check} not found")
+            log.trace(f"image {canon_image_name} not found")
+            log.info(f"reverting to previous release tag image {prev_release}")
         except requests.exceptions.HTTPError:
-            log.trace(f"http error when looking for image {check}")
+            log.trace(f"http error when looking for image {canon_image_name}")
             raise
 
-    log.info(f"image {image_name} not found. downloading...")
+    log.trace(f"asking local docker for image {prev_release}")
     try:
-        docker_api.pull_verbose(docker_client, image_name)
-        return image_name
+        docker_client.images.get(prev_release)
+        log.success(f"found docker image {image_name} as {prev_release}")
+        return prev_release
+    except docker.errors.ImageNotFound:
+        log.trace(f"image {prev_release} not found")
+    except requests.exceptions.HTTPError:
+        log.trace(f"http error when looking for image {prev_release}")
+        raise
+
+    log.info(f"image {prev_release} not found. downloading...")
+    try:
+        docker_api.pull_verbose(docker_client, prev_release)
+        return prev_release
     except docker.errors.NotFound:
-        if image_name in ALL:
-            raise ValueError(
-                "You are attempting to pull an unpublished armory docker image.\n"
-                "This is likely because you're running armory from a dev branch. "
-                "If you want a stable release with "
-                "published docker images try pip installing 'armory-testbed' "
-                "or using out one of the release branches on the git repository. "
-                "If you'd like to continue working on the developer image please "
-                "build it from source on your machine as described here:\n"
-                "https://armory.readthedocs.io/en/latest/contributing/#development-docker-containers\n"
-                "python docker/build.py --help"
-            )
-        else:
-            log.error(f"Image {image_name} could not be downloaded")
-            raise
+        log.error(f"Image {prev_release} could not be downloaded")
+        raise ValueError(
+            "You are attempting to pull an unpublished armory docker image.\n"
+            "This is likely because you're running armory from a dev branch. "
+            "If you want a stable release with "
+            "published docker images try pip installing 'armory-testbed' "
+            "or using out one of the release branches on the git repository. "
+            "If you'd like to continue working on the developer image please "
+            "build it from source on your machine as described here:\n"
+            "https://armory.readthedocs.io/en/latest/contributing/#development-docker-containers\n"
+            "python docker/build.py --help"
+        )
     except requests.exceptions.ConnectionError:
         log.error("Docker connection refused. Is Docker Daemon running?")
         raise
