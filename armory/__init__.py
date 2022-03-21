9cbdb6af8e296da09d958f7a54e9dac01a6ced33
David Slater <david.slater@twosixlabs.com>
2022-03-21 14:47:12 -0700

    remove unneeded line

--- armory/__init__.py
+++ armory/__init__.py
@@ -79,7 +79,6 @@ try:
         # Currently, memory growth needs to be the same across GPUs
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
-        logical_gpus = tf.config.list_logical_devices("GPU")
         log.info("Setting tf.config.experimental.set_memory_growth to True on all GPUs")
 except RuntimeError:
     log.exception("Import armory before initializing GPU tensors")
