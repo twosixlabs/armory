e6e91f4004557191904481c9436929d0d9808d3f
davidslater <david.slater@twosixlabs.com>
2022-03-23 11:43:17 -0700

    update name in test (#1365)

--- tests/end_to_end/test_no_docker.py
+++ tests/end_to_end/test_no_docker.py
@@ -30,7 +30,7 @@ def test_run(scenario_configs, config, args):
 def test_interactive(scenario_configs, config):
     # log.info("Executing Config Dir: {}".format(scenario_configs))
     log.info("Running Armory Scenarios interactive in `--no-docker` mode")
-    from armory import scenarios
+    from armory.scenarios.main import get as get_scenario
     from armory import paths
 
     log.info("Setting Paths to `host`")
@@ -38,6 +38,6 @@ def test_interactive(scenario_configs, config):
 
     config = os.path.join(scenario_configs, "no_docker", config)
     log.info("Loading Config: {}".format(config))
-    s = scenarios.get(config).load()
+    s = get_scenario(config).load()
     log.info("Evaluating Config")
     s.evaluate()
