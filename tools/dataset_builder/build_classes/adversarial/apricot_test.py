f60213b619b919ff8f0a2e57913c056947bd471d
Seth Henshaw <seth.henshaw@twosixtech.com>
2022-03-31 14:20:34 -0400

    Added adversarial datasets apricot, carla, dapricot, gtsrb.... dapricot gives some issue about slicing but ignoring for now

---
+++ tools/dataset_builder/build_classes/adversarial/apricot_test.py
@@ -0,0 +1,268 @@
+"""apricot_test dataset."""
+
+import collections
+import json
+import os
+
+import tensorflow.compat.v1 as tf
+import tensorflow_datasets.public_api as tfds
+
+from armory.data.adversarial.apricot_metadata import APRICOT_PATCHES, APRICOT_MODELS
+
+_CITATION = """
+@misc{braunegg2020apricot,
+      title={APRICOT: A Dataset of Physical Adversarial Attacks on Object Detection},
+      author={A. Braunegg and Amartya Chakraborty and Michael Krumdick and Nicole Lape and Sara Leary and Keith Manville and Elizabeth Merkhofer and Laura Strickhart and Matthew Walmer},
+      year={2020},
+      eprint={1912.08166},
+      archivePrefix={arXiv},
+      primaryClass={cs.CV}
+}
+"""
+
+_DESCRIPTION = """
+The APRICOT Dataset
+============================================================================
+A collection of Adversarial Patches Rearranged In COnText
+Version: 1.1
+
+============================================================================
+
+ANNOTATION FORMAT
+-----------------
+Annotations for APRICOT are provided in a JSON format similar to COCO, but
+with extra metadata for the adversarial patches. You can find information
+about the COCO format here: https://cocodataset.org/#format-data. The
+APRICOT JSON files are compatible with the COCO API:
+https://github.com/cocodataset/cocoapi. Object and patch locations are
+provided in bounding box format only.
+
+The fields in the APRICOT JSON format are as follows:
+
+- categories: A list of the COCO categories. The ‘adversarial patch’
+  category has been added as id 12 since that id was not used by COCO.
+
+- patches: A list of the 60 patches used in APRICOT. Each patch gets a
+  unique name and id and some additional information:
+  - adv_target: The category_id of the target category
+  - adv_model: The id of the detector used to generate the patch
+  - is_square and is_circle: Two booleans that tell the shape of the patch
+
+- models: A list of the models used to generate the adversarial patches.
+  They have unique ids referenced by the list of patches, the name of the
+  model, classifier backbone, and a link to the model from the tensorflow
+  object detection API.
+
+- info: High level information about the dataset
+
+- annotations: A list of bounding box annotations for the objects and
+  patches in APRICOT. Similar to COCO annotations, but with the following
+  additional information:
+  - angle: The viewing angle of the patch. 0 = head on, 1 = slightly off
+    angle, 2 = severe angle. Each patch has annotations from 3 people.
+  - is_warped: The patches were printed on heavy cardstock, but in some
+    images you will see the patches are warped from the paper bending. We
+    annotated this so we could later measure the effect of warped patches.
+
+- images: A list of the images in APRICOT, following the COCO format.
+
+============================================================================
+
+DATASET PARTITIONS
+------------------
+The APRICOT dataset is divided into two partitions, a development partition
+(dev) for validation and a testing partition (test) for reporting results.
+For dataset herein, only the test partition is provided.
+
+These files are provided as a convenience to help with different types of
+experiments. For example, experiments which aim to detect patches directly
+should probably be run with apricot_*_patch_annotations.json, while
+experiments designed to measure COCO performance before and after a
+defensive mechanism is added should use apricot_*_coco_annotations.json.
+
+============================================================================
+
+LEGAL
+-----
+The APRICOT Dataset is distributed under an Apache License Version 2.0.
+
+Copyright 2019 The MITRE Corporation. All rights reserved. Approved for
+public release; distribution unlimited.  Case #19-3440.
+"""
+
+_URL = "https://arxiv.org/abs/1912.08166"
+
+_URLS = "https://armory-public-data.s3.us-east-2.amazonaws.com/adversarial-datasets/apricot_test.tar.gz"
+
+
+class ApricotTest(tfds.core.GeneratorBasedBuilder):
+    """APRICOT Test dataset."""
+
+    VERSION = tfds.core.Version("1.0.0")
+
+    def _info(self):
+        features = {
+            "image": tfds.features.Image(encoding_format="jpeg"),
+            "images": tfds.features.Sequence(
+                tfds.features.FeaturesDict(
+                    {
+                        "file_name": tfds.features.Text(),
+                        "height": tf.int64,
+                        "width": tf.int64,
+                        "id": tf.int64,
+                    }
+                )
+            ),
+            "categories": tfds.features.Sequence(
+                tfds.features.FeaturesDict(
+                    {
+                        "id": tf.int64,
+                        "name": tfds.features.Text(),
+                        "supercategory": tfds.features.Text(),
+                    }
+                )
+            ),
+            "models": tfds.features.Sequence(
+                tfds.features.FeaturesDict(
+                    {
+                        "classifier": tfds.features.Text(),
+                        "id": tf.int64,
+                        "model": tfds.features.Text(),
+                        "url": tfds.features.Text(),
+                    }
+                )
+            ),
+            "patches": tfds.features.Sequence(
+                tfds.features.FeaturesDict(
+                    {
+                        "adv_model": tf.int64,
+                        "adv_target": tf.int64,
+                        "id": tf.int64,
+                        "is_circle": tf.bool,
+                        "is_square": tf.bool,
+                        "name": tfds.features.Text(),
+                    }
+                )
+            ),
+            "objects": tfds.features.Sequence(
+                {
+                    "id": tf.int64,
+                    "image_id": tf.int64,
+                    "area": tf.int64,  # un-normalized area
+                    "boxes": tfds.features.BBoxFeature(),  # normalized bounding box [ymin, xmin, ymax, xmax]
+                    "labels": tfds.features.ClassLabel(num_classes=91),
+                    "is_crowd": tf.bool,
+                    "is_warped": tf.bool,
+                    "angle": tfds.features.Tensor(shape=(3,), dtype=tf.int64),
+                    "patch_id": tf.int64,
+                }
+            ),
+        }
+
+        return tfds.core.DatasetInfo(
+            builder=self,
+            description=_DESCRIPTION,
+            features=tfds.features.FeaturesDict(features),
+            homepage=_URL,
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        """Returns SplitGenerators."""
+        paths = dl_manager.download_and_extract(_URLS)
+        return [
+            tfds.core.SplitGenerator(
+                name=split,
+                gen_kwargs={"path": os.path.join(paths, "APRICOT"), "model": split},
+            )
+            for split in ["retinanet", "frcnn", "ssd"]
+        ]
+
+    def _generate_examples(self, path, model):
+        """yield examples"""
+        annotation_path = os.path.join(
+            path, "Annotations/apricot_test_all_annotations.json"
+        )
+        apricot = ApricotAnnotation(annotation_path)
+        images = apricot.images()
+
+        # iterate over all images
+        for image_info in sorted(images, key=lambda x: x["id"]):
+            annotations = apricot.get_annotations(image_info["id"])
+
+            def build_bbox(x, y, width, height):
+                return tfds.features.BBox(
+                    ymin=y / image_info["height"],
+                    xmin=x / image_info["width"],
+                    ymax=(y + height) / image_info["height"],
+                    xmax=(x + width) / image_info["width"],
+                )
+
+            example = {
+                "image": os.path.join(path, "Images/test", image_info["file_name"]),
+                "images": apricot.images(),
+                "categories": apricot.categories(),
+                "models": apricot.models(),
+                "patches": apricot.patches(),
+                "objects": [
+                    {
+                        "id": anno["id"],
+                        "image_id": anno["image_id"],
+                        "area": anno["area"],
+                        "boxes": build_bbox(*anno["bbox"]),
+                        "labels": anno["category_id"],
+                        "is_crowd": bool(anno["iscrowd"]),
+                        "is_warped": anno.get("is_warped", False),
+                        "angle": anno.get("angle", [-1, -1, -1]),
+                        "patch_id": anno.get("patch_id", -1),
+                    }
+                    for anno in annotations
+                ],
+            }
+
+            patch_id = [
+                i["patch_id"] for i in example["objects"] if i["patch_id"] != -1
+            ][0]
+            adv_model_id = APRICOT_PATCHES[patch_id]["adv_model"]
+            model_name = [
+                i["model"] for i in APRICOT_MODELS if i["id"] == adv_model_id
+            ][0]
+            if model_name == model:
+                yield image_info["id"], example
+
+
+class ApricotAnnotation(object):
+    """Apricot annotation helper class."""
+
+    def __init__(self, annotation_path):
+        with tf.io.gfile.GFile(annotation_path) as f:
+            data = json.load(f)
+        self._data = data
+
+        img_id2annotations = collections.defaultdict(list)
+        for a in self._data["annotations"]:
+            img_id2annotations[a["image_id"]].append(a)
+        self._img_id2annotations = {
+            k: list(sorted(v, key=lambda a: a["id"]))
+            for k, v in img_id2annotations.items()
+        }
+
+    def categories(self):
+        """Return the category dicts, as sorted in the file."""
+        return self._data["categories"]
+
+    def images(self):
+        """Return the image dicts, as sorted in the file."""
+        return self._data["images"]
+
+    def models(self):
+        """Return the model dicts, as sorted in the file."""
+        return self._data["models"]
+
+    def patches(self):
+        """Return the patch dicts, as sorted in the file."""
+        return self._data["patches"]
+
+    def get_annotations(self, img_id):
+        """Return all annotations associated with the image id string."""
+        return self._img_id2annotations.get(img_id, [])
---
+++ tools/dataset_builder/build_classes/adversarial/dapricot_test.py
@@ -0,0 +1,350 @@
+"""dapricot_test dataset."""
+
+import collections
+import json
+import os
+
+import tensorflow.compat.v1 as tf
+import tensorflow_datasets as tfds
+
+from armory.data.adversarial import pandas_proxy
+
+_DESCRIPTION = """
+LEGAL
+-----
+Copyright 2021 The MITRE Corporation. All rights reserved.
+"""
+
+_CITATION = """
+Dataset is unpublished at this time.
+"""
+
+_URLS = "https://armory-public-data.s3.us-east-2.amazonaws.com/adversarial-datasets/dapricot_test.tar.gz"
+
+
+class DapricotTest(tfds.core.GeneratorBasedBuilder):
+    """DatasetBuilder for dapricot_test dataset."""
+
+    VERSION = tfds.core.Version("1.0.0")
+    RELEASE_NOTES = {
+        "1.0.0": "Initial release.",
+    }
+
+    def _info(self) -> tfds.core.DatasetInfo:
+        """Returns the dataset metadata."""
+        features = {
+            # all Sequences are for [camera_1, camera_2, camera_3]
+            "image": tfds.features.Sequence(
+                tfds.features.Image(shape=(None, None, 3)),  # encoding_format="jpeg"),
+                length=3,
+            ),
+            "images": tfds.features.Sequence(
+                tfds.features.FeaturesDict(
+                    {
+                        "file_name": tfds.features.Text(),
+                        "height": tf.int64,
+                        "width": tf.int64,
+                        "id": tf.int64,
+                    }
+                ),
+                length=3,
+            ),
+            "categories": tfds.features.Sequence(
+                tfds.features.Sequence(
+                    tfds.features.FeaturesDict(
+                        {
+                            "id": tf.int64,  # {'octagon':12, 'diamond':26, 'rect':29}
+                            "name": tfds.features.Text(),
+                        }
+                    )
+                ),
+                length=3,
+            ),
+            "objects": tfds.features.Sequence(
+                tfds.features.Sequence(
+                    {
+                        "id": tf.int64,
+                        "image_id": tf.int64,
+                        "area": tf.int64,  # un-normalized area
+                        "boxes": tfds.features.BBoxFeature(),  # normalized bounding box [ymin, xmin, ymax, xmax]
+                        "labels": tfds.features.ClassLabel(num_classes=91),
+                        "is_crowd": tf.bool,
+                    }
+                ),
+                length=3,
+            ),
+            "patch_metadata": tfds.features.Sequence(
+                # these data only apply to the "green screen patch" objects
+                tfds.features.FeaturesDict(
+                    {
+                        "gs_coords": tfds.features.Sequence(
+                            tfds.features.Tensor(
+                                shape=[2], dtype=tf.int64
+                            ),  # green screen vertices in (x,y)
+                        ),
+                        "cc_ground_truth": tfds.features.Tensor(
+                            shape=[24, 3], dtype=tf.float32
+                        ),  # colorchecker color ground truth
+                        "cc_scene": tfds.features.Tensor(
+                            shape=[24, 3], dtype=tf.float32
+                        ),  # colorchecker colors in a scene
+                        "shape": tfds.features.Text(),  # "diamond", "rect", "octagon"
+                    }
+                ),
+                length=3,
+            ),
+        }
+
+        return tfds.core.DatasetInfo(
+            builder=self,
+            description=_DESCRIPTION,
+            features=tfds.features.FeaturesDict(features),
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager: tfds.download.DownloadManager):
+        """Returns SplitGenerators."""
+        paths = dl_manager.download_and_extract(_URLS)
+        return [
+            tfds.core.SplitGenerator(
+                name=patch_size,
+                gen_kwargs={"path": os.path.join(paths, "test"), "size": patch_size},
+            )
+            for patch_size in ["large", "medium", "small"]
+        ]
+
+    def _generate_examples(self, path, size):
+        """yield examples"""
+
+        scenes = [
+            "02",
+            "03",
+            "04",
+            "05",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "15",
+        ]
+
+        size_dist = {"small": "dist15", "medium": "dist10", "large": "dist5"}
+
+        yield_id = 0
+        # For each scene, read JSONs for all cameras.
+        # For each camera, go through each image.
+        # For each image, gets its annotations and yield relevant data
+        for scene in scenes:
+
+            annotation_path_camera_1 = os.path.join(
+                path, "annotations/labels_scene_{}_camera_1.json".format(scene)
+            )
+            annotation_path_camera_2 = os.path.join(
+                path, "annotations/labels_scene_{}_camera_2.json".format(scene)
+            )
+            annotation_path_camera_3 = os.path.join(
+                path, "annotations/labels_scene_{}_camera_3.json".format(scene)
+            )
+
+            dapricot_camera_1 = DapricotAnnotation(annotation_path_camera_1)
+            dapricot_camera_2 = DapricotAnnotation(annotation_path_camera_2)
+            dapricot_camera_3 = DapricotAnnotation(annotation_path_camera_3)
+
+            images_camera_1 = dapricot_camera_1.images()
+            images_camera_2 = dapricot_camera_2.images()
+            images_camera_3 = dapricot_camera_3.images()
+
+            # sort images alphabetically so all three cameras are consistent
+            images_camera_1 = sorted(
+                images_camera_1, key=lambda x: x["file_name"].lower()
+            )
+            images_camera_2 = sorted(
+                images_camera_2, key=lambda x: x["file_name"].lower()
+            )
+            images_camera_3 = sorted(
+                images_camera_3, key=lambda x: x["file_name"].lower()
+            )
+
+            for image_camera_1, image_camera_2, image_camera_3 in zip(
+                images_camera_1, images_camera_2, images_camera_3
+            ):
+
+                # verify consistency
+                fname1 = image_camera_1[
+                    "file_name"
+                ]  # fname has format "scene_#_camera_1_<SHAPE>_<HEIGHT>_<DIST>.JPG"
+                fname2 = image_camera_2["file_name"]
+                fname3 = image_camera_3["file_name"]
+                assert fname1 == ("_").join(
+                    fname2.split("_")[:3] + ["1"] + fname2.split("_")[4:]
+                ), "{} and {} are inconsistent".format(fname1, fname2)
+                assert fname1 == ("_").join(
+                    fname3.split("_")[:3] + ["1"] + fname3.split("_")[4:]
+                ), "{} and {} are inconsistent".format(fname1, fname3)
+
+                # get object annotations for each image
+                annotations_camera_1 = dapricot_camera_1.get_annotations(
+                    image_camera_1["id"]
+                )
+                annotations_camera_2 = dapricot_camera_2.get_annotations(
+                    image_camera_2["id"]
+                )
+                annotations_camera_3 = dapricot_camera_3.get_annotations(
+                    image_camera_3["id"]
+                )
+
+                # convert bbox to Pytorch format
+                def build_bbox(x, y, width, height):
+                    return tfds.features.BBox(
+                        ymin=y
+                        / image_camera_1[
+                            "height"
+                        ],  # all images are the same size, so using image_camera_1 is fine
+                        xmin=x / image_camera_1["width"],
+                        ymax=(y + height) / image_camera_1["height"],
+                        xmax=(x + width) / image_camera_1["width"],
+                    )
+
+                # convert segmentation format of (x0,y0,x1,y1,...) to ( (x0, y0), (x1, y1), ... )
+                def build_coords(segmentation):
+                    xs = segmentation[::2]
+                    ys = segmentation[1::2]
+                    coords = [[int(round(x)), int(round(y))] for (x, y) in zip(xs, ys)]
+
+                    return coords
+
+                # convert green screen shape given in file name to shape expected in downstream algorithms
+                def get_shape(in_shape):
+                    out_shape = {"stp": "octagon", "pxg": "diamond", "spd": "rect"}
+                    return out_shape[in_shape]
+
+                # get colorchecker color box values. There are 24 color boxes, so output shape is (24, 3)
+                def get_cc(ground_truth=True, scene=None, camera=None):
+                    if ground_truth:
+                        return pandas_proxy.read_csv_to_numpy_float32(
+                            os.path.join(
+                                path,
+                                "annotations",
+                                "xrite_passport_colors_sRGB-GMB-2005.csv",
+                            ),
+                            header=None,
+                        )
+                    else:
+                        return pandas_proxy.read_csv_to_numpy_float32(
+                            os.path.join(
+                                path,
+                                "annotations",
+                                "scene_{}_camera_{}_CC_values.csv".format(
+                                    scene, camera
+                                ),
+                            ),
+                            header=None,
+                        )
+
+                example = {
+                    "image": [
+                        os.path.join(
+                            path,
+                            "scene_{}/camera_{}".format(scene, camera + 1),
+                            im_cam["file_name"],
+                        )
+                        for camera, im_cam in enumerate(
+                            [image_camera_1, image_camera_2, image_camera_3]
+                        )
+                    ],
+                    "images": [image_camera_1, image_camera_2, image_camera_3],
+                    "categories": [
+                        d_cam.categories()
+                        for d_cam in [
+                            dapricot_camera_1,
+                            dapricot_camera_2,
+                            dapricot_camera_3,
+                        ]
+                    ],
+                    "objects": [
+                        [
+                            {
+                                "id": anno["id"],
+                                "image_id": anno["image_id"],
+                                "area": anno["area"],
+                                "boxes": build_bbox(*anno["bbox"]),
+                                "labels": anno["category_id"],
+                                "is_crowd": bool(anno["iscrowd"]),
+                            }
+                            for anno in annos
+                        ]
+                        for annos in [
+                            annotations_camera_1,
+                            annotations_camera_2,
+                            annotations_camera_3,
+                        ]
+                    ],
+                    "patch_metadata": [
+                        [
+                            {
+                                "gs_coords": build_coords(*anno["segmentation"]),
+                                "cc_ground_truth": get_cc(),
+                                "cc_scene": get_cc(
+                                    ground_truth=False, scene=scene, camera=camera + 1
+                                ),
+                                "shape": get_shape(
+                                    im_info["file_name"].split("_")[4].lower()
+                                ),  # file_name has format "scene_#_camera_#_<SHAPE>_<HEIGHT>_<DIST>.JPG"
+                            }
+                            for anno in annos
+                            if len(anno["segmentation"]) > 0
+                        ][0]
+                        for camera, (annos, im_info) in enumerate(
+                            zip(
+                                [
+                                    annotations_camera_1,
+                                    annotations_camera_2,
+                                    annotations_camera_3,
+                                ],
+                                [image_camera_1, image_camera_2, image_camera_3],
+                            )
+                        )
+                    ],
+                }
+
+                yield_id = yield_id + 1
+
+                patch_size = image_camera_1["file_name"].split(".")[
+                    0
+                ]  # scene_#_camera_#_<SHAPE>_<HEIGHT>_<DIST>
+                patch_size = patch_size.split("_")[-1].lower()  # <DIST>
+                if size_dist[size] == patch_size:
+                    yield yield_id, example
+
+
+class DapricotAnnotation(object):
+    """Dapricot annotation helper class."""
+
+    def __init__(self, annotation_path):
+        with tf.io.gfile.GFile(annotation_path) as f:
+            data = json.load(f)
+        self._data = data
+
+        # for each images["id"], find all annotations such that annotations["image_id"] == images["id"]
+        img_id2annotations = collections.defaultdict(list)
+        for a in self._data["annotations"]:
+            img_id2annotations[a["image_id"]].append(a)
+        self._img_id2annotations = {
+            k: list(sorted(v, key=lambda a: a["id"]))
+            for k, v in img_id2annotations.items()
+        }
+
+    def categories(self):
+        """Return the category dicts, as sorted in the file."""
+        return self._data["categories"]
+
+    def images(self):
+        """Return the image dicts, as sorted in the file."""
+        return self._data["images"]
+
+    def get_annotations(self, img_id):
+        """Return all annotations associated with the image id string."""
+        return self._img_id2annotations.get(img_id, [])
