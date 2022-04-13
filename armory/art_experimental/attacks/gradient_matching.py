c2242a72d9f0b700216552dbe34292d5266cf60b
Sterling Suggs <sterling.suggs@twosixtech.com>
2022-04-13 12:31:53 -0700

    attack runs and loads or saves properly.  lots of todos

--- armory/art_experimental/attacks/gradient_matching.py
+++ armory/art_experimental/attacks/gradient_matching.py
@@ -1,44 +1,56 @@
+import os
 import numpy as np
 from art.attacks.poisoning.gradient_matching_attack import GradientMatchingAttack
 
 
+
 class GradientMatchingWrapper(GradientMatchingAttack):
 
-    def __init__(self, **kwargs):
+    def __init__(self, classifier, **kwargs):
         self.attack_kwargs = kwargs
-        estimator = kwargs["estimator"]
-        percent_poison = kwargs["percent_poison"]
+        percent_poison = kwargs.pop("percent_poison")
+        learning_rates, schedule = kwargs.pop("learning_rate_schedule")
+        learning_rate_schedule = (np.array(learning_rates), schedule)
+
         epsilon = 0.01 # TODO: this depended on the dataset, in the notebook
 
-
-
-        super().__init__(estimator,
-                percent_poison,
-                max_trials=1,
-                max_epochs=500,
-                # clip_values=(min_,max_),
-                learning_rate_schedule=(np.array([1e-1, 1e-2, 1e-3, 1e-4, 1e-5]), [250, 350, 400, 430, 460]),
+        super().__init__(
+                classifier=classifier,
+                percent_poison=percent_poison,
+                # max_epochs=50, # TODO reset to 500 or something in the config
+                # clip_values=(min_,max_),  get this somewhere
+                learning_rate_schedule=learning_rate_schedule,
                 epsilon=epsilon,
-                verbose=1)
+                **kwargs
+                )
 
 
-    def poison(self, filepath, x_trigger, y_trigger, x_train, y_train):
+    def poison(self, filepath, x_trigger, y_trigger, x_train, y_train, trigger_index):
         """
-        if filepath doesn't exist, data should be provided
-        if data is none, filepath should be path to pregenerated data
+        Return a dataset poisoned to cause misclassification of x_trigger.
+
+        filepath, string: path to load data from (if it exists) or save it to (if it doesn't)
+        x_trigger, array: Images from the test set that we hope to misclassify
+        y_trigger, array: Labels for x_trigger
+        x_train, array: The training images to be poisoned
+        y_train, array: Labels for x_train
+        trigger_index, int or array: Index or indices of trigger images
         """
 
-        # check if file exists to return
-        if os.path.exists(filepath):
-
-            return # TODO load data from npz
-
+        # check if pre-poisoned dataset exists
+        if filepath is not None and os.path.exists(filepath): 
+            poison_npz = np.load(filepath)
+            x_poison, y_poison, poison_index, trigger_index = poison_npz['x_poison'], poison_npz['y_poison'], poison_npz['poison_index'], poison_npz['trigger_index']
+            # TODO check that trigger index is the same as passed arg, right here.
+            # make sure this attack wrapper is stand-alone from scenario and dataset.
+            
         else:
-
             x_poison, y_poison = super().poison(x_trigger, y_trigger, x_train, y_train)
-            poison_index = np.where([np.any(p != o) for (p,o) in zip(x_poison,x_train)])[0]
+            poison_index = np.where([np.any(p != b) for (p, b) in zip(x_poison, x_train)])[0]
 
-            # TODO then save data to filepath as npz
-        return x_poison, y_poison, poison_index
+            if filepath is not None:
+                np.savez(filepath, x_poison=x_poison, y_poison=y_poison, poison_index=poison_index, trigger_index=trigger_index)
+        
+        return x_poison, y_poison, poison_index, trigger_index
 
 
--- armory/scenarios/poisoning_cifar10_witches_brew.py
+++ armory/scenarios/poisoning_cifar10_witches_brew.py
@@ -1,53 +1,61 @@
+import os
 from armory.scenarios.poison import Poison
 from armory.logs import log
 from armory.utils import config_loading
+from armory import paths
+from art.utils import to_categorical 
 import numpy as np
 
 
+
 class DatasetPoisonerWitchesBrew():
-    def __init__(self, attack, source_class, target_class, x_test, y_test):
+    def __init__(self, attack, x_test, y_test, source_class, target_class, data_filepath):
         """
         Individual source-class triggers are chosen from x_test.  At poison time, the
         train set is modified to induce misclassification of the triggers as target_class.
 
         """
         self.attack = attack
-        self.source_class = source_class
-        self.target_class = target_class
         self.x_test = x_test
         self.y_test = y_test
+        self.source_class = source_class
+        self.target_class = target_class
+        self.data_filepath = data_filepath
 
 
-    def poison_dataset(self, x_train, y_train, return_index=True):
+    def poison_dataset(self, x_train, y_train, trigger_index, return_index=True):
         """
         Return a poisoned version of dataset x, y
             if return_index, return x, y, index
-        If fraction is not None, use it to override default
         """
         if len(x_train) != len(y_train):
             raise ValueError("Sizes of x and y do not match")
+        
+        x_trigger = self.x_test[trigger_index]
+        if len(x_trigger.shape) == 3:
+            x_trigger = np.expand_dims(x_trigger, axis=0)
 
-        from art.utils import to_categorical
-        print(self.y_test[:10])
-        print(type(self.y_test))
-        index_target = np.where(self.y_test==self.source_class)[0][5] # TODO index 0 because where returns 1-length tuple.  5 to just pick one.
+        y_trigger  = to_categorical([self.target_class], nb_classes=len(np.unique(y_train)))
 
-        # Trigger sample
-        x_trigger = self.x_test[index_target:index_target+1]
-        y_trigger  = to_categorical([self.target_class], nb_classes=10)
-        print ("HELLO FROM OVER HERE")
-        print(index_target)
+        # TODO is armory data oriented and scaled right for art_experimental
+        poison_x, poison_y, poison_index, returned_trigger_index = self.attack.poison(self.data_filepath, x_trigger, y_trigger, x_train, y_train, trigger_index) 
         
 
-        filepath = "stuff.npz" # TODO where to get this from
+        if returned_trigger_index != trigger_index:
+            # We loaded a presaved dataset, but it was trained for a different trigger
+            # TODO move this into attack.poison probably
+            raise ValueError(f"The trigger image index requested in the config ({trigger_index}) "
+            + f"does not match the trigger image index from the saved adversarial dataset ({returned_trigger_index}).  "
+            + "Please clarify your intent by updating adhoc['trigger_index'] or attack['kwargs']['data_filepath'] in the config. "
+            )
 
-        poison_x, poison_y, poison_index = self.attack.poison(filepath, x_trigger, y_trigger, x_train, y_train) # TODO is armory data oriented and scaled right for art
-
+        # Return trigger_index to make sure 
         if return_index:
-            return poison_x, poison_y, poison_index
-        return poison_x, poison_y
+            return poison_x, poison_y, trigger_index, poison_index
+        return poison_x, poison_y, trigger_index
 
 
+# TODO Config: decide what goes in adhoc vs attack kwargs
 
 
 class CifarWitchesBrew(Poison):
@@ -67,18 +75,50 @@ class CifarWitchesBrew(Poison):
             dataset_config, split="test", num_batches=None, **self.dataset_kwargs,
         )
         x_test, y_test = (np.concatenate(z, axis=0) for z in zip(*list(test_dataset)))
+
+        # TODO how to pick or set trigger images
+        self.trigger_index = adhoc_config["trigger_index"]
+        # self.trigger_index = np.where(y_test==self.source_class)[0][5].
+        if y_test[self.trigger_index] != self.source_class:
+            raise ValueError(f"Trigger image does not belong to source class (class {y_test[self.trigger_index]} != class {self.source_class})")
+
+
         if self.use_poison:
-            attack_config["kwargs"]["estimator"] = self.model
+
             attack_config["kwargs"]["percent_poison"] = adhoc_config["fraction_poisoned"]
-            attack = config_loading.load(attack_config)
+            
+            data_filepath = attack_config['kwargs'].pop('data_filepath') if 'data_filepath' in attack_config['kwargs'].keys() else None
+
+            attack = config_loading.load_attack(attack_config, self.model)
+            if data_filepath is not None:
+                data_filepath = os.path.join(paths.runtime_paths().dataset_dir, data_filepath)
             self.poisoner = DatasetPoisonerWitchesBrew(
                 attack,
-                self.source_class,
-                self.target_class,
                 x_test,
                 y_test,
+                self.source_class,
+                self.target_class,
+                data_filepath,
             )
             self.test_poisoner = self.poisoner
+
+    def poison_dataset(self):
+        
+        if self.use_poison:
+            (
+                self.x_poison,
+                self.y_poison,
+                self.trigger_index,
+                self.poison_index,
+            ) = self.poisoner.poison_dataset(
+                self.x_clean, self.y_clean, self.trigger_index, return_index=True
+            )
+        else:
+            self.x_poison, self.y_poison, self.poison_index = (
+                self.x_clean,
+                self.y_clean,
+                np.array([]),
+            )
 
 
     def load(self):
@@ -97,6 +137,9 @@ class CifarWitchesBrew(Poison):
     def evaluate_current(self):
 
         x, y = self.x, self.y
+
+        # TODO do triggers and non-triggers separately
+        # need to build self.benign_test_accuracy_per_class for validation accuracy?
 
         x.flags.writeable = False
         y_pred = self.model.predict(x, **self.predict_kwargs)
