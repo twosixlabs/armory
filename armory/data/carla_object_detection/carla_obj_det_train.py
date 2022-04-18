160894b5103e58d5ee327c9c4ae2c7a436c6903a
Yusong <ytan@mitre.org>
2022-04-18 01:01:13 +0000

    updated carla object detection training data

--- armory/data/carla_object_detection/carla_obj_det_train.py
+++ armory/data/carla_object_detection/carla_obj_det_train.py
@@ -1,7 +1,6 @@
 import collections
 import json
 import os
-from copy import deepcopy
 
 import tensorflow.compat.v1 as tf
 import tensorflow_datasets as tfds
@@ -21,17 +20,18 @@ _CITATION = """
 """
 
 # fmt: off
-_URLS = "https://armory-public-data.s3.us-east-2.amazonaws.com/carla/carla_OD_train.tar.gz"
+_URLS = "carla_train_val_dataset.tar.gz"
 # fmt: on
 
 
 class CarlaObjDetTrain(tfds.core.GeneratorBasedBuilder):
     """DatasetBuilder for carla_obj_det_train dataset."""
 
-    VERSION = tfds.core.Version("1.0.1")
+    VERSION = tfds.core.Version("2.0.0")
     RELEASE_NOTES = {
         "1.0.0": "Initial release.",
         "1.0.1": "Correcting error to RGB and depth image pairing",
+        "2.0.0": "Eval5 update with higher resolution, HD textures, and accurate annotations",
     }
 
     def _info(self) -> tfds.core.DatasetInfo:
@@ -39,7 +39,7 @@ class CarlaObjDetTrain(tfds.core.GeneratorBasedBuilder):
         features = {
             # sequence of [RGB, depth] images
             "image": tfds.features.Sequence(
-                tfds.features.Image(shape=(600, 800, 3)), length=2,
+                tfds.features.Image(shape=(960, 1280, 3)), length=2,
             ),
             # sequence of image features for [RGB, depth]
             "images": tfds.features.Sequence(
@@ -57,8 +57,9 @@ class CarlaObjDetTrain(tfds.core.GeneratorBasedBuilder):
             "categories": tfds.features.Sequence(
                 tfds.features.FeaturesDict(
                     {
-                        "id": tf.int64,  # {'pedstrian':1, 'vehicles':2, 'trafficlight':3, 'patch':4}
+                        "id": tf.int64,  # {'pedstrian':1, 'vehicles':2, 'trafficlight':3}
                         "name": tfds.features.Text(),
+                        "supercategory": tfds.features.Text(),
                     }
                 )
             ),
@@ -87,8 +88,9 @@ class CarlaObjDetTrain(tfds.core.GeneratorBasedBuilder):
         path = dl_manager.download_and_extract(_URLS)
         return [
             tfds.core.SplitGenerator(
-                name="train", gen_kwargs={"path": os.path.join(path, "train")},
+                name=split, gen_kwargs={"path": os.path.join(path, split)},
             )
+            for split in ["train", "val"]
         ]
 
     def _generate_examples(self, path):
@@ -97,53 +99,57 @@ class CarlaObjDetTrain(tfds.core.GeneratorBasedBuilder):
 
         yield_id = 0
 
-        annotation_path = os.path.join(path, "annotations/coco_annotations.json")
+        annotation_path = os.path.join(path, "kwcoco_annotations.json")
 
         cocoanno = COCOAnnotation(annotation_path)
 
-        images_rgb = (
+        images = (
             cocoanno.images()
-        )  # list of dictionaries of RGB image id, height, width, file_name
+        )  # list of dictionaries of image id, height, width, file_name
 
         # sort images alphabetically
-        images_rgb = sorted(images_rgb, key=lambda x: x["file_name"].lower())
+        images = sorted(images, key=lambda x: x["file_name"].lower())
 
-        for image_rgb in images_rgb:
+        for image in images:
 
-            # verify RGB and depth are paired
-            fname_rgb = image_rgb["file_name"]  # rgb image file
-            fname, fext = fname_rgb.split(".")
-            fname_depth = fname + "_Depth." + fext  # depth image file
-            image_depth = deepcopy(image_rgb)
-            image_depth["file_name"] = fname_depth
+            # remove unimportant keys/values
+            image.pop("license", None)
+            image.pop("flickr_url", None)
+            image.pop("coco_url", None)
+            image.pop("data_captured", None)
+
+            fname = image["file_name"]
 
             # get object annotations for each image
-            annotations = cocoanno.get_annotations(image_rgb["id"])
+            annotations = cocoanno.get_annotations(image["id"])
 
             # convert bbox to Tensorflow format
             def build_bbox(x, y, width, height):
                 return tfds.features.BBox(
-                    ymin=y / image_rgb["height"],
-                    xmin=x / image_rgb["width"],
-                    ymax=(y + height) / image_rgb["height"],
-                    xmax=(x + width) / image_rgb["width"],
+                    ymin=y / image["height"],
+                    xmin=x / image["width"],
+                    ymax=(y + height) / image["height"],
+                    xmax=(x + width) / image["width"],
                 )
 
             example = {
                 "image": [
                     os.path.join(path, modality,)
-                    for modality in [fname_rgb, fname_depth]
+                    for modality in [
+                        os.path.join("rgb", fname),
+                        os.path.join("depth", fname),
+                    ]
                 ],
-                "images": [image_rgb, image_depth],
+                "images": [image, image],
                 "categories": cocoanno.categories(),
                 "objects": [
                     {
                         "id": anno["id"],
                         "image_id": anno["image_id"],
-                        "area": anno["areas"],
+                        "area": anno["area"],
                         "boxes": build_bbox(*anno["bbox"]),
                         "labels": anno["category_id"],
-                        "is_crowd": bool(anno["iscrowd"]),
+                        "is_crowd": 0,
                     }
                     for anno in annotations
                 ],
