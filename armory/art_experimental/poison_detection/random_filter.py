7ab6d56c703fbead101041ba294b0bd28c089eb8
Sterling Suggs <sterling.suggs@twosixtech.com>
2022-05-03 07:24:29 -0600

    rename variable to match other art defense

--- armory/art_experimental/poison_detection/random_filter.py
+++ armory/art_experimental/poison_detection/random_filter.py
@@ -31,15 +31,15 @@ class RandomFilterBaselineDefense(PoisonFilteringDefence):
                     and a 0 indicates that x_train[i] is detected as poison.
         """
         
-        fraction_to_remove = kwargs.get("fraction_to_remove", None)
-        if fraction_to_remove is None:
-            fraction_to_remove = 0.1
-            log.info("Setting fraction_to_remove to 0.1.  This can be set under defense/kwargs in the config")
+        expected_pp_poison = kwargs.get("expected_pp_poison", None)
+        if expected_pp_poison is None:
+            expected_pp_poison = 0.3
+            log.info("Setting expected_pp_poison to 0.3.  This can be set under defense/kwargs in the config")
         
-        if fraction_to_remove < 0 or fraction_to_remove > 1:
-            raise ValueError(f"defense/kwargs/fraction_to_remove must be set between 0 and 1 in the config.  Got {fraction_to_remove}")
+        if expected_pp_poison < 0 or expected_pp_poison > 1:
+            raise ValueError(f"defense/kwargs/expected_pp_poison must be set between 0 and 1 in the config.  Got {expected_pp_poison}")
 
 
-        is_clean = np.random.choice([0,1], self.n_data, p=[fraction_to_remove, 1 - fraction_to_remove])
+        is_clean = np.random.choice([0,1], self.n_data, p=[expected_pp_poison, 1 - expected_pp_poison])
 
         return None, is_clean
