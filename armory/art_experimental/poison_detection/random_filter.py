5747c5041f548e4a94af58511514c4406ad78280
Sterling Suggs <sterling.suggs@twosixtech.com>
2022-05-03 09:56:48 -0600

    first version of perfect filter

--- armory/art_experimental/poison_detection/random_filter.py
+++ armory/art_experimental/poison_detection/random_filter.py
@@ -6,8 +6,6 @@ from armory.logs import log
 
 
 class RandomFilterBaselineDefense(PoisonFilteringDefence):
-
-
     def __init__(self, classifier, x_train, y_train, **kwargs):
         """
         Create a :class:`.RandomFilterBaselineDefense` object with the provided classifier.
@@ -19,8 +17,9 @@ class RandomFilterBaselineDefense(PoisonFilteringDefence):
         self.n_data = len(y_train)
 
     def evaluate_defence(self, is_clean, **kwargs):
-        raise NotImplementedError("evaluate_defence() not implemented for RandomFilterBaselineDefense")
-
+        raise NotImplementedError(
+            "evaluate_defence() not implemented for RandomFilterBaselineDefense"
+        )
 
     def detect_poison(self, **kwargs):
         """
@@ -30,16 +29,21 @@ class RandomFilterBaselineDefense(PoisonFilteringDefence):
                 where is_clean is a list, where a 1 at index i indicates that x_train[i] is clean,
                     and a 0 indicates that x_train[i] is detected as poison.
         """
-        
+
         expected_pp_poison = kwargs.get("expected_pp_poison", None)
         if expected_pp_poison is None:
             expected_pp_poison = 0.3
-            log.info("Setting expected_pp_poison to 0.3.  This can be set under defense/kwargs in the config")
-        
+            log.info(
+                "Setting expected_pp_poison to 0.3.  This can be set under defense/kwargs in the config"
+            )
+
         if expected_pp_poison < 0 or expected_pp_poison > 1:
-            raise ValueError(f"defense/kwargs/expected_pp_poison must be set between 0 and 1 in the config.  Got {expected_pp_poison}")
+            raise ValueError(
+                f"defense/kwargs/expected_pp_poison must be set between 0 and 1 in the config.  Got {expected_pp_poison}"
+            )
 
-
-        is_clean = np.random.choice([0,1], self.n_data, p=[expected_pp_poison, 1 - expected_pp_poison])
+        is_clean = np.random.choice(
+            [0, 1], self.n_data, p=[expected_pp_poison, 1 - expected_pp_poison]
+        )
 
         return None, is_clean
