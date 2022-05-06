a3d4d83f40d3157052aa594aed64608af8cff1fa
Sterling Suggs <sterling.suggs@twosixtech.com>
2022-05-06 10:44:20 -0600

    clarify witches' brew errors, add config option to overwrite saved dataset

--- armory/art_experimental/attacks/gradient_matching.py
+++ armory/art_experimental/attacks/gradient_matching.py
@@ -14,12 +14,81 @@ class GradientMatchingWrapper(GradientMatchingAttack):
         self.triggers_chosen_randomly = kwargs.pop("triggers_chosen_randomly")
         learning_rate_schedule = tuple(kwargs.pop("learning_rate_schedule"))
 
+        self.overwrite_presaved_data = False if "overwrite_presaved_data" not in kwargs else kwargs.pop("overwrite_presaved_data")
+
         super().__init__(
             classifier=classifier,
             # clip_values=(min_,max_),  get this somewhere
             learning_rate_schedule=learning_rate_schedule,
             **kwargs,
         )
+
+    def _check_for_config_conflicts(self, filepath, trigger_index, load_trigger_index, load_source_class, load_target_class, percent_poison, epsilon):
+        # Check that config parameters are consistent with pre-poisoned dataset and raise a super helpful error if not
+        # If config is consistent with pre-saved data, do nothing.
+
+        loaded_description = f"Adversarial dataset at filepath {filepath} is not consistent with config.  The saved dataset was trained:\n"
+        config_description = f"But the config is requesting:\n"
+        suggestion = ("Please clarify your intent by doing one of the following:\n" +
+                        "  1 Reconcile the config with the above parameters (Armory can then use the saved dataset)--see note below,\n" +
+                        "  2 Delete or rename the saved dataset (Armory will generate a new dataset with the old filename),\n" +
+                        "  3 Change attack/kwargs/data_filepath in the config (Armory will generate a new dataset with a new filename),\n" +
+                        "  4 Set attack/kwargs/overwrite_presaved_data to True in the config (Armory will generate a new dataset and overwrite the saved one).\n" +
+                        "Note for 1: Either source_class or trigger_index can be set to null, or all three of source_class, trigger_index, and target_class.\n")
+
+        config_conflict = False
+        
+        if self.triggers_chosen_randomly:
+            # If random_triggers is True, we can override them with loaded data because user didn't care exactly which trigger images.
+            # But, we still enforce that there are the correct _nummber_ of triggers.
+            if len(load_trigger_index) != len(trigger_index):
+                loaded_description += f"-  for {len(load_trigger_index)} trigger image(s)\n"
+                config_description += f"-  {len(trigger_index)} trigger image(s)\n"
+                config_conflict = True
+        else:
+            # if random_triggers is false, trigger_index must match exactly.
+            if sorted(load_trigger_index) != sorted(trigger_index):
+                loaded_description += f"-  for trigger image(s) {load_trigger_index}\n"
+                config_description += f"-  trigger image(s) {trigger_index}\n"
+                config_conflict = True
+
+        # source class must match element-wise
+        if not (
+            len(load_source_class) == len(self.target_class)
+            and sum(
+                [s1 == s2 for s1, s2 in zip(load_source_class, self.source_class)]
+            )
+            == len(load_source_class)
+        ):
+            loaded_description += f"-  for source class(es) {load_source_class}\n"
+            config_description += f"-  source class(es) {self.source_class}\n"
+            config_conflict = True
+
+        # target class must match element-wise
+        if not (
+            len(load_target_class) == len(self.target_class)
+            and sum(
+                [t1 == t2 for t1, t2 in zip(load_target_class, self.target_class)]
+            )
+            == len(load_target_class)
+        ):
+            loaded_description += f"-  for target class(es) {load_target_class}\n"
+            config_description += f"-  target class(es) {self.target_class}\n"
+            config_conflict = True
+
+        # Fraction poisoned and epsilon must also match
+        if percent_poison != self.percent_poison:
+            loaded_description += f"-  with {percent_poison} fraction poisoned\n"
+            config_description += f"-  {self.percent_poison} fraction poisoned\n"
+            config_conflict = True
+        if epsilon != self.epsilon:
+            loaded_description += f"-  with an L_inf bound of {epsilon}\n"
+            config_description += f"-  an L_inf bound of {self.epsilon}\n"
+            config_conflict = True
+
+        if config_conflict:
+            raise ValueError(loaded_description + config_description + suggestion)
+
 
     def poison(self, filepath, x_trigger, y_trigger, x_train, y_train, trigger_index):
         """
@@ -35,6 +104,11 @@ class GradientMatchingWrapper(GradientMatchingAttack):
         y_train, array: Labels for x_train
         trigger_index, int or array: Index or indices of trigger images
         """
+
+        if self.overwrite_presaved_data:
+            if os.path.exists(filepath):
+                log.info("Deleting pre-saved dataset and generating anew.")
+                os.remove(filepath)
 
         # check if pre-poisoned dataset exists
         if filepath is not None and os.path.exists(filepath):
@@ -64,53 +138,8 @@ class GradientMatchingWrapper(GradientMatchingAttack):
                     load_target_class,
                 )
 
-            # Check that config parameters are consistent with pre-poisoned dataset
-
-            if len(load_trigger_index) != len(trigger_index):
-                raise ValueError(
-                    f"Adversarial dataset at filepath {filepath} was trained for {len(load_trigger_index)} trigger images, not {len(trigger_index)} as requested by the config."
-                )
-
-            # if random_triggers is True, we can override them with loaded data because user didn't care exactly which trigger images.
-            if not self.triggers_chosen_randomly:
-                if sorted(load_trigger_index) != sorted(trigger_index):
-                    raise ValueError(
-                        f"Adversarial dataset at filepath {filepath} was trained for trigger image(s) {load_trigger_index}, not {trigger_index} as requested by the config."
-                    )
-
-            # source class must match element-wise
-            if not (
-                len(load_source_class) == len(self.target_class)
-                and sum(
-                    [s1 == s2 for s1, s2 in zip(load_source_class, self.source_class)]
-                )
-                == len(load_source_class)
-            ):
-                raise ValueError(
-                    f"Adversarial dataset at filepath {filepath} was trained for source class(es) {load_source_class}, not {self.source_class} as requested by the config."
-                )
-
-            # target class must match element-wise
-            if not (
-                len(load_target_class) == len(self.target_class)
-                and sum(
-                    [t1 == t2 for t1, t2 in zip(load_target_class, self.target_class)]
-                )
-                == len(load_target_class)
-            ):
-                raise ValueError(
-                    f"Adversarial dataset at filepath {filepath} was trained for target class(es) {load_target_class}, not {self.target_class} as requested by the config."
-                )
-
-            # Fraction poisoned and epsilon must also match
-            if percent_poison != self.percent_poison:
-                raise ValueError(
-                    f"Adversarial dataset at filepath {filepath} has a poison frequency of {percent_poison}, not {self.percent_poison} as requested by the config."
-                )
-            if epsilon != self.epsilon:
-                raise ValueError(
-                    f"Adversarial dataset at filepath {filepath} has a L_inf perturbation bound of {epsilon}, not {self.epsilon} as requested by the config."
-                )
+            
+            self._check_for_config_conflicts(filepath, trigger_index, load_trigger_index, load_source_class, load_target_class, percent_poison, epsilon)
 
             trigger_index = list(load_trigger_index)
             source_class = list(load_source_class)
