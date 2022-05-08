3d2e04bb37cab71db46fd91bcb0b055297a3b9bc
David Slater <david.slater@twosixlabs.com>
2022-05-08 15:54:43 -0700

    supported metrics references

--- armory/metrics/__init__.py
+++ armory/metrics/__init__.py
@@ -1,1 +1,25 @@
 from armory.metrics import compute, perturbation, statistical, task
+
+SUPPORTED_METRICS = {}
+for namespace in (
+    perturbation.batch,
+    task.batch,
+    task.aggregate,
+    task.population,
+    statistical.registered,
+):
+    assert not any(k in namespace for k in SUPPORTED_METRICS)
+    SUPPORTED_METRICS.update(namespace)
+
+
+def get(name):
+    try:
+        function = SUPPORTED_METRICS[name]
+    except KeyError:
+        raise KeyError(f"{name} is not part of armory.metrics")
+    if isinstance(function, type) and issubclass(function, object):
+        # If a class is given, instantiate it
+        function = function()
+    assert callable(function), f"function {name} is not callable"
+    # TODO: add loading of non-armory metrics
+    return function
--- armory/utils/metrics.py
+++ armory/utils/metrics.py
@@ -2,28 +2,4 @@
 Metrics for scenarios
 """
 
-from armory.metrics import perturbation, task, statistical
-
-
-SUPPORTED_METRICS = {}
-for namespace in (
-    perturbation.batch,
-    task.batch,
-    task.aggregate,
-    task.population,
-    statistical.registered,
-):
-    assert not any(k in namespace for k in SUPPORTED_METRICS)
-    SUPPORTED_METRICS.update(namespace)
-
-
-def get_supported_metric(name):
-    try:
-        function = SUPPORTED_METRICS[name]
-    except KeyError:
-        raise KeyError(f"{name} is not part of armory.metrics")
-    if isinstance(function, type) and issubclass(function, object):
-        # If a class is given, instantiate it
-        function = function()
-    assert callable(function), f"function {name} is not callable"
-    return function
+from armory.metrics import get as get_supported_metric  # noqa: F401
