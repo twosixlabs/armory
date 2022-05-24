580aee6fd9cc952268d5ae0e6c4cc5249f8e1718
lucas.cadalzo <lucas.cadalzo@twosixlabs.com>
2022-05-24 10:51:04 -0400

    asr exporter uses self.test_dataset, so we must load datasets before export meters

--- armory/scenarios/scenario.py
+++ armory/scenarios/scenario.py
@@ -265,13 +265,13 @@ class Scenario:
         )
 
     def load(self):
+        self.load_dataset()
         self.load_export_meters()
         self.load_model()
         if self.use_fit:
             self.load_train_dataset()
             self.fit()
         self.load_attack()
-        self.load_dataset()
         self.load_metrics()
         return self
 
