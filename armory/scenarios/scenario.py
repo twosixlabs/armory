3093ef0b4c7e22284b459a32d2522f8b74e9985a
lucas.cadalzo <lucas.cadalzo@twosixlabs.com>
2022-05-24 11:55:46 -0400

    set load() order back to what it is in 0.15.0; the previous reordering is no longer necessary due to subsequent commits that refactored out the dependency

--- armory/scenarios/scenario.py
+++ armory/scenarios/scenario.py
@@ -265,14 +265,14 @@ class Scenario:
         )
 
     def load(self):
-        self.load_dataset()
-        self.load_export_meters()
         self.load_model()
         if self.use_fit:
             self.load_train_dataset()
             self.fit()
         self.load_attack()
+        self.load_dataset()
         self.load_metrics()
+        self.load_export_meters()
         return self
 
     def evaluate_all(self):
