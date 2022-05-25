f96eaf4d366cb316219fcb6a37b41235d37935fb
lucas.cadalzo <lucas.cadalzo@twosixlabs.com>
2022-05-25 15:09:04 -0400

    don't set y_pred_clean_probe if self.skip_benign

--- armory/scenarios/scenario.py
+++ armory/scenarios/scenario.py
@@ -256,7 +256,7 @@ class Scenario:
             "pred_dict_exporter",
             self.export_dir,
             y_probe="scenario.y",
-            y_pred_clean_probe="scenario.y_pred",
+            y_pred_clean_probe="scenario.y_pred" if not self.skip_benign else None,
             y_pred_adv_probe="scenario.y_pred_adv" if not self.skip_attack else None,
             max_batches=self.num_export_batches,
         )
