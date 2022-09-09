09607cf914f2aa936713c05d8eac3f245d6269ff
Christopher Woodall <woodall.christopher@gmail.com>
2022-09-09 16:32:20 -0400

    Implement changes

--- tests/end_to_end/test_check_results.py
+++ tests/end_to_end/test_check_results.py
@@ -6,14 +6,14 @@
 Example:
     $ pytest --verbose --show-capture=no tests/end_to_end/test_check_results.py
 
-Todo:
-    * Decalare this test as being '--no-docker'. Use pytest mark. [woodall]
+TODO: Decalare this test as being '--no-docker'. Use pytest mark. [woodall]
 """
 
-import re
 import json
+import math
 import pprint
 import pytest
+import statistics
 
 from pathlib import Path
 
@@ -24,8 +24,9 @@ from armory.__main__ import run
 # Marks all tests in this file as `end_to_end`
 pytestmark = pytest.mark.end_to_end
 
+test_data     = Path(Path(__file__).resolve().parent / 'test_check_results/model_scores.json')
 host_paths    = paths.runtime_paths()
-result_path   = Path(host_paths.output_dir)
+results_path  = Path(host_paths.output_dir)
 
 pp = pprint.PrettyPrinter(indent=4, width=80, compact=True)
 
@@ -33,75 +34,61 @@ pp = pprint.PrettyPrinter(indent=4, width=80, compact=True)
 @pytest.fixture(scope="module", autouse=True)
 def setup():
     """Pre-flight checks. Future tests will  not continue if setup fails.
-
-    Raises:
-        Exception: If `scenario_path` or `result_path` are missing.
     """
-    if not result_path.exists():
+    if not all([Path(d).exists() for d in [results_path, test_data]]):
         raise Exception(f"Required application paths do not exist!")
-        pytest.exit("Previous tests indicate catastrophic failure.")
 
 
-@pytest.fixture(scope="module")
-def results():
-    """Results generator.
 
-    Returns:
-        func: Generator function that yields a results json file.
-    """
-    def results_generator():
-        """Loops over the results from the user's armory directory.
+def test_results(capsys):
+    scores = {}
+    model_results = json.loads(test_data.read_text())
 
-        Yields:
-            dict: Result from the user's armory directory.
+    result_tolerance = 0.05
 
-        Raises:
-            AssertionError: If the result file is invalid.
+    for result in results_path.glob("**/*.json"):
 
-        Example:
-            >>> print([result for result in results_generator()])
-                [{<result>}, {<result>}, ...]
-        """
-        for result in result_path.glob("**/*.json"):
-            try:
-                yield json.loads(result.read_text())
-            except json.JSONDecodeError:
-                assert False, f"Invalid JSON file: {result}"
-                continue
-    return results_generator
+        result_json = json.loads(result.read_text())
+
+        results = result_json['results']
+
+        filepath = result_json['config']['sysconfig']['filepath']
+        check_used = result_json['config']['sysconfig']['use_gpu']
+        gpu_used = result_json['config']['sysconfig']['use_gpu']
 
 
-def test_scenarios(capsys, results):
-    """Result testing.
+        adversarial_scores = results['adversarial_mean_categorical_accuracy']
+        benign_scores      = results['benign_mean_categorical_accuracy']
 
-    Args:
-        results (Iterator): Generator for results files.
-        capsys  (:pytest.CaptureFixture:`str`): PyYest builtin to capture stdin/stdout.
-    """
 
-    for result in results():
-        try:
-            _config     = result['config']
-            config      = _config['sysconfig']['filepath']
-            scenario    = config.split("/")[-1].split(".")[0].replace("_", " ").title()
-            description = _config['_description']
-            dataset     = _config['dataset']['name']
-            gpu_used    = _config['sysconfig']['use_gpu']
-            results     = result['results']
+        adversarial_median = statistics.median(adversarial_scores)
+        benign_median      = statistics.median(benign_scores)
 
-            with capsys.disabled():
-                print("\n".join([
-                        f"\n\n{'=' * 80}",
-                        f"Scenario:\t{scenario}",
-                        f"Description:\t{description}",
-                        f"Config:\t\t{config}",
-                        f"Dataset:\t{dataset}",
-                        f"GPU Used:\t{gpu_used}",
-                        f"{'-' * 80}",
-                        f"Results:",
-                        pp.pformat(results),
-                        f"{'=' * 80}"
-                    ]))
 
-        except Exception as e:
-            assert False, f"Invalid result file: {result}"
+        with capsys.disabled():
+            print("\n\n")
+            print(math.isclose(adversarial_median, benign_median, abs_tol = result_tolerance))
+            print(all((
+                math.isclose(bt, at, abs_tol = result_tolerance) for at, bt in
+                    zip(adversarial_scores, benign_scores)
+                    if at != 0.0 and bt != 0.0
+            )))
+
+        # SETUP
+        scores[filepath] = {
+            'delta': 0,
+            'results': [
+                {
+                'tolerance': result_tolerance,
+                'adversarial_median': adversarial_median,
+                'benign_median': benign_median,
+                'check_used': check_used,
+                'gpu_used': gpu_used
+                }
+            ]
+        }
+        # /SETUP
+
+    with capsys.disabled():
+        print(json.dumps(scores, indent=2))
+
