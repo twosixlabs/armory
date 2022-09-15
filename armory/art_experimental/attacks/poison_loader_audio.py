0d8019659811cda8956ab33d5af006cababe1a3f
Sterling <sterling.suggs@twosixtech.com>
2022-09-15 17:54:03 +0000

    format

--- armory/art_experimental/attacks/poison_loader_audio.py
+++ armory/art_experimental/attacks/poison_loader_audio.py
@@ -1,5 +1,8 @@
 from art.attacks.poisoning import PoisoningAttackBackdoor
-from art.attacks.poisoning.perturbations.audio_perturbations import insert_audio_trigger, insert_tone_trigger
+from art.attacks.poisoning.perturbations.audio_perturbations import (
+    insert_audio_trigger,
+    insert_tone_trigger,
+)
 
 
 def poison_loader_audio(**kwargs):
@@ -7,14 +10,18 @@ def poison_loader_audio(**kwargs):
     backdoor_kwargs = kwargs.pop("backdoor_kwargs")
 
     if "backdoor_path" in backdoor_kwargs:
+
         def poison_func(x):
             return insert_audio_trigger(x, **backdoor_kwargs)
+
     elif "frequency" in backdoor_kwargs:
+
         def poison_func(x):
             return insert_tone_trigger(x, **backdoor_kwargs)
-    else:
-        raise ValueError("backdoor_kwargs should include either \"frequency\" or \"backdoor_path\"")
 
+    else:
+        raise ValueError(
+            'backdoor_kwargs should include either "frequency" or "backdoor_path"'
+        )
 
     return PoisoningAttackBackdoor(poison_func, **kwargs)
-
--- armory/baseline_models/tf_graph/audio_cnn.py
+++ armory/baseline_models/tf_graph/audio_cnn.py
@@ -15,8 +15,7 @@ from art.estimators.classification import TensorFlowV2Classifier
 
 def get_spectrogram(audio):
     waveform = tf.convert_to_tensor(audio, dtype=tf.float32)
-    spectrogram = tf.signal.stft(
-                      waveform, frame_length=255, frame_step=128)
+    spectrogram = tf.signal.stft(waveform, frame_length=255, frame_step=128)
     spectrogram = tf.abs(spectrogram)
     spectrogram = spectrogram[..., tf.newaxis]
     return spectrogram  # shape (124, 129, 1)
@@ -24,34 +23,39 @@ def get_spectrogram(audio):
 
 def make_audio_cnn(**kwargs) -> tf.keras.Model:
     """
-        A CNN for audio poisoning on the speech commands dataset
+    A CNN for audio poisoning on the speech commands dataset
     """
     norm_layer = layers.Normalization()
     input_shape = (16000,)
     num_labels = 12
-    model = models.Sequential([
-        layers.Input(shape=input_shape),
-        layers.Lambda(get_spectrogram),
-        layers.Resizing(32, 32),
-        norm_layer,
-        layers.Conv2D(32, 3, activation='relu'),
-        layers.Conv2D(64, 3, activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Dropout(0.25),
-        layers.Flatten(),
-        layers.Dense(128, activation='relu'),
-        layers.Dropout(0.5),
-        layers.Dense(num_labels),
-    ])
+    model = models.Sequential(
+        [
+            layers.Input(shape=input_shape),
+            layers.Lambda(get_spectrogram),
+            layers.Resizing(32, 32),
+            norm_layer,
+            layers.Conv2D(32, 3, activation="relu"),
+            layers.Conv2D(64, 3, activation="relu"),
+            layers.MaxPooling2D(),
+            layers.Dropout(0.25),
+            layers.Flatten(),
+            layers.Dense(128, activation="relu"),
+            layers.Dropout(0.5),
+            layers.Dense(num_labels),
+        ]
+    )
 
     return model
+
 
 def get_art_model(
     model_kwargs: dict, wrapper_kwargs: dict, weights_path: Optional[str] = None
 ):
 
     if weights_path:
-        raise ValueError("This model is implemented for poisoning and does not (yet) load saved weights.")
+        raise ValueError(
+            "This model is implemented for poisoning and does not (yet) load saved weights."
+        )
 
     model = make_audio_cnn(**model_kwargs)
     optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
@@ -65,15 +69,13 @@ def get_art_model(
 
     loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-    art_classifier = TensorFlowV2Classifier(model=model, 
-                                        loss_object=loss_object, 
-                                        train_step=train_step,
-                                        input_shape=(16000,),
-                                        nb_classes=12,
-                                        **wrapper_kwargs,
-                                        )
+    art_classifier = TensorFlowV2Classifier(
+        model=model,
+        loss_object=loss_object,
+        train_step=train_step,
+        input_shape=(16000,),
+        nb_classes=12,
+        **wrapper_kwargs,
+    )
 
     return art_classifier
-
-
-
