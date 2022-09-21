18b325ea5f55fe2dce32318031069cde7ebecaa2
christopherwoodall <christopher.woodall@twosixtech.com>
2022-09-21 16:59:35 -0400

    Prettify

--- armory/__init__.py
+++ armory/__init__.py
@@ -5,9 +5,8 @@ Adversarial Robustness Evaluation Test Bed
 from armory.logs import log
 from armory.utils import version, typedef
 
-__all__ = ("__version__", "Config")
-
-__version__ = version.get_version()
+__all__: set     = ("__version__", "Config")
+__version__: str = version.get_version()
 
 
 Config = typedef.Config
