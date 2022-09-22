33f8bc3626ab247356f1272b006bbdd2b2254be0
christopherwoodall <christopher.woodall@twosixtech.com>
2022-09-22 11:35:49 -0400

    pause until fixed in upstream:develop branch

--- tools/pre-commit.sh
+++ tools/pre-commit.sh
@@ -1,28 +1,38 @@
 #!/usr/bin/env bash
 
-python -m black --check ./ > /dev/null 2>&1
-need_format=$?
-set -e
-if [ $need_format -ne 0 ]
-then
-    python -m black ./
-    echo Some Python files were formatted
-    echo You need to do git add and git commit again
-    exit $need_format
-fi
-set +e
+ARMORY_ROOT_DIR=`git rev-parse --show-toplevel`
+ARMORY_SRC_DIR="${ARMORY_ROOT_DIR}/armory"
 
-python -m tools.format_json --check > /dev/null 2>&1
-need_format=$?
-set -e
-if [ $need_format -ne 0 ]
-then
-    python -m tools.format_json
-    echo Some JSON files were formatted
-    echo You need to do git add and git commit again
-    exit $need_format
-fi
+# TODO: Style Guides(?)
+#     - PEP8: https://peps.python.org/pep-0008/
+#     - Google Python Style Guide: https://google.github.io/styleguide/pyguide.html
+python -m black \
+  --config "${ARMORY_ROOT_DIR}/pyproject.toml" \
+  --check \
+  "${ARMORY_SRC_DIR}" > /dev/null 2>&1
 
-yamllint --no-warnings ./
+# need_format=$?
+# set -e
+# if [ $need_format -ne 0 ]
+# then
+#     python -m black ./
+#     echo Some Python files were formatted
+#     echo You need to do git add and git commit again
+#     exit $need_format
+# fi
+# set +e
 
-python -m flake8
+# python -m tools.format_json --check > /dev/null 2>&1
+# need_format=$?
+# set -e
+# if [ $need_format -ne 0 ]
+# then
+#     python -m tools.format_json
+#     echo Some JSON files were formatted
+#     echo You need to do git add and git commit again
+#     exit $need_format
+# fi
+
+# yamllint --no-warnings ./
+
+# python -m flake8
