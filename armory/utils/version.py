5ab5548a158d698ef3a2493cfc19d12353a6b2be
matt wartell <matt.wartell@twosixtech.com>
2022-10-07 08:00:54 -0400

    merge #1645 into #1635 for testing
    I need to see if this helps or alters the CI failure in #1635

--- armory/utils/version.py
+++ armory/utils/version.py
@@ -42,15 +42,19 @@ def get_metadata_version(package: str = PYPI_PACKAGE_NAME) -> str:
 
 
 def get_tag_version(git_dir: Path = None) -> str:
-    """Retrieve the version from the most recent git tag"""
+    """Retrieve the version from the most recent git tag, return empty string on
+    failure"""
     project_root = Path(__file__).parent.parent.parent
     scm_config = {
         "root": project_root,
         "version_scheme": "post-release",
     }
-    if not Path(project_root / ".git").is_dir():
-        raise LookupError("Unable to find `.git` directory!")
-    return setuptools_scm.get_version(**scm_config)
+
+    try:
+        # setuptools_scm is really persnickety, this fails often
+        return setuptools_scm.get_version(**scm_config)
+    except LookupError:
+        return ""
 
 
 def get_build_hook_version() -> str:
