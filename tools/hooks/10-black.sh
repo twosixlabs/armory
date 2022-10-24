975f15271bca4ce551ed5c67ea4a96a6ee22c291
Christopher Woodall <woodall.christopher@gmail.com>
2022-10-24 11:55:26 -0400

    update hook

--- tools/hooks/10-black.sh
+++ tools/hooks/10-black.sh
@@ -4,12 +4,14 @@
 
 echo "Executing 'black' formatter..."
 
-if python -m black --check ./; then
-  exit 0
-else
-  python -m black ./
-  # git add -u
-  echo "Some files were formatted."
-  # echo "You need to do git add and git commit again."
-  exit 1
-fi
+pushd "`git rev-parse --git-dir`/../" > /dev/null
+  if black --check --diff --color .; then
+    exit 0
+  else
+    python -m black ./
+    # git add -u
+    echo "Some files were formatted."
+    # echo "You need to do git add and git commit again."
+    exit 1
+  fi
+popd
