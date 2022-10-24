2fd7c3ab4f659c586d9ccd7f59faf73def8fb7e0
Christopher Woodall <woodall.christopher@gmail.com>
2022-10-24 12:06:37 -0400

    add json hook

--- tools/hooks/30-json.sh
+++ tools/hooks/30-json.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
-echo "Executing 'black' formatter..."
+echo "Executing 'json' formatter..."
 
 pushd `git rev-parse --show-toplevel` > /dev/null
-
+    for FILE in `git ls-tree --full-tree --name-only -r HEAD | grep -P '\.((js)|(json))$'`; do
+        python -mjson.tool $FILE 2> /dev/null
+        if [ $? -ne 0 ] ; then
+            echo "Lint check of JSON object failed. Your changes were not commited."
+            echo "in $git_dir/$file:"
+            python -mjson.tool $file
+            exit 1
+        fi
+    done
 popd
--- tools/pre-commit.sh
+++ tools/pre-commit.sh
@@ -8,23 +8,13 @@ PROJECT_ROOT=`git rev-parse --show-toplevel`
 pushd $PROJECT_ROOT > /dev/null
 
     . ./tools/hooks/10-black.sh
+    . ./tools/hooks/30-json.sh
 
+    # TODO: Auto add files back?
+    # git add -u
 popd > /dev/null
 
-
-# python -m tools.format_json --check > /dev/null 2>&1
-# need_format=$?
-# set -e
-# if [ $need_format -ne 0 ]
-# then
-#     python -m tools.format_json
-#     echo Some JSON files were formatted
-#     echo You need to do git add and git commit again
-#     exit $need_format
-# fi
 
 # yamllint --no-warnings ./
 
 # python -m flake8
-
-
