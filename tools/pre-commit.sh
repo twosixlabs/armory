a28a2f330776d4a02393e76780398073339fc360
Christopher Woodall <woodall.christopher@gmail.com>
2022-10-24 12:31:29 -0400

    dynamically load hooks

--- tools/pre-commit.sh
+++ tools/pre-commit.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Copy or link this script into .git/hooks/
+#   $ ln -s tools/pre-commit.sh .git/hooks/pre-commit.sh
 # It runs automatically in the project root directory (parent of .git/).
 
 EXIT_STATUS=0
@@ -7,17 +8,20 @@ PROJECT_ROOT=`git rev-parse --show-toplevel`
 
 
 pushd $PROJECT_ROOT > /dev/null
-
-    . ./tools/hooks/10-black.sh
-    . ./tools/hooks/30-json.sh
+    for FILE in `ls -1 ./tools/hooks/*.sh | sort`; do
+        echo "Importing ${FILE}..."
+        . "${FILE}"
+        if [ $? -ne 0 ]; then
+            EXIT_STATUS=1
+        fi
+    done
 
     # TODO: Auto add files back?
     # git add -u
 popd > /dev/null
 
+if [ $EXIT_STATUS -ne 0 ]; then
+  echo "🚨 Pre-commit hooks failed. Please run 'pre-commit run --all-files' locally to fix the issues 🚑"
+fi
 
 exit $EXIT_STATUS
-
-# yamllint --no-warnings ./
-
-# python -m flake8
