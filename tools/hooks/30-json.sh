2b3598faa8c0f66e171f3d9d89f8c01252923df2
Christopher Woodall <woodall.christopher@gmail.com>
2022-10-25 07:09:18 -0400

    Fix git directory

--- tools/hooks/30-json.sh
+++ tools/hooks/30-json.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 echo "Executing 'json' formatter..."
 
+PROJECT_ROOT=`git rev-parse --show-toplevel`
+
 pushd `git rev-parse --show-toplevel` > /dev/null
     for FILE in `git ls-tree --full-tree --name-only -r HEAD | grep -P '\.((js)|(json))$'`; do
         python -mjson.tool $FILE 2>&1 /dev/null
         if [ $? -ne 0 ] ; then
             echo "Lint check of JSON object failed. Your changes were not commited."
-            echo "in $git_dir/${FILE}:"
+            echo "in ${PROJECT_ROOT}/${FILE}:"
             python -mjson.tool "${FILE}"
             EXIT_STATUS=1
         fi
