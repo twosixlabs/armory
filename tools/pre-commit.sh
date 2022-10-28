f9072b965b1ec34e24032e9d4c5b05ff706d0e32
Christopher Woodall <woodall.christopher@gmail.com>
2022-10-28 11:21:49 -0400

    add CI flag

--- tools/pre-commit.sh
+++ tools/pre-commit.sh
@@ -6,8 +6,18 @@
 EXIT_STATUS=0
 PROJECT_ROOT=`git rev-parse --show-toplevel`
 
+# Environmental variable used to notify hooks we are running in a workflow.
+CI_TEST="${CI_TEST:-0}"
+
+# Collect tracked files based on CI_TEST
+TRACKED_FILES="git diff HEAD --name-only"
+if [ "${CI_TEST}" -ne 0 ]; then
+    TRACKED_FILES="git ls-files"
+fi
+
 
 pushd $PROJECT_ROOT > /dev/null
+    # Source hooks
     for FILE in `ls -1 ./tools/hooks/*.sh | sort`; do
         echo "Importing ${FILE}..."
         source "${FILE}"
@@ -17,8 +27,10 @@ pushd $PROJECT_ROOT > /dev/null
     done
 popd > /dev/null
 
-if [ $EXIT_STATUS -ne 0 ]; then
+
+if [ "${EXIT_STATUS}" -ne 0 ]; then
   echo "🚨 Pre-commit hooks failed. Please run 'pre-commit run --all-files' locally to fix the issues 🚑"
 fi
+
 
 exit $EXIT_STATUS
