f1b398bb5356a00fe6cd3d406d4ad68547d6e0e7
Christopher Woodall <woodall.christopher@gmail.com>
2022-10-28 11:22:15 -0400

    filter and use tracked list

--- tools/hooks/10-black.sh
+++ tools/hooks/10-black.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 echo "Executing 'black' formatter..."
 
-cd `git rev-parse --show-toplevel` > /dev/null
-  python -m black --check --diff --color {armory,tests,docker}/*.py
+TARGET_FILES=`$TRACKED_FILES | grep -P '^((armory)|(tests)|(docker)).*\.py$'`
+
+pushd $PROJECT_ROOT > /dev/null
+  python -m black --check --diff --color $TARGET_FILES
   if [ $? -ne 0 ]; then
-    python -m black ./
+    python -m black $TARGET_FILES
     echo "Some files were formatted."
     echo "You need to do git add and git commit again."
     EXIT_STATUS=1
   fi
-cd -
+popd
