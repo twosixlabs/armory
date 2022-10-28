1e11158d82e60889e10d5d21634b7b7a1bd05af8
Christopher Woodall <woodall.christopher@gmail.com>
2022-10-28 11:24:54 -0400

    filter tracked files

--- tools/hooks/30-json.sh
+++ tools/hooks/30-json.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "Executing 'json' formatter..."
 
-PROJECT_ROOT=`git rev-parse --show-toplevel`
+TARGET_FILES=`$TRACKED_FILES | grep -P '\.json'`
 
 pushd $PROJECT_ROOT > /dev/null
-    for FILE in `git diff HEAD --name-only | grep -P '\.json'`; do
+    for FILE in ${TARGET_FILES}; do
         python -mjson.tool $FILE 2>&1 /dev/null
         if [ $? -ne 0 ] ; then
             echo "Lint check of JSON object failed. Your changes were not commited."
