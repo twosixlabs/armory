377073e3201a64b82af4942a536d4782fe83f5f9
Christopher Woodall <woodall.christopher@gmail.com>
2022-10-29 09:38:55 -0400

    add `sort-keys` flag to `json-lint`

--- tools/hooks/30-json.sh
+++ tools/hooks/30-json.sh
@@ -5,7 +5,7 @@ TARGET_FILES=`$TRACKED_FILES | grep -E '\.json'`
 
 pushd $PROJECT_ROOT > /dev/null
     for FILE in ${TARGET_FILES}; do
-        python -mjson.tool $FILE 2>&1 /dev/null
+        python -m json.tool --sort-keys $FILE 2>&1 /dev/null
         if [ $? -ne 0 ] ; then
             echo "Lint check of JSON object failed. Your changes were not commited."
             echo "in ${PROJECT_ROOT}/${FILE}:"
