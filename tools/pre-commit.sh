f981de520f786caffce07a4c9be8a7765633c793
Christopher Woodall <woodall.christopher@gmail.com>
2022-11-07 12:35:57 -0500

    use 4 spaces

--- tools/pre-commit.sh
+++ tools/pre-commit.sh
@@ -17,66 +17,66 @@ ARMORY_CI_TEST="${ARMORY_COMMIT_HOOK_CI:-0}"
 
 TRACKED_FILES="git --no-pager diff --name-only"
 if [ "${ARMORY_CI_TEST}" -ne 0 ]; then
-  TRACKED_FILES="git --no-pager ls-files"
+    TRACKED_FILES="git --no-pager ls-files"
 fi
 [ -z "$TRACKED_FILES" ] && exit 0
 
 
 function CHECK_EXIT_STATUS ()
 {
-  if [ $1 -ne 0 ]; then
-    EXIT_STATUS=1
-    if [ $EXIT_EARLY -ne 0 ]; then
-      echo "🚨 Pre-commit hooks failed. Please fix the issues and re-run 'git add' and 'git commit' 🚑"
-      exit ${EXIT_STATUS}
+    if [ $1 -ne 0 ]; then
+        EXIT_STATUS=1
+        if [ $EXIT_EARLY -ne 0 ]; then
+            echo "🚨 Pre-commit hooks failed. Please fix the issues and re-run 'git add' and 'git commit' 🚑"
+            exit ${EXIT_STATUS}
+        fi
     fi
-  fi
 }
 
 
 pushd $PROJECT_ROOT > /dev/null || exit 1
-  ############
-  # Black
-  echo "⚫ Executing 'black' formatter..."
-  TARGET_FILES=`${TRACKED_FILES} | grep -E '\.py$' | sed 's/\n/ /g'`
-  [ -z "$TARGET_FILES" ] && exit 0
-  python -m black --check --diff --color ${TARGET_FILES}
-  if [ $? -ne 0 ]; then
-    python -m black $TARGET_FILES
-    echo "⚫ some files were formatted."
-    CHECK_EXIT_STATUS 1
-  fi
-
-  ############
-  # Flake8
-  echo "🎱 Executing 'flake8' formatter..."
-  python -m flake8 --config=.flake8 ${TARGET_FILES}
-  CHECK_EXIT_STATUS $?
-
-  ############
-  # JSON Linting
-  echo "📄 Executing 'json' formatter..."
-  TARGET_FILES=`${TRACKED_FILES} | sed 's/ /\n/g' | grep -E '.*\.json$'`
-  for TARGET_FILE in ${TARGET_FILES}; do
-    # Check if file is too large to be linted
-    FILE_SIZE=`du -m ${TARGET_FILE} | cut -f1`
-    if [ ${FILE_SIZE} -gt ${MAX_FILE_SIZE} ]; then
-      echo "⚫ Skipping ${TARGET_FILE} (too large)"
-      continue
+    ############
+    # Black
+    echo "⚫ Executing 'black' formatter..."
+    TARGET_FILES=`${TRACKED_FILES} | grep -E '\.py$' | sed 's/\n/ /g'`
+    [ -z "$TARGET_FILES" ] && exit 0
+    python -m black --check --diff --color ${TARGET_FILES}
+    if [ $? -ne 0 ]; then
+        python -m black $TARGET_FILES
+        echo "⚫ some files were formatted."
+        CHECK_EXIT_STATUS 1
     fi
-    python -mjson.tool --sort-keys --indent=4 ${TARGET_FILE} 2>&1 | diff - ${TARGET_FILE}
-    if [ $? -ne 0 ] ; then
-      JSON_PATCH="`python -mjson.tool --sort-keys --indent=4 ${TARGET_FILE}`"
-      echo "${JSON_PATCH}" > ${TARGET_FILE}    # The double quotes are important here!
-      echo "📄 modified ${PROJECT_ROOT}/${TARGET_FILE}"
-      CHECK_EXIT_STATUS 1
-    fi
-  done
+
+    ############
+    # Flake8
+    echo "🎱 Executing 'flake8' formatter..."
+    python -m flake8 --config=.flake8 ${TARGET_FILES}
+    CHECK_EXIT_STATUS $?
+
+    ############
+    # JSON Linting
+    echo "📄 Executing 'json' formatter..."
+    TARGET_FILES=`${TRACKED_FILES} | sed 's/ /\n/g' | grep -E '.*\.json$'`
+    for TARGET_FILE in ${TARGET_FILES}; do
+        # Check if file is too large to be linted
+        FILE_SIZE=`du -m ${TARGET_FILE} | cut -f1`
+        if [ ${FILE_SIZE} -gt ${MAX_FILE_SIZE} ]; then
+            echo "⚫ Skipping ${TARGET_FILE} (too large)"
+            continue
+        fi
+        python -mjson.tool --sort-keys --indent=4 ${TARGET_FILE} 2>&1 | diff - ${TARGET_FILE}
+        if [ $? -ne 0 ] ; then
+            JSON_PATCH="`python -mjson.tool --sort-keys --indent=4 ${TARGET_FILE}`"
+            echo "${JSON_PATCH}" > ${TARGET_FILE}    # The double quotes are important here!
+            echo "📄 modified ${PROJECT_ROOT}/${TARGET_FILE}"
+            CHECK_EXIT_STATUS 1
+        fi
+    done
 popd > /dev/null
 
 
 if [ "${EXIT_STATUS}" -ne 0 ]; then
-  echo "🚨 Pre-commit hooks failed. Please fix the issues and re-run 'git add' and 'git commit' 🚑"
+    echo "🚨 Pre-commit hooks failed. Please fix the issues and re-run 'git add' and 'git commit' 🚑"
 fi
 
 
