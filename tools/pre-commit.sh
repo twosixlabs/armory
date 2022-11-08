290e55612cef4b21219bb36b048b0d3f0abac97a
Christopher Woodall <christopher.woodall@twosixtech.com>
2022-11-08 09:22:49 -0500

    Update pre-commit hooks (#1708)
    * update hooks
    * update pathing
    
    * add json hook
    
    * remove `git add`
    
    * fail with global exit status
    
    * dynamically load hooks
    
    * update json formatting
    
    * centralize job into pre-commit
    
    * add yaml and flake8 formatter
    
    * update instructions
    
    * do not track yamllint or flake8
    
    * Resolve TODO
    
    * remove yaml lint
    
    * call black as module
    
    * Fix git directory
    
    * remove dependencies
    
    * disable yamllint
    
    * remove inline linter rules
    
    * Add scenario runner to CI (#1704)
    
    * add option to pass params and env to test
    
    * Create test_e2e_scenarios.py
    
    * adds scenario eval environment setup
    
    * Update end2end_cron.yml
    
    * add return to run method
    
    * Create scenario-evaluation-config.json
    
    * Fix decoding errors
    
    * make purely* cron
    
    *plus dispatch
    
    * clean up
    
    * fix linting issues
    
    * update bug report
    
    * fix linting
    
    * more yaml linting
    
    * update e2e cron
    
    * remove previous runner code
    
    * remove capsys context manager
    
    * add noqa sections
    
    * more qa fixes
    
    * even more qa
    
    * fix ci versioning
    
    * tests need test
    
    * use dispatch instead of cron
    
    * update cache key
    
    * update cache key
    
    * update description
    
    * update directory names
    
    * add `--interactive`
    
    * make `true` the default command for `docker run`
    
    * args are already a tuple
    
    * add comment
    
    * remove constructor, use symbols
    
    * fix args
    
    * Revert "Add scenario runner to CI (#1704)"
    
    This reverts commit c844b55915bafaa2ff98238c04be174b7911d615.
    
    * revert merge
    
    * replace pushd with cd
    
    * remove dead code
    
    * limit linting to project directories
    
    * spell out source
    
    * clean up newlines
    
    * inherit pre-commit variable
    
    * add CI flag
    
    * filter and use tracked list
    
    * filter tracked files
    
    * update ci flag name to be more specific
    
    * update error message
    
    * move env flag from code to yaml
    
    * revert changes
    
    * cleanup and trigger
    
    * remove trigger
    
    * remove `yamllint` from docs
    
    * update exit status
    
    * link pre-commit instead of copying it over
    
    keeps updates centralized to `tools/pre-commit.sh`
    
    * use cp
    
    * remove yamllint
    
    * update grep flag
    
    * set executable bit on scripts
    
    * tell the user to fix the issues
    
    * add `sort-keys` flag to `json-lint`
    
    * test json functionality
    
    * explicit targeting of json
    
    * update json hook
    
    * re-enable hook directory
    
    * update docs
    
    * add pushd checks
    
    * add check for empty target files
    
    * fix line endings
    
    * decrease verbosity
    
    * remove cached
    
    * misc fixes
    
    * fix spacing and exit_status
    
    * add anchor
    
    * change ci variable name
    
    * update ci
    
    * pass target files to flake
    
    * remove dead code
    
    * add global exit status
    
    * update language
    
    * update example
    
    * add file size check
    
    * add a method to exit early
    
    * fix indentation
    
    * use 4 spaces
    
    * execute python linting in own shell
    
    * fix emojis
    
    * fix black check
    
    * update style with early exit
    
    * remove directory prefix
    
    * make json output bold
    
    * ensure we do not write back empty json
    
    * use tput
    
    * fix echo
    
    * exit from shell if no files found
    
    * fix subshell exit
    
    * clean up spacing
    
    * more beautification
    
    * cast exit status
    
    * tag variable
    
    * update pre-commit hook installation
    
    * fix tput error in ci
    
    * update `TERM` if empty
    
    * update ci fix comment
    
    * update spacing
    
    Co-authored-by: matt wartell <matt.wartell@twosixtech.com>
    Co-authored-by: matt wartell <matt.wartell@twosixlabs.com>

--- tools/pre-commit.sh
+++ tools/pre-commit.sh
@@ -1,28 +1,107 @@
 #!/usr/bin/env bash
+# Copy or link this script into .git/hooks/
+#   $ cp "`pwd`/tools/pre-commit.sh" "`pwd`/.git/hooks/pre-commit"
+# This script runs automatically in the project's root directory (parent of .git/).
 
-python -m black --check ./ > /dev/null 2>&1
-need_format=$?
-set -e
-if [ $need_format -ne 0 ]
-then
-    python -m black ./
-    echo Some Python files were formatted
-    echo You need to do git add and git commit again
-    exit $need_format
+EXIT_STATUS=0
+EXIT_EARLY=0
+MAX_FILE_SIZE=32 # Max JSON file size in Megabytes.
+PROJECT_ROOT=`git rev-parse --show-toplevel`
+
+# Determine command to use in order to collect
+# tracked files. Toggle between `dif` and `lstree`
+# using the `ARMORY_CI_TEST` environmental variable.
+# Example:
+#   $ ARMORY_CI_TEST=1 ./tools/pre-commit.sh
+ARMORY_CI_TEST="${ARMORY_COMMIT_HOOK_CI:-0}"
+
+TRACKED_FILES="git --no-pager diff --name-only"
+if [ "${ARMORY_CI_TEST}" -ne 0 ]; then
+    TRACKED_FILES="git --no-pager ls-files"
 fi
-set +e
+[ -z "$TRACKED_FILES" ] && exit 0
 
-python -m tools.format_json --check > /dev/null 2>&1
-need_format=$?
-set -e
-if [ $need_format -ne 0 ]
-then
-    python -m tools.format_json
-    echo Some JSON files were formatted
-    echo You need to do git add and git commit again
-    exit $need_format
+
+# Fixes Github Action error, "tput: No value for $TERM and no -T specified",
+# that occurs When $TERM is empty (non-interactive shell) by faking a value
+# for the terminal profile.
+[[ ${TERM}=="" ]] && export TERM='xterm-256color'
+
+
+function CHECK_EXIT_STATUS ()
+{
+    if [ $1 -ne 0 ]; then
+        EXIT_STATUS=1
+        if [ $EXIT_EARLY -ne 0 ]; then
+            echo "🚨 Pre-commit hooks failed. Please fix the issues and re-run 'git add' and 'git commit' 🚑"
+            exit ${EXIT_STATUS}
+        fi
+    fi
+}
+
+
+pushd $PROJECT_ROOT > /dev/null || exit 1
+    # Execute python pre-commit hooks in seperate processes
+    # so that json linting can still occur.
+    echo "🐍 $(tput bold)executing python pre-commit hooks$(tput sgr0)"
+    (   PRE_COMMIT_EXIT_STATUS=0
+        echo "📁 collecting files to lint"
+        TARGET_FILES=`${TRACKED_FILES} | grep -E '\.py$' | sed 's/\n/ /g'`
+        if [ -z "$TARGET_FILES" ]; then
+            echo "📁 $(tput bold)no python files to check$(tput sgr0)"
+            exit 0
+        fi
+
+        ############
+        # Black
+        echo "⚫ Executing 'black' formatter..."
+        python -m black --check > /dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            python -m black $TARGET_FILES
+            echo "⚫ some files were formatted."
+            PRE_COMMIT_EXIT_STATUS=1
+        fi
+
+        ############
+        # Flake8
+        echo "🎱 Executing 'flake8' formatter..."
+        python -m flake8 --config=.flake8 ${TARGET_FILES}
+        PRE_COMMIT_EXIT_STATUS=$?
+
+        exit ${PRE_COMMIT_EXIT_STATUS}
+    ) || CHECK_EXIT_STATUS $?
+
+    ############
+    # JSON Linting
+    echo "📄 Executing 'json' formatter..."
+    TARGET_FILES=`${TRACKED_FILES} | sed 's/ /\n/g' | grep -E '.*\.json$'`
+    for TARGET_FILE in ${TARGET_FILES}; do
+        # Check if file is too large to be linted
+        FILE_SIZE=`du -m ${TARGET_FILE} | cut -f1`
+        if [ ${FILE_SIZE} -gt ${MAX_FILE_SIZE} ]; then
+            echo "📄 Skipping ${TARGET_FILE} (too large)"
+            continue
+        fi
+
+        python -m json.tool --sort-keys --indent=4 ${TARGET_FILE} 2>&1 | diff - ${TARGET_FILE} > /dev/null 2>&1
+
+        if [ $? -ne 0 ] ; then
+            JSON_PATCH="`python -m json.tool --sort-keys --indent=4 ${TARGET_FILE}`"
+            if [[ ! -z "${JSON_PATCH// }" ]]; then
+                echo "${JSON_PATCH}" > ${TARGET_FILE}    # The double quotes are important here!
+                echo "📄 $(tput bold)modified ${TARGET_FILE}$(tput sgr0)"
+            else
+                echo "📄 $(tput bold)${TARGET_FILE} is not valid JSON!$(tput sgr0)"
+            fi
+            CHECK_EXIT_STATUS 1
+        fi
+    done
+popd > /dev/null
+
+
+if [ "${EXIT_STATUS}" -ne 0 ]; then
+    echo "🚨 Pre-commit hooks failed. Please fix the issues and re-run 'git add' and 'git commit' 🚑"
 fi
 
-yamllint --no-warnings ./
 
-python -m flake8
+exit $EXIT_STATUS
