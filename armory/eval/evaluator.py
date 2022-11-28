439cdfd64898f4567c166559adadd5997f28ddb2
Christopher Woodall <woodall.christopher@gmail.com>
2022-11-28 14:52:06 -0500

    remove redundant `exit_code`

--- armory/eval/evaluator.py
+++ armory/eval/evaluator.py
@@ -204,11 +204,9 @@ class Evaluator(object):
 
         except KeyboardInterrupt:
             log.warning("Keyboard interrupt caught")
-            exit_code = 0
 
         except Exception:
             log.exception("Error running scenario")
-            exit_code = 0
 
         finally:
             if self.no_docker:
