1b1ced85450ef9f8cf78a0b45e311956441e241e
Christopher Woodall <woodall.christopher@gmail.com>
2022-11-30 14:05:21 -0500

    remove `tf2` container

--- armory/docker/images.py
+++ armory/docker/images.py
@@ -15,20 +15,20 @@ log.trace(f"armory.__version__: {armory.__version__}")
 
 DOCKER_REPOSITORY = "twosixarmory"
 
-PYTORCH = f"{DOCKER_REPOSITORY}/pytorch:{TAG}"
+ARMORY_IMAGE_NAME = f"{DOCKER_REPOSITORY}/armory:{TAG}"
+PYTORCH = ARMORY_IMAGE_NAME
+TF2 = ARMORY_IMAGE_NAME
 PYTORCH_DEEPSPEECH = f"{DOCKER_REPOSITORY}/pytorch-deepspeech:{TAG}"
-TF2 = f"{DOCKER_REPOSITORY}/tf2:{TAG}"
 ALL = (
-    PYTORCH,
+    ARMORY_IMAGE_NAME,
     PYTORCH_DEEPSPEECH,
-    TF2,
 )
 REPOSITORIES = tuple(x.split(":")[0] for x in ALL)
 IMAGE_MAP = {
-    "pytorch": PYTORCH,
+    "pytorch": ARMORY_IMAGE_NAME,
     "pytorch-deepspeech": PYTORCH_DEEPSPEECH,
-    "tf2": TF2,
-    "carla-mot": PYTORCH,
+    "tf2": ARMORY_IMAGE_NAME,
+    "carla-mot": ARMORY_IMAGE_NAME,
 }
 
 
