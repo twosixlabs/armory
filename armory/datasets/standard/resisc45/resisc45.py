6732e7160beb8d47ae56eec32969103f14dd2bb5
lucas.cadalzo <lucas.cadalzo@twosixlabs.com>
2022-11-30 18:02:22 -0500

    dl to resisc_45 not resisc45_split; use single file and create splits programmatically

--- armory/datasets/standard/resisc45/resisc45.py
+++ armory/datasets/standard/resisc45/resisc45.py
@@ -79,20 +79,12 @@ _LABELS = [
     "wetland",
 ]
 
-_URL = "http://www.escience.cn/people/JunweiHan/NWPU-RESISC45.html"
+_HOMEPAGE = "http://www.escience.cn/people/JunweiHan/NWPU-RESISC45.html"
 
-_URL_PREFIX = "https://armory-public-data.s3.us-east-2.amazonaws.com/resisc45/"
-_URLS = [
-    os.path.join(_URL_PREFIX, x)
-    for x in (
-        "resisc45_train.tar.gz",
-        "resisc45_validation.tar.gz",
-        "resisc45_test.tar.gz",
-    )
-]
+_URL = "https://armory-public-data.s3.us-east-2.amazonaws.com/resisc45/NWPU-RESISC45.tar.gz"
 
 
-class Resisc45Split(tfds.core.GeneratorBasedBuilder):
+class Resisc45(tfds.core.GeneratorBasedBuilder):
     """NWPU Remote Sensing Image Scene Classification (RESISC) Dataset."""
 
     VERSION = tfds.core.Version("3.0.0")
@@ -109,33 +101,34 @@ class Resisc45Split(tfds.core.GeneratorBasedBuilder):
                 }
             ),
             supervised_keys=("image", "label"),
-            homepage=_URL,
+            homepage=_HOMEPAGE,
             citation=_CITATION,
         )
 
     def _split_generators(self, dl_manager):
         """Returns SplitGenerators."""
-        paths = dl_manager.download_and_extract(_URLS)
-        splits = []
-        for path, subdir, name in zip(
-            paths,
-            ["train", "validation", "test"],
-            [tfds.Split.TRAIN, tfds.Split.VALIDATION, tfds.Split.TEST],
-        ):
-            splits.append(
-                tfds.core.SplitGenerator(
-                    name=name, gen_kwargs={"path": os.path.join(path, subdir)}
-                )
-            )
-        return splits
+        path = dl_manager.download_and_extract(_URL)
+        return {
+            split: self._generate_examples(path, split)
+            for split in ["train", "validation", "test"]
+        }
 
-    def _generate_examples(self, path):
+    def _generate_examples(self, path, split):
         """Yields examples."""
-        for label in tf.io.gfile.listdir(path):
-            for filename in tf.io.gfile.glob(os.path.join(path, label, "*.jpg")):
+        for label in tf.io.gfile.listdir(f"{path}/NWPU-RESISC45"):
+            for idx, filename in enumerate(
+                tf.io.gfile.glob(f"{path}/NWPU-RESISC45/{label}/*.jpg")
+            ):
                 example = {
                     "image": filename,
                     "label": label,
                     "filename": os.path.basename(filename),
                 }
-                yield filename, example
+                if idx <= 500:
+                    split_idx = "train"
+                elif idx <= 600:
+                    split_idx = "validation"
+                else:
+                    split_idx = "test"
+                if split_idx == split:
+                    yield filename, example
