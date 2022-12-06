4d8ac635273f6220a4d427cdd4e42f0104489f2b
Paul Park <paul.park@twosixtech.com>
2022-12-06 01:36:06 +0000

    reverting changes to test loading custom metrics

--- armory/scenarios/scenario.py
+++ armory/scenarios/scenario.py
@@ -20,9 +20,6 @@ from armory.metrics import compute
 from armory.utils import config_loading, json_utils
 from armory.logs import log
 
-from armory.metrics.task import population
-from armory.metrics.common import supported
-
 
 class Scenario:
     """
@@ -449,8 +446,6 @@ class Scenario:
         self.load()
         self.evaluate_all()
         self.finalize_results()
-        log.info(supported.__dict__)
-        log.info(population)
         log.debug("Clearing global instrumentation variables")
         del_globals()
 
