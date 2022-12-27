74da0b0c719f6057d8f61a1e9f0cd2d87795dabd
Paul Park <paul.park@twosixtech.com>
2022-12-27 22:09:41 +0000

    initial commit for german_traffic_signal after executing tfds new german_traffic_signal in armory/dataset/standard

---
+++ armory/datasets/standard/german_traffic_signal/__init__.py
@@ -0,0 +1,3 @@
+"""german_traffic_signal dataset."""
+
+from .german_traffic_signal import GermanTrafficSignal
---
+++ armory/datasets/standard/german_traffic_signal/german_traffic_signal.py
@@ -0,0 +1,64 @@
+"""german_traffic_signal dataset."""
+
+import tensorflow_datasets as tfds
+
+# TODO(german_traffic_signal): Markdown description  that will appear on the catalog page.
+_DESCRIPTION = """
+Description is **formatted** as markdown.
+
+It should also contain any processing which has been applied (if any),
+(e.g. corrupted example skipped, images cropped,...):
+"""
+
+# TODO(german_traffic_signal): BibTeX citation
+_CITATION = """
+"""
+
+
+class GermanTrafficSignal(tfds.core.GeneratorBasedBuilder):
+    """DatasetBuilder for german_traffic_signal dataset."""
+
+    VERSION = tfds.core.Version("1.0.0")
+    RELEASE_NOTES = {
+        "1.0.0": "Initial release.",
+    }
+
+    def _info(self) -> tfds.core.DatasetInfo:
+        """Returns the dataset metadata."""
+        # TODO(german_traffic_signal): Specifies the tfds.core.DatasetInfo object
+        return tfds.core.DatasetInfo(
+            builder=self,
+            description=_DESCRIPTION,
+            features=tfds.features.FeaturesDict(
+                {
+                    # These are the features of your dataset like images, labels ...
+                    "image": tfds.features.Image(shape=(None, None, 3)),
+                    "label": tfds.features.ClassLabel(names=["no", "yes"]),
+                }
+            ),
+            # If there's a common (input, target) tuple from the
+            # features, specify them here. They'll be used if
+            # `as_supervised=True` in `builder.as_dataset`.
+            supervised_keys=("image", "label"),  # Set to `None` to disable
+            homepage="https://dataset-homepage/",
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager: tfds.download.DownloadManager):
+        """Returns SplitGenerators."""
+        # TODO(german_traffic_signal): Downloads the data and defines the splits
+        path = dl_manager.download_and_extract("https://todo-data-url")
+
+        # TODO(german_traffic_signal): Returns the Dict[split names, Iterator[Key, Example]]
+        return {
+            "train": self._generate_examples(path / "train_imgs"),
+        }
+
+    def _generate_examples(self, path):
+        """Yields examples."""
+        # TODO(german_traffic_signal): Yields (key, example) tuples from the dataset
+        for f in path.glob("*.jpeg"):
+            yield "key", {
+                "image": f,
+                "label": "yes",
+            }
