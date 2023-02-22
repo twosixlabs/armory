fa47eea4128d4b4ffd808fd296b9f0f7beea2429
Paul Park <paul.park@twosixtech.com>
2023-02-22 04:49:37 +0000

    removed commented template code

--- armory/datasets/standard/carla_over_obj_det_train/carla_over_obj_det_train.py
+++ armory/datasets/standard/carla_over_obj_det_train/carla_over_obj_det_train.py
@@ -37,24 +37,6 @@ class CarlaOverObjDetTrain(tfds.core.GeneratorBasedBuilder):
 
     def _info(self) -> tfds.core.DatasetInfo:
         """Returns the dataset metadata."""
-        # TODO(carla_over_obj_det_train): Specifies the tfds.core.DatasetInfo object
-        # return tfds.core.DatasetInfo(
-        #     builder=self,
-        #     description=_DESCRIPTION,
-        #     features=tfds.features.FeaturesDict(
-        #         {
-        #             # These are the features of your dataset like images, labels ...
-        #             "image": tfds.features.Image(shape=(None, None, 3)),
-        #             "label": tfds.features.ClassLabel(names=["no", "yes"]),
-        #         }
-        #     ),
-        #     # If there's a common (input, target) tuple from the
-        #     # features, specify them here. They'll be used if
-        #     # `as_supervised=True` in `builder.as_dataset`.
-        #     supervised_keys=("image", "label"),  # Set to `None` to disable
-        #     homepage="https://dataset-homepage/",
-        #     citation=_CITATION,
-        # )
         features = {
             # sequence of [RGB, depth] images
             "image": tfds.features.Sequence(
@@ -105,14 +87,8 @@ class CarlaOverObjDetTrain(tfds.core.GeneratorBasedBuilder):
 
     def _split_generators(self, dl_manager: tfds.download.DownloadManager):
         """Returns SplitGenerators."""
-        # TODO(carla_over_obj_det_train): Downloads the data and defines the splits
-        # path = dl_manager.download_and_extract("https://todo-data-url")
         path = dl_manager.download_and_extract(_URLS)
 
-        # TODO(carla_over_obj_det_train): Returns the Dict[split names, Iterator[Key, Example]]
-        # return {
-        #     "train": self._generate_examples(path / "train_imgs"),
-        # }
         return [
             tfds.core.SplitGenerator(
                 name=split,
@@ -123,13 +99,6 @@ class CarlaOverObjDetTrain(tfds.core.GeneratorBasedBuilder):
 
     def _generate_examples(self, path):
         """Yields examples."""
-        # TODO(carla_over_obj_det_train): Yields (key, example) tuples from the dataset
-        # for f in path.glob("*.jpeg"):
-        #     yield "key", {
-        #         "image": f,
-        #         "label": "yes",
-        #     }
-
         yield_id = 0
 
         annotation_path = os.path.join(path, "kwcoco_annotations.json")
