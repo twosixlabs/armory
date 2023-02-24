a291f7f39f2195e72f213a20353cbea03ceaf442
Jonathan Prokos <jonathan.prokos@twosixtech.com>
2023-02-24 15:08:09 +0000

    black formatting

--- armory/utils/__main__.py
+++ armory/utils/__main__.py
@@ -6,6 +6,7 @@ from armory.__main__ import _debug, Command
 from typing import Union, Tuple
 import os
 from pathlib import Path
+
 try:
     import numpy as np
 except ImportError:
@@ -33,7 +34,10 @@ try:
     from armory.art_experimental.attacks.carla_obj_det_utils import rgb_depth_to_linear
     from armory.art_experimental.attacks.carla_obj_det_utils import linear_to_log
 except ImportError:
-    print("WARNING: armory.art_experimental.attacks.carla_obj_det_utils cannot be imported. Using stubbed functions.")
+    print(
+        "WARNING: armory.art_experimental.attacks.carla_obj_det_utils cannot be imported. Using stubbed functions."
+    )
+
     def rgb_depth_to_linear(r, g, b):
         """
         Converts rgb depth values between [0,1] to linear depth in meters.
@@ -46,6 +50,7 @@ except ImportError:
         depth_m = r_ + g_ * 256 + b_ * 256 * 256
         depth_m = depth_m * 1000.0 / (256**3 - 1)
         return depth_m
+
     # Reference: https://github.com/carla-simulator/data-collector/blob/master/carla/image_converter.py
     def linear_to_log(depth_meters):
         """
@@ -74,9 +79,10 @@ def load_images(path: Union[str, Path, list]) -> list:
         return [load_image(p) for p in path]
     return [load_image(path)]
 
+
 def convert_image(image: PIL.Image):  # -> Tuple[PIL.Image, PIL.Image]:
-    r,g,b = [np.array(x) for x in image.split()]
-    depth_m = rgb_depth_to_linear(r/255.0, g/255.0, b/255.0)
+    r, g, b = [np.array(x) for x in image.split()]
+    depth_m = rgb_depth_to_linear(r / 255.0, g / 255.0, b / 255.0)
     img_linear = PIL.Image.fromarray(depth_m)
     img_log = PIL.Image.fromarray(linear_to_log(depth_m) * 255)
     return img_linear, img_log
@@ -84,31 +90,25 @@ def convert_image(image: PIL.Image):  # -> Tuple[PIL.Image, PIL.Image]:
 
 def rgb_depth_convert(command_args, prog, description):
     parser = argparse.ArgumentParser(
-        prog=prog, 
+        prog=prog,
         description=description,
-        formatter_class=argparse.RawTextHelpFormatter,)
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
 
     parser.add_argument(
-        "-i",
-        "--input",
-        type=Path,
-        nargs="+",
-        help="Path to depth image to convert"
+        "-i", "--input", type=Path, nargs="+", help="Path to depth image to convert"
     )
     parser.add_argument(
-        "-o",
-        "--output",
-        type=str,
-        help="Path to save converted depth image"
+        "-o", "--output", type=str, help="Path to save converted depth image"
     )
     parser.add_argument(
         "-s",
         "--show",
         action="store_true",
-        help="Show converted depth image using matplotlib" +
-        "\n  Note:\trequires X11 forwarding & GUI backend" +
-        "\n\tinstalled (ex. PyQt5) to work over ssh." +
-        "\n  See:\thttps://matplotlib.org/faq/usage_faq.html#what-is-a-backend"
+        help="Show converted depth image using matplotlib"
+        + "\n  Note:\trequires X11 forwarding & GUI backend"
+        + "\n\tinstalled (ex. PyQt5) to work over ssh."
+        + "\n  See:\thttps://matplotlib.org/faq/usage_faq.html#what-is-a-backend",
     )
     parser.add_argument(
         "--format",
@@ -123,10 +123,10 @@ def rgb_depth_convert(command_args, prog, description):
 
     if args.input is None:
         parser.error("input path is required")
-    
+
     if args.output is None and not args.show:
         parser.error("output path or show flag is required")
-    
+
     images = load_images(args.input)
     print(f"Loaded {len(images)} images from {args.input}")
     # Create a figure and plot the initial image
@@ -135,10 +135,14 @@ def rgb_depth_convert(command_args, prog, description):
     if len(images) > 1:
         # Add slider to switch image
         ax_slider = fig.add_axes([0.1, 0, 0.2, 0.05])
-        slider = widgets.Slider(ax_slider, 'Image', 0, len(images)-1, valinit=0, valstep=1)
+        slider = widgets.Slider(
+            ax_slider, "Image", 0, len(images) - 1, valinit=0, valstep=1
+        )
+
         def update_image(event):
             im.set_array(images[int(slider.val)])
             fig.canvas.draw_idle()
+
         slider.on_changed(update_image)
 
     # if args.format == "original":
@@ -149,30 +153,30 @@ def rgb_depth_convert(command_args, prog, description):
     #     im = ax.imshow(img_linear)
     # else:
     #     raise ValueError(f"Invalid format {args.format}")
-    
+
     def show_linear(event):
         im.set_array(convert_image(images[int(slider.val)])[0])
         fig.canvas.draw_idle()
-    
+
     def show_log(event):
         im.set_array(convert_image(images[int(slider.val)])[1])
         fig.canvas.draw_idle()
-    
+
     def show_orig(event):
         im.set_array(images[int(slider.val)])
         fig.canvas.draw_idle()
 
     # Add buttons to the plot
     linear_ax = fig.add_axes([0.7, 0.9, 0.2, 0.05])
-    linear_button = widgets.Button(linear_ax, 'Show Linear')
+    linear_button = widgets.Button(linear_ax, "Show Linear")
     linear_button.on_clicked(show_linear)
 
     log_ax = fig.add_axes([0.5, 0.9, 0.2, 0.05])
-    log_button = widgets.Button(log_ax, 'Show Log')
+    log_button = widgets.Button(log_ax, "Show Log")
     log_button.on_clicked(show_log)
 
     orig_ax = fig.add_axes([0.3, 0.9, 0.2, 0.05])
-    orig_button = widgets.Button(orig_ax, 'Show Original')
+    orig_button = widgets.Button(orig_ax, "Show Original")
     orig_button.on_clicked(show_orig)
 
     # Show the plot
