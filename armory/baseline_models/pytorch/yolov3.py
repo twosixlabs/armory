ff8fb45df8395a128867e1d496203880418180b2
Sterling <sterling.suggs@twosixtech.com>
2023-04-03 13:52:56 +0000

    yolov3 model

---
+++ armory/baseline_models/pytorch/yolov3.py
@@ -0,0 +1,49 @@
+import os
+from typing import Optional
+
+import torch
+
+from art.estimators.object_detection import PyTorchYolo
+from pytorchyolo.utils.loss import compute_loss
+from pytorchyolo.models import load_model
+
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class Yolo(torch.nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def forward(self, x, targets=None):
+        if self.training:
+            outputs = self.model(x)
+            loss, _ = compute_loss(outputs, targets, self.model)
+            loss_components_dict = {"loss_total": loss}
+            return loss_components_dict
+        else:
+            out = self.model(x)
+            return out
+
+
+def get_art_model(
+    model_kwargs: dict, wrapper_kwargs: dict, weights_path: Optional[str] = None
+) -> PyTorchYolo:
+
+    model_path = "armory/baseline_models/pytorch/yolov3.cfg"
+    model = load_model(model_path=model_path, weights_path=weights_path)
+    model_wrapper = Yolo(model)
+
+    params = [p for p in model_wrapper.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.01)
+
+    detector = PyTorchYolo(
+        model=model_wrapper,
+        device_type=DEVICE,
+        input_shape=(416, 416, 3),
+        optimizer=optimizer,
+        clip_values=(0, 1),
+        channels_first=False,
+        attack_losses=("loss_total",)
+    )
+    return detector
