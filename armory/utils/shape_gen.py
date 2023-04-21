cff7d7558d8fd6a54cc3cfc9826ee105b5d57efd
Jonathan Prokos <jonathan.prokos@twosixtech.com>
2023-04-21 20:04:45 +0000

    Adding concentric circles, jxcr_gear, and sierpinski

--- armory/utils/shape_gen.py
+++ armory/utils/shape_gen.py
@@ -3,13 +3,32 @@ from dataclasses import dataclass
 import inspect
 from io import BytesIO
 import os
+from pathlib import Path
+from typing import Union
 
 from PIL import Image
+import cairosvg
 import matplotlib.pyplot as plt
 import numpy as np
+import svg
+
+RADIUS_INNER = -50  # 25
+STROKE_WIDTH = 2
 
 
-def asterisk(outdir) -> str:
+def _write(outdir: Union[str, Path, BytesIO], **kwargs):
+    if not isinstance(outdir, BytesIO):
+        outfile = os.path.join(
+            os.path.abspath(outdir),
+            f"{inspect.currentframe().f_back.f_code.co_name}.png",
+        )
+    else:
+        outfile = outdir
+    plt.savefig(outfile, transparent=True, **kwargs)
+    return outfile
+
+
+def asterisk(outdir: Union[str, Path, BytesIO]) -> Union[str, BytesIO]:
     if not isinstance(outdir, BytesIO) and not os.path.isdir(outdir):
         raise ValueError(f"outdir must be valid a directory or BytesIO, got: {outdir}")
     theta = np.linspace(0, 2 * np.pi, 18, endpoint=False)
@@ -26,17 +45,10 @@ def asterisk(outdir) -> str:
     ax.set_axis_off()
     ax.fill(x1, y1, color="slateblue")
     ax.set_facecolor((1, 1, 1, 0))
-    if not isinstance(outdir, BytesIO):
-        outfile = os.path.join(
-            os.path.abspath(outdir), f"{inspect.currentframe().f_code.co_name}.png"
-        )
-    else:
-        outfile = outdir
-    fig.savefig(outfile, transparent=True)
-    return outfile
+    return _write(outdir)
 
 
-def circle(outdir) -> str:
+def circle(outdir: Union[str, Path, BytesIO]) -> Union[str, BytesIO]:
     if not isinstance(outdir, BytesIO) and not os.path.isdir(outdir):
         raise ValueError(f"outdir must be valid a directory, got: {outdir}")
     radius = 1
@@ -62,17 +74,15 @@ def circle(outdir) -> str:
     ax.fill(x, y, color="slateblue", alpha=a)
     # ax.set_facecolor((1, 1, 1, 0))
     ax.margins(0, 0)
-    if not isinstance(outdir, BytesIO):
-        outfile = os.path.join(
-            os.path.abspath(outdir), f"{inspect.currentframe().f_code.co_name}.png"
-        )
-    else:
-        outfile = outdir
-    fig.savefig(outfile, transparent=True, bbox_inches="tight", pad_inches=0)
-    return outfile
+    return _write(outdir)
 
 
-def grid(outdir, num_circles=25, circle_radius=0.5, circle_spacing=2.5) -> str:
+def grid(
+    outdir: Union[str, Path, BytesIO],
+    num_circles=25,
+    circle_radius=0.5,
+    circle_spacing=2.5,
+) -> Union[str, BytesIO]:
     if not isinstance(outdir, BytesIO) and not os.path.isdir(outdir):
         raise ValueError(f"outdir must be valid a directory, got: {outdir}")
     # Set up figure and axes
@@ -112,17 +122,241 @@ def grid(outdir, num_circles=25, circle_radius=0.5, circle_spacing=2.5) -> str:
             ax.add_artist(circle)
 
     # Save figure as png with transparent background
+    return _write(outdir, bbox_inches="tight", pad_inches=0)
+
+
+# Start of SVG generation code
+def _write_svg(
+    shape: svg.SVG, outdir: Union[str, Path, BytesIO]
+) -> Union[str, BytesIO]:
     if not isinstance(outdir, BytesIO):
         outfile = os.path.join(
-            os.path.abspath(outdir), f"{inspect.currentframe().f_code.co_name}.png"
+            os.path.abspath(outdir),
+            f"{inspect.currentframe().f_back.f_code.co_name}.png",
         )
     else:
         outfile = outdir
-    plt.savefig(outfile, bbox_inches="tight", pad_inches=0, transparent=True)
+    cairosvg.svg2png(bytestring=str(shape), write_to=outfile)
     return outfile
 
 
-_SHAPES = {f.__name__: f for f in (asterisk, circle, grid)}
+def _circles_outer(num_rot, cent_x, cent_y, rad):
+    circles_lst = []
+    angle = 360 / num_rot
+    curr = 0
+    for i in range(num_rot):
+        curr = i * angle
+        circles_lst.append(_circle_outer(curr, cent_x, cent_y, rad))
+    return circles_lst
+
+
+def _circle_outer(rotation, cent_x, cent_y, rad):
+    return svg.Circle(
+        cx=cent_x,
+        cy=cent_y - rad - RADIUS_INNER,
+        r=rad,
+        stroke="#F73859",
+        fill="transparent",
+        stroke_width=STROKE_WIDTH,
+        transform=[
+            svg.Rotate(
+                rotation, cent_x, cent_y
+            )  # rotate wrt shape, 90=rotational value, x, y
+        ],
+    )
+
+
+def _circles_concentric(num_it, cent_x, cent_y, min_rad, max_rad):
+    circles_lst = []
+    if num_it == 0:
+        return []
+    rad_increase = (max_rad - min_rad) / num_it  # (outer_rad - inner_rad) / num_it
+    rad = min_rad
+    while rad <= max_rad:
+        circles_lst.append(_circle_concentric(cent_x, cent_y, rad))
+        rad += rad_increase
+    return circles_lst
+
+
+def _circle_concentric(cent_x, cent_y, rad):
+    return svg.Circle(
+        cx=cent_x,
+        cy=cent_y,
+        r=rad,
+        stroke="black",
+        fill="transparent",
+        stroke_width=STROKE_WIDTH,
+    )
+
+
+def concentric_circles(
+    outdir: Union[str, Path, BytesIO],
+    num_it=0,
+    cent_x=250,
+    cent_y=250,
+    min_rad=RADIUS_INNER,
+    max_rad=200,
+    # circles outer
+    num_rot=20,
+    rad=100,
+    width=500,
+    height=500,
+) -> Union[str, BytesIO]:
+    if not isinstance(outdir, BytesIO) and not os.path.isdir(outdir):
+        raise ValueError(f"outdir must be valid a directory or BytesIO, got: {outdir}")
+    circles_con = _circles_concentric(num_it, cent_x, cent_y, min_rad, max_rad)
+    circles_out = _circles_outer(num_rot, cent_x, cent_y, rad)
+
+    out = svg.SVG(width=width, height=height, elements=circles_con + circles_out)
+
+    _write_svg(out, outdir)
+
+
+def _kites(num_rot, cent_x, cent_y):
+    kites_lst = []
+    angle = 360 / num_rot
+    curr = 0
+    for i in range(num_rot):
+        curr = i * angle
+        kites_lst.append(_kite(curr, cent_x, cent_y))
+    return kites_lst
+
+
+def _kite(rotation, cent_x, cent_y):
+    return svg.Polygon(
+        points=[
+            250,
+            250,  # center
+            215,
+            100,  # left point
+            250,
+            80,  # center top point (origin - (diff between radii))
+            285,
+            100,  # right point
+        ],
+        stroke="#F73859",
+        fill="#F73859",  # "transparent", #181a1b
+        stroke_width=STROKE_WIDTH,
+        transform=[
+            svg.Rotate(
+                rotation, cent_x, cent_y
+            )  # rotate wrt shape, 90=rotational value, x, y
+        ],
+    )
+
+
+def _kite_lines(num_rot, x1, y1, x2, y2):
+    lines_lst = []
+    angle = 360 / num_rot
+    curr = 0
+    for i in range(num_rot):
+        curr = i * angle
+        lines_lst.append(_kite_line(curr, x1, y1, x2, y2))
+    return lines_lst
+
+
+def _kite_line(rotation, cx, cy, ox, oy):
+    return svg.Line(
+        x1=cx,
+        y1=cy,
+        x2=ox,
+        y2=oy,
+        stroke="#F73859",
+        stroke_width=STROKE_WIDTH,
+        transform=[
+            svg.Rotate(rotation, cx, cy)  # rotate wrt shape, 90=rotational value, x, y
+        ],
+    )
+
+
+def jxcr_gear(outdir: Union[str, Path, BytesIO]) -> Union[str, BytesIO]:
+    kite_elems = _kites(8, 250, 250)
+    kite_line_elems = _kite_lines(
+        8, 250, 250, 250, 50
+    )  # num_rotations, center_x, center_y, outer_x (center_x - outer_radius), outer_y (center_y - outer_radius)
+    circle_elems = [
+        svg.Circle(
+            cx=250,
+            cy=250,
+            r=200,
+            stroke="#F73859",
+            fill="transparent",
+            stroke_width=STROKE_WIDTH,
+        ),
+        svg.Circle(
+            cx=250,
+            cy=250,
+            r=125,
+            stroke="#F73859",
+            fill="transparent",
+            stroke_width=STROKE_WIDTH,
+        ),
+        svg.Circle(
+            cx=250,
+            cy=250,
+            r=25,
+            stroke="#F73859",
+            fill="#F73859",
+            stroke_width=2,
+        ),
+    ]
+    return _write_svg(
+        shape=svg.SVG(
+            width=500, height=500, elements=kite_line_elems + kite_elems + circle_elems
+        ),
+        outdir=outdir,
+    )
+
+
+# Grid-like
+def _sierpinski(n, p1, p2, p3):
+    """
+    Recursively generates a Sierpinski triangle.
+    """
+    if n == 0:
+        return [p1, p2, p3]
+    else:
+        p12 = (p1 + p2) / 2
+        p23 = (p2 + p3) / 2
+        p31 = (p3 + p1) / 2
+        s1 = _sierpinski(n - 1, p1, p12, p31)
+        s2 = _sierpinski(n - 1, p12, p2, p23)
+        s3 = _sierpinski(n - 1, p31, p23, p3)
+        return np.vstack((s1, s2, s3))
+
+
+def sierpinski(outdir: Union[str, Path, BytesIO]) -> Union[str, BytesIO]:
+    depth = 5  # depth of recursion
+    # Triangle vertices
+    p1 = np.array([0, 0])
+    p2 = np.array([1, 0])
+    p3 = np.array([0.5, np.sqrt(3) / 2])
+
+    points = _sierpinski(depth, p1, p2, p3)
+
+    # fig, ax = plt.subplots()
+    fig = plt.figure(frameon=False)
+    ax = fig.add_axes([0, 0, 1, 1])
+
+    ax.scatter(points[:, 0], points[:, 1], s=1, color="darkgray")
+
+    ax.set_xlim(-0.1, 1.1)
+    ax.set_ylim(-0.1, 1.1)
+    ax.set_aspect("equal")
+
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    # ax.set_title('Sierpinski Triangle')
+    ax.set_axis_off()
+    ax.set_facecolor((1, 1, 1, 0))
+    return _write(outdir, bbox_inches="tight", pad_inches=0)
+
+
+_SHAPES = {
+    f.__name__: f
+    for f in (asterisk, circle, grid, concentric_circles, jxcr_gear, sierpinski)
+}
 
 
 @dataclass
