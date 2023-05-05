687d3b743c896c6467bbf8200183704a384f1a50
Jonathan Prokos <jonathan.prokos@twosixtech.com>
2023-05-05 15:22:45 +0000

    Fixed to work from any working directory

--- armory/utils/cli.py
+++ armory/utils/cli.py
@@ -1,18 +1,26 @@
+import os
+
 from armory.logs import log
 
 
 def log_current_branch():
-    """Log the current git branch"""
+    """Log the current git branch of armory. Works independent of the current working directory."""
+    try:
+        from armory.__about__ import __version__
+
+        log.info(f"Armory version: {__version__}")
+    except ModuleNotFoundError:
+        log.info("Unable to extract armory version from __about__.py")
     try:
         import git
+
+        repo = git.Repo(
+            os.path.dirname(os.path.realpath(__file__)), search_parent_directories=True
+        )
+        log.info(f"Git branch: {repo.active_branch}")
     except ImportError:
-        log.warning("Unable to import git module")
-        return
-    try:
-        repo = git.Repo(search_parent_directories=True)
-        branch_name = repo.active_branch.name
-        log.info(f"Git branch: {branch_name}")
+        log.info("Unable to import gitpython, cannot determine git branch")
     except git.exc.InvalidGitRepositoryError:
-        log.warning("Unable to find git repository")
+        log.info("Unable to find .git directory, cannot determine git branch")
     except git.exc.GitCommandError:
-        log.warning("Unable to find git branch")
+        log.info("Unable to determine git branch")
