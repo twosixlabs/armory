4813dddfd29210979087f025745febf940221fa0
Sterling <sterling.suggs@twosixtech.com>
2023-05-16 17:29:27 +0000

    load full trigger path

--- armory/art_experimental/attacks/poison_loader_audio.py
+++ armory/art_experimental/attacks/poison_loader_audio.py
@@ -1,15 +1,20 @@
 from art.attacks.poisoning import PoisoningAttackBackdoor
-
 from art.attacks.poisoning.perturbations.audio_perturbations import (
     CacheAudioTrigger,
     CacheToneTrigger,
 )
+
+from armory.utils import triggers
 
 
 def poison_loader_audio(**kwargs):
     backdoor_kwargs = kwargs.pop("backdoor_kwargs")
 
     if "backdoor_path" in backdoor_kwargs:
+
+        backdoor_kwargs["backdoor_path"] = triggers.get_path(
+            backdoor_kwargs["backdoor_path"]
+        )
         trigger = CacheAudioTrigger(**backdoor_kwargs)
 
         def poison_func(x):
