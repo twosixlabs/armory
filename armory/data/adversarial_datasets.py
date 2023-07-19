d4ca3442f34a31209ea8fcdad61cdff747b40229
Yusong <ytan@mitre.org>
2023-07-19 18:07:47 +0000

    added CARLA overhead object detection test data

--- armory/data/adversarial_datasets.py
+++ armory/data/adversarial_datasets.py
@@ -789,7 +789,7 @@ def carla_over_obj_det_test(
     )
 
     return datasets._generator_from_tfds(
-        "carla_over_obj_det_test:1.0.0",
+        "carla_over_obj_det_test:2.0.0",
         split=split,
         batch_size=batch_size,
         epochs=epochs,
