784dbb6bd077cdcf7eab98142ef76dd76f0eaf4b
Jonathan Prokos <jonathan.prokos@twosixtech.com>
2023-08-31 03:31:37 +0000

    removing duplicate old code

--- armory/cli/tools/rgb_depth_convert.py
+++ armory/cli/tools/rgb_depth_convert.py
@@ -1,9 +1,7 @@
 import argparse
 from pathlib import Path
 from typing import Literal, Union
-import os
 
-from armory import paths
 from armory.cli.tools.utils import _debug
 from armory.logs import log, update_filters
 
@@ -303,35 +301,3 @@ def plot_mAP_by_giou_with_patch_cli(command_args, prog, description):
         output_filepath=args.output,
         include_classes=not args.exclude_classes,
     )
-
-
-def collect_armory_outputs(command_args, prog, description):
-    """TODO: Fill"""
-    parser = argparse.ArgumentParser(
-        prog=prog,
-        description=description,
-        formatter_class=argparse.RawTextHelpFormatter,
-    )
-    parser.add_argument(
-        "--glob",
-        "-g",
-        type=str,
-        help="Glob pattern to match outputs. Defaults to `*`.",
-        default="*",
-    )
-    _debug(parser)
-    args = parser.parse_args(command_args)
-    update_filters(args.log_level, args.debug)
-
-    # We need to check both output dirs since `--no-docker` isn't passed
-    _host = os.path.isdir(paths.HostPaths().output_dir)
-    _dock = os.path.isdir(paths.DockerPaths().output_dir)
-    if not _host and not _dock:
-        raise ValueError("No output dir found. Please run a task first.")
-    outputs = os.listdir(paths.HostPaths().output_dir) if _host else []
-    outputs += os.listdir(paths.DockerPaths().output_dir) if _dock else []
-    if len(outputs) == 0:
-        raise ValueError("No outputs found. Please run a task first.")
-    log.debug(f"Found {len(outputs)} outputs:\n{outputs}")
-
-    breakpoint()
